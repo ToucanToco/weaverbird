6c19b559d5e1b06e5004cf3aab93271b42b4a3cd
Adrien Di Mascio <adrien.dimascio@logilab.fr>
2019-01-23 18:06:46 +0100

    implement multi-steps transformation for $match
    A `$match` transform can generate one or two pipeline steps :
    
    - if `domain` is found in the input step, a `Domain` step must be generated
    - if there are other properties than `domain`, a `Filter` step must be genrated.

--- src/pipebuild.ts
+++ src/pipebuild.ts
@@ -16,10 +16,23 @@ export interface PipelineStep {
     [propName: string]: any;
 }
 
+/**
+ * Transform a mongo `$match` step into a list of pipeline steps.
+ * If a `domain` key is found in the `$match` step, generate a `Domain`
+ * pipeline step. After that, generate a `Filter` step with all the remaining
+ * properties.
+ *
+ * @param matchStep the match mongo step
+ */
 function transformMatch(matchStep: MongoStep): Array<PipelineStep> {
     const output: Array<PipelineStep> = [];
     if (matchStep.$match.domain !== undefined) {
         output.push({ step: 'Domain', query: { $match: { domain: matchStep.$match.domain } } });
+    }
+    const nodomain = { ...matchStep.$match };
+    delete nodomain.domain;
+    if (Object.keys(nodomain).length) {
+        output.push({ step: 'Filter', query: { $match: nodomain } });
     }
     return output;
 }
--- tests/unit/pipebuild.spec.ts
+++ tests/unit/pipebuild.spec.ts
@@ -1,9 +1,44 @@
 import { MongoStep, mongoToPipe } from '@/pipebuild';
 
 describe('Pipebuild translator', () => {
-    it('generate match steps', () => {
+    it('generate domain step', () => {
         const query: Array<MongoStep> = [{ $match: { domain: 'test_cube' } }];
         const pipeline = mongoToPipe(query);
         expect(pipeline).toEqual([{ step: 'Domain', query: { $match: { domain: 'test_cube' } } }]);
+    });
+
+    it('generate domain and filter steps from one match', () => {
+        const query: Array<MongoStep> = [
+            { $match: { domain: 'test_cube', Region: 'Europe', Manager: 'Pierre' } },
+        ];
+        const pipeline = mongoToPipe(query);
+        expect(pipeline).toEqual([
+            { step: 'Domain', query: { $match: { domain: 'test_cube' } } },
+            { step: 'Filter', query: { $match: { Region: 'Europe', Manager: 'Pierre' } } },
+        ]);
+    });
+
+    it('generate domain and filter steps', () => {
+        const query: Array<MongoStep> = [
+            { $match: { domain: 'test_cube' } },
+            { $match: { Region: 'Europe' } },
+        ];
+        const pipeline = mongoToPipe(query);
+        expect(pipeline).toEqual([
+            { step: 'Domain', query: { $match: { domain: 'test_cube' } } },
+            { step: 'Filter', query: { $match: { Region: 'Europe' } } },
+        ]);
+    });
+
+    it('generate domain step and filter several columns step', () => {
+        const query: Array<MongoStep> = [
+            { $match: { domain: 'test_cube' } },
+            { $match: { Region: 'Europe', Manager: 'Pierre' } },
+        ];
+        const pipeline = mongoToPipe(query);
+        expect(pipeline).toEqual([
+            { step: 'Domain', query: { $match: { domain: 'test_cube' } } },
+            { step: 'Filter', query: { $match: { Region: 'Europe', Manager: 'Pierre' } } },
+        ]);
     });
 });
