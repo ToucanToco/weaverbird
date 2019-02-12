33a246d6bbe5d55d6e02f3a6185ef9750afa8b85
Adrien Di Mascio <adrien.dimascio@logilab.fr>
2019-02-12 22:44:39 +0100

    [pkg] don't use lodash until necessary
    lodash is a great library and we might end up using it but
    let\'s try to keep dependencies list as lean as possible

--- src/components/Pipeline.vue
+++ src/components/Pipeline.vue
@@ -1,63 +1,71 @@
 <template>
-  <div class="query-pipeline">
-    <DomainSelector :domains-list="domainsList" :selected-domain="selectedDomain" @selectedDomain="updateDomain"/>
-    <div v-if="isEmpty" class="query-pipeline__empty-container">
-      <div class="query-pipeline__empty-message">
-        Start playing with data right from the table opposite or switch to Code View with <i class="fas fa-code"></i> !
-      </div>
-      <i class="fas fa-magic"></i>
+    <div class="query-pipeline">
+        <DomainSelector
+            :domains-list="domainsList"
+            :selected-domain="selectedDomain"
+            @selectedDomain="updateDomain"
+        />
+        <div v-if="isEmpty" class="query-pipeline__empty-container">
+            <div
+                class="query-pipeline__empty-message"
+            >Start playing with data right from the table opposite or switch to Code View with
+                <i class="fas fa-code"></i> !
+            </div>
+            <i class="fas fa-magic"></i>
+        </div>
+        <Step
+            v-else
+            v-for="(step, index) in stepsWithoutDomain"
+            :key="index"
+            :isActive="isStepSelected(index)"
+            :isFirst="index === 0"
+            :isLast="index === stepsWithoutDomain.length - 1"
+            :step="step"
+            @selectedStep="selectStep(index)"
+        />
     </div>
-    <Step v-else v-for="(step, index) in stepsWithoutDomain" :key="index"
-          :isActive="isStepSelected(index)"
-          :isFirst="index === 0"
-          :isLast="index === stepsWithoutDomain.length - 1"
-          :step="step"
-          @selectedStep="selectStep(index)"
-    />
-  </div>
 </template>
 <script>
-import _ from 'lodash';
 import DomainSelector from './DomainSelector.vue';
 import Step from './Step.vue';
 
 export default {
-  name: 'pipeline',
-  components: {
-    DomainSelector,
-    Step,
-  },
-  props: {
-    steps: Array,
-    domainsList: Array,
-  },
-  data() {
-    return {
-      selectedStep: this.steps.length - 2,
-    };
-  },
-  computed: {
-    isEmpty() {
-      return this.stepsWithoutDomain.length === 0;
+    name: 'pipeline',
+    components: {
+        DomainSelector,
+        Step,
     },
-    selectedDomain() {
-      return this.steps[0].domain;
+    props: {
+        steps: Array,
+        domainsList: Array,
     },
-    stepsWithoutDomain() {
-      return _.tail(this.steps);
+    data() {
+        return {
+            selectedStep: this.steps.length - 2,
+        };
     },
-  },
-  methods: {
-    isStepSelected(index) {
-      return this.selectedStep === index;
+    computed: {
+        isEmpty() {
+            return this.stepsWithoutDomain.length === 0;
+        },
+        selectedDomain() {
+            return this.steps[0].domain;
+        },
+        stepsWithoutDomain() {
+            return this.steps.slice(1);
+        },
     },
-    selectStep(index) {
-      this.selectedStep = index;
+    methods: {
+        isStepSelected(index) {
+            return this.selectedStep === index;
+        },
+        selectStep(index) {
+            this.selectedStep = index;
+        },
+        updateDomain(newDomain) {
+            return newDomain; // Emit an event
+        },
     },
-    updateDomain(newDomain) {
-      return newDomain; // Emit an event
-    },
-  },
 };
 </script>
 <style scoped>
