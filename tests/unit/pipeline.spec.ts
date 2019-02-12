dc54b521bef073ea185a6b99a595fcb2e4277ea9
Adrien Di Mascio <adrien.dimascio@logilab.fr>
2019-02-12 22:54:57 +0100

    [tests] fix Pipeline rendering test
    The first step (supposedly the "domain" one) is processed
    separately by the Pipeline component, there is no corresponding
    <Step> component.

--- tests/unit/pipeline.spec.ts
+++ tests/unit/pipeline.spec.ts
@@ -5,25 +5,22 @@ describe('Pipeline.vue', () => {
   it('renders steps', () => {
     const wrapper = shallowMount(Pipeline, {
       propsData: {
-        steps: [{ name: 'filter' }, { name: 'rename' }, { name: 'group' }],
+        steps: [{ name: 'domain' }, { name: 'rename' }, { name: 'group' }],
       },
     });
     const steps = wrapper.findAll('step-stub');
-    expect(steps.length).toBe(3);
-    const [step1, step2, step3] = steps.wrappers.map(stub => stub.props());
+    // first step is the domain step and is processed separately by the
+    // Pipeline component. There should only be the 2 generated <Step>
+    // components.
+    expect(steps.length).toBe(2);
+    const [step1, step2] = steps.wrappers.map(stub => stub.props());
     expect(step1).toEqual({
-      step: { name: 'filter' },
+      step: { name: 'rename' },
       isActive: false,
       isFirst: true,
       isLast: false,
     });
     expect(step2).toEqual({
-      step: { name: 'rename' },
-      isActive: false,
-      isFirst: false,
-      isLast: false,
-    });
-    expect(step3).toEqual({
       step: { name: 'group' },
       isActive: true,
       isFirst: false,
