fbe150d87d78cdc375e71a971fbdf449106eb502
Adrien Di Mascio <adrien.dimascio@logilab.fr>
2019-02-13 09:48:29 +0100

    use 2-spaces indentation style on Pipeline.vue

--- src/components/Pipeline.vue
+++ src/components/Pipeline.vue
@@ -1,71 +1,71 @@
 <template>
-    <div class="query-pipeline">
-        <DomainSelector
-            :domains-list="domainsList"
-            :selected-domain="selectedDomain"
-            @selectedDomain="updateDomain"
-        />
-        <div v-if="isEmpty" class="query-pipeline__empty-container">
-            <div
-                class="query-pipeline__empty-message"
-            >Start playing with data right from the table opposite or switch to Code View with
-                <i class="fas fa-code"></i> !
-            </div>
-            <i class="fas fa-magic"></i>
-        </div>
-        <Step
-            v-else
-            v-for="(step, index) in stepsWithoutDomain"
-            :key="index"
-            :isActive="isStepSelected(index)"
-            :isFirst="index === 0"
-            :isLast="index === stepsWithoutDomain.length - 1"
-            :step="step"
-            @selectedStep="selectStep(index)"
-        />
+  <div class="query-pipeline">
+    <DomainSelector
+      :domains-list="domainsList"
+      :selected-domain="selectedDomain"
+      @selectedDomain="updateDomain"
+    />
+    <div v-if="isEmpty" class="query-pipeline__empty-container">
+      <div
+          class="query-pipeline__empty-message"
+      >Start playing with data right from the table opposite or switch to Code View with
+          <i class="fas fa-code"></i> !
+      </div>
+      <i class="fas fa-magic"></i>
     </div>
+    <Step
+      v-else
+      v-for="(step, index) in stepsWithoutDomain"
+      :key="index"
+      :isActive="isStepSelected(index)"
+      :isFirst="index === 0"
+      :isLast="index === stepsWithoutDomain.length - 1"
+      :step="step"
+      @selectedStep="selectStep(index)"
+    />
+  </div>
 </template>
 <script>
 import DomainSelector from './DomainSelector.vue';
 import Step from './Step.vue';
 
 export default {
-    name: 'pipeline',
-    components: {
-        DomainSelector,
-        Step,
+  name: 'pipeline',
+  components: {
+    DomainSelector,
+    Step,
+  },
+  props: {
+    steps: Array,
+    domainsList: Array,
+  },
+  data() {
+    return {
+      selectedStep: this.steps.length - 2,
+    };
+  },
+  computed: {
+    isEmpty() {
+      return this.stepsWithoutDomain.length === 0;
     },
-    props: {
-        steps: Array,
-        domainsList: Array,
+    selectedDomain() {
+      return this.steps[0].domain;
     },
-    data() {
-        return {
-            selectedStep: this.steps.length - 2,
-        };
+    stepsWithoutDomain() {
+      return this.steps.slice(1);
     },
-    computed: {
-        isEmpty() {
-            return this.stepsWithoutDomain.length === 0;
-        },
-        selectedDomain() {
-            return this.steps[0].domain;
-        },
-        stepsWithoutDomain() {
-            return this.steps.slice(1);
-        },
+  },
+  methods: {
+    isStepSelected(index) {
+      return this.selectedStep === index;
     },
-    methods: {
-        isStepSelected(index) {
-            return this.selectedStep === index;
-        },
-        selectStep(index) {
-            this.selectedStep = index;
-        },
-        updateDomain(newDomain) {
-            return newDomain; // Emit an event
-        },
+    selectStep(index) {
+      this.selectedStep = index;
     },
+    updateDomain(newDomain) {
+      return newDomain; // Emit an event
+    },
+  },
 };
 </script>
 <style scoped>
