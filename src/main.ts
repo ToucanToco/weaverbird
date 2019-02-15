4f0a1f2e9cdb338f5a716f04ccc6fb160bcde29d
Jordan Lorho <jordan.lorho@gmail.com>
2019-02-15 11:58:54 +0100

    add :: possibility to test Pipeline into App component

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,16 @@ export { mongoToPipe, pipeToMongo } from './pipebuild';
 import Pipeline from './components/Pipeline.vue';
 
 export { Pipeline };
+
+/*
+  Decomment to test it out the box
+*/
+
+// import Vue from 'vue';
+// import App from './App.vue';
+
+// Vue.config.productionTip = false;
+
+// new Vue({
+//   render: h => h(App)
+// }).$mount('#app');
