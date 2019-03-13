c713a82361ae80b68c98d3ed608cf7d3433c76c0
David Nowinsky <david@nowinsky.net>
2019-03-13 20:00:09 +0100

    Convert Step vue component to TS

--- src/components/Step.vue
+++ src/components/Step.vue
@@ -20,64 +20,72 @@
     </div>
   </div>
 </template>
-<script>
+<script lang="ts">
 import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
+import { PipelineStep } from '../lib/steps'
 
-export default Vue.extend({
-  name: 'step',
-  props: {
-    isFirst: Boolean,
-    isLast: Boolean,
-    isActive: {
-      type: Boolean,
-      default: false
-    },
-    isDisabled: {
-      type: Boolean,
-      default: false,
-    },
-    step: Object,
-  },
-  computed: {
-    classDot() {
-      return {
-        'query-pipeline-queue__dot': true,
-        'query-pipeline-queue__dot--active': this.isActive,
-        'query-pipeline-queue__dot--disabled': this.isDisabled,
-      };
-    },
-    classDotInk() {
-      return {
-        'query-pipeline-queue__dot-ink': true,
-        'query-pipeline-queue__dot-ink--active': this.isActive,
-        'query-pipeline-queue__dot-ink--disabled': this.isDisabled,
-      };
-    },
-    classStep() {
-      return {
-        'query-pipeline-step': true,
-        'query-pipeline-step--disabled': this.isDisabled,
-      }
-    },
-    firstStrokeClass() {
-      return {
-        'query-pipeline-queue__stroke': true,
-        'query-pipeline-queue__stroke--hidden': this.isFirst,
-      };
-    },
-    lastStrokeClass() {
+@Component({
+  name: 'step'
+})
+export default class Step extends Vue {
+  @Prop()
+  isFirst: boolean = false
+
+  @Prop()
+  isLast: boolean = false
+
+  @Prop()
+  isActive: boolean = false
+
+  @Prop()
+  isDisabled: boolean = false
+
+  @Prop()
+  step: PipelineStep | undefined
+
+  get classDot() {
+    return {
+      'query-pipeline-queue__dot': true,
+      'query-pipeline-queue__dot--active': this.isActive,
+      'query-pipeline-queue__dot--disabled': this.isDisabled,
+    };
+  }
+
+  get classDotInk() {
+    return {
+      'query-pipeline-queue__dot-ink': true,
+      'query-pipeline-queue__dot-ink--active': this.isActive,
+      'query-pipeline-queue__dot-ink--disabled': this.isDisabled,
+    };
+  }
+
+  get classStep() {
+    return {
+      'query-pipeline-step': true,
+      'query-pipeline-step--disabled': this.isDisabled,
+    }
+  }
+
+  get firstStrokeClass() {
+    return {
+      'query-pipeline-queue__stroke': true,
+      'query-pipeline-queue__stroke--hidden': this.isFirst,
+    };
+  }
+
+  get lastStrokeClass() {
       return {
         'query-pipeline-queue__stroke': true,
         'query-pipeline-queue__stroke--hidden': this.isLast,
       };
-    },
-  },
-  methods: {
-    select() {
-      this.$emit('selectedStep');
-    },
-  },
-});
+    }
+
+
+  select() {
+    this.$emit('selectedStep');
+  }
+}
 </script>
 <style lang="scss" scoped>
   @import '../styles/Steps';
