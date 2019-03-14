da52cb7925747f88aa202bcc14884ba7094a7762
David Nowinsky <david@nowinsky.net>
2019-03-14 10:20:58 +0100

    Convert Pipeline to TS

--- src/components/Pipeline.vue
+++ src/components/Pipeline.vue
@@ -25,73 +25,84 @@
     />
   </div>
 </template>
-<script>
-import DomainSelector from './DomainSelector.vue';
-import Step from './Step.vue';
 
-export default {
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
+
+import { PipelineStep } from '@/lib/steps'
+import DomainSelector from './DomainSelector.vue'
+import Step from './Step.vue'
+
+@Component({
   name: 'pipeline',
   components: {
     DomainSelector,
     Step,
   },
-  props: {
-    steps: Array,
-    domainsList: Array,
-  },
-  data() {
-    return {
-      selectedStep: -1,
-      activePipeline: [],
-      disabledPipeline: [],
-    };
-  },
-  computed: {
-    selectedIndex() {
-      if (this.selectedStep < 0) {
-        return this.stepsWithoutDomain.length -1;
-      }
+})
+export default class Pipeline extends Vue {
+  @Prop()
+  steps!: Array<PipelineStep>
 
-      return this.selectedStep;
-    },
-    isEmpty() {
-      return this.stepsWithoutDomain.length === 0;
-    },
-    stepDomain() {
-      return this.steps[0];
-    },
-    stepsWithoutDomain() {
-      return this.steps.slice(1).concat(this.disabledPipeline);
-    },
-  },
-  methods: {
-    isDisabled(index) {
-      if (this.selectedStep < 0) {
-        return false;
-      }
-      return index > this.selectedStep;
-    },
-    selectStep(index) {
-      let pipeline = [];
-      this.selectedStep = index;
+  @Prop()
+  domainsList!: Array<string>
 
-      this.activePipeline = this.stepsWithoutDomain.slice(0, this.selectedStep + 1);
+  selectedStep: number = -1
+  activePipeline: Array<PipelineStep> = []
+  disabledPipeline: Array<PipelineStep> = []
 
-      // Separate steps that are after the selected one to keep in memory
-      this.disabledPipeline = this.stepsWithoutDomain.slice(this.selectedStep + 1, this.stepsWithoutDomain.length);
+  get selectedIndex() {
+    if (this.selectedStep < 0) {
+      return this.stepsWithoutDomain.length -1
+    }
 
-      // We emit the active pipeline with the step 0 (select domain) to the parent
-      this.$emit('selectedPipeline', pipeline.concat(this.stepDomain, this.activePipeline));
-    },
-    resetSelectedStep() {
-      this.selectedStep = -1;
-    },
-    updateDomain(newDomain) {
-      return newDomain; // Emit an event
-    },
-  },
-};
+    return this.selectedStep
+  }
+
+  get isEmpty() {
+    return this.stepsWithoutDomain.length === 0
+  }
+
+  get stepDomain() {
+    return this.steps[0]
+  }
+
+  get stepsWithoutDomain() {
+    return this.steps.slice(1).concat(this.disabledPipeline)
+  }
+
+  isDisabled(index: number) {
+    if (this.selectedStep < 0) {
+      return false;
+    }
+    return index > this.selectedStep
+  }
+
+  selectStep(index: number) {
+    let pipeline: Array<PipelineStep> = []
+    this.selectedStep = index
+
+    this.activePipeline = this.stepsWithoutDomain.slice(0, this.selectedStep + 1)
+
+    // Separate steps that are after the selected one to keep in memory
+    this.disabledPipeline = this.stepsWithoutDomain.slice(this.selectedStep + 1, this.stepsWithoutDomain.length)
+
+    // We emit the active pipeline with the step 0 (select domain) to the parent
+    this.$emit('selectedPipeline', pipeline.concat(this.stepDomain, this.activePipeline))
+  }
+
+  resetSelectedStep() {
+    this.selectedStep = -1
+  }
+
+  updateDomain(newDomain: string) {
+    return newDomain // Emit an event
+  }
+}
 </script>
+
 <style lang="scss" scoped>
   @import '../styles/Pipeline';
 </style>
