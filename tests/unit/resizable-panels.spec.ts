1b58324d2a29ded8b9b0a55677c36f4811a1bab7
Jordan Lorho <jordan.lorho@gmail.com>
2019-04-17 12:15:43 +0200

    tests :: resize behavior and ratio div

--- tests/unit/resizable-panels.spec.ts
+++ tests/unit/resizable-panels.spec.ts
@@ -34,4 +34,59 @@ describe('Resizable Panels', () => {
     expect(wrapper.exists()).toBeTruthy();
     expect(wrapperRightPanelChild.exists()).toBeTruthy();
   });
+
+  describe('left panel', () => {
+    it('should have the default ratio apply to its width', () => {
+      const wrapper = shallowMount(ResizablePanels);
+      const panels: WrapperArray<any> = wrapper.findAll('.resizable-panels__panel');
+
+      expect(panels.at(0).element.style.width).toEqual('40%');
+    });
+  });
+
+  describe('right panel', () => {
+    it('should have the default ratio apply to its width', () => {
+      const wrapper = shallowMount(ResizablePanels);
+      const panels: WrapperArray<any> = wrapper.findAll('.resizable-panels__panel');
+
+      expect(panels.at(1).element.style.width).toEqual('60%');
+    });
+  });
+
+  describe('when resizing', () => {
+    it('should know that is being resized', () => {
+      const wrapper = shallowMount(ResizablePanels, {
+        attachToDocument: true,
+      });
+      const resizerWrapper = wrapper.find('.resizable-panels__resizer');
+
+      resizerWrapper.trigger('mousedown');
+      wrapper.trigger('mousemove', { movementX: 10 });
+      expect(wrapper.vm.$data.isResized).toBeTruthy();
+    });
+
+    it('should change the ratio', () => {
+      const wrapper = shallowMount(ResizablePanels, {
+        attachToDocument: true,
+      });
+      const resizerWrapper = wrapper.find('.resizable-panels__resizer');
+      const fakeGetBoundingClientRect = () => {
+        return {
+          bottom: 0,
+          left: 0,
+          right: 0,
+          top: 0,
+          width: 1000,
+          height: 1000,
+        };
+      };
+      wrapper.vm.$el.getBoundingClientRect = jest.fn(fakeGetBoundingClientRect);
+
+      resizerWrapper.trigger('mousedown');
+      wrapper.trigger('mousemove', { movementX: 100 });
+      // baseRatio: 0.4
+      // If I move my mouse of 100px - that is 1/10 compare to its width - then I increase my ratio by 0.1
+      expect(wrapper.vm.$data.ratio).toEqual(0.5);
+    });
+  });
 });
