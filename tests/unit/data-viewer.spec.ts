9fdaeed24cbf0b1b32c1adae1d9ca6a88c945052
Jordan Lorho <jordan.lorho@gmail.com>
2019-04-24 12:57:14 +0200

    add :: test when a column is already selected

--- tests/unit/data-viewer.spec.ts
+++ tests/unit/data-viewer.spec.ts
@@ -94,6 +94,29 @@ describe('Data Viewer', () => {
         firstHeaderCellWrapper.trigger('click');
         expect(firstHeaderCellWrapper.classes()).toContain('data-viewer__header-cell--active');
       });
+
+      it('should remove selection on an active column', () => {
+        const dataset = [
+          { columnA: 'value1', columnB: 'value2', columnC: 'value3' },
+          { columnA: 'value4', columnB: 'value5', columnC: 'value6' },
+          { columnA: 'value7', columnB: 'value8', columnC: 'value9' },
+          { columnA: 'value10', columnB: 'value11', columnC: 'value12' },
+          { columnA: 'value13', columnB: 'value14', columnC: 'value15' },
+        ];
+        const wrapper = shallowMount(DataViewer, {
+          propsData: {
+            dataset: dataset,
+          },
+        });
+
+        const firstHeaderCellWrapper = wrapper.find('.data-viewer__header-cell');
+        // Select Column
+        firstHeaderCellWrapper.trigger('click');
+        expect(firstHeaderCellWrapper.classes()).toContain('data-viewer__header-cell--active');
+        // Unselect Column
+        firstHeaderCellWrapper.trigger('click');
+        expect(firstHeaderCellWrapper.classes()).not.toContain('data-viewer__header-cell--active');
+      });
     });
   });
 
