0b48557b2daf1207ec9a98a35ee7d78c45f58309
Jordan Lorho <jordan.lorho@gmail.com>
2019-04-24 12:20:30 +0200

    adapt :: App.vue example to use the new component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Pipeline :steps="steps" :domains-list="domainsList" @selectedPipeline="setSteps"/>
+    <ResizablePanels>
+      <Pipeline
+        slot="left-panel"
+        :steps="steps"
+        :domains-list="domainsList"
+        @selectedPipeline="setSteps"
+      />
+      <DataViewer slot="right-panel" :dataset="dataset"/>
+    </ResizablePanels>
   </div>
 </template>
 
 <script lang="ts">
+import _ from 'lodash';
 import { Component, Vue } from 'vue-property-decorator';
+import DataViewer from './components/DataViewer.vue';
 import Pipeline from './components/Pipeline.vue';
 import { PipelineStep } from '@/lib/steps';
 
+import fakeDataset from './fake_dataset.json';
+
 @Component({
   components: {
+    DataViewer,
     Pipeline,
   },
 })
@@ -21,7 +33,10 @@ export default class App extends Vue {
     { name: 'filter', column: 'my-column', value: 42, operator: 'eq' },
     { name: 'rename', oldname: 'my-column', newname: 'new-name' },
   ];
+
   domainsList = ['horizontal_barchart', 'bubble_chart', 'cities_data'];
+
+  dataset = fakeDataset;
 
   setSteps(pipeline: Array<PipelineStep>) {
     this.steps = pipeline;
@@ -30,12 +45,16 @@ export default class App extends Vue {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
 }
 </style>
