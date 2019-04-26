d64abc398cadeebc63da52fe5e92091a1b3397fa
Adrien Di Mascio <adrien.dimascio@logilab.fr>
2019-04-26 18:49:23 +0200

    [playground] new DataSetColum type
    Introduce a new `DataSetColumn` type that hold both column name and
    column type information. We'll probably decide to add extra metadata
    later on.
    
    Use this type instead of raw column names and update code / html
    template accordingly.

--- playground/App.vue
+++ playground/App.vue
@@ -10,7 +10,7 @@
       <table slot="right-panel">
         <thead>
           <tr>
-            <th :key="col" v-for="col in dataset.columns">{{ col }}</th>
+            <th :key="col" v-for="col in dataset.columns">{{ col.name }}</th>
           </tr>
         </thead>
         <tbody>
@@ -32,17 +32,44 @@ import { mongodb, pipeline, domains } from './config';
 
 const mongo36translator = getTranslator('mongo36');
 
+type DataSetColumn = {
+  name: string;
+  type?: 'integer' | 'float' | 'boolean' | 'string' | 'date' | 'object';
+};
+
 type DataSet = {
-  columns: Array<string>;
+  columns: Array<DataSetColumn>;
   data: Array<Array<any>>;
+};
+
+/**
+ * implement set union
+ * > setUnion(new Set([1, 2, 3]), new Set([2, 4, 6]))
+ * Set([1, 2, 3, 4, 6])
+ */
+function setUnion<T>(set1: Set<T>, set2: Set<T>): Set<T> {
+  const result = new Set(set1);
+  set2.forEach(val => result.add(val));
+  return result;
 }
 
-function formatDataset(results: Array<object>): DataSet {
+
+/**
+ * transform a mongo resultset (i.e. a list of json documents) into
+ * a `DataSet` structure
+ */
+function formatDataset(results: Array<{ [k: string]: any }>): DataSet {
   const dataset: DataSet = { columns: [], data: [] };
   if (results.length) {
-    dataset.columns = Object.keys(results[0]);
+    // each document migh have a different set of keys therefore we need
+    // to loop over all documents and make the union of all keys
+    const colnames = results
+      .map(row => new Set(Object.keys(row)))
+      .reduce(setUnion, new Set());
+    // transform set of names to list of DataSetColumn objects
+    dataset.columns = Array.from(colnames).map(name => ({ name }));
     for (const row of results) {
-      dataset.data.push(dataset.columns.map(colname => row[colname]));
+      dataset.data.push(dataset.columns.map(coldef => row[coldef.name] || null));
     }
   }
   return dataset;
@@ -59,7 +86,7 @@ export default class App extends Vue {
   domainsList = domains;
   code: string = JSON.stringify(mongo36translator.translate(pipeline), null, 2);
   dataset: DataSet = {
-    columns: ['col1', 'col2', 'col3'],
+    columns: [{ name: 'col1' }, { name: 'col2' }, { name: 'col3' }],
     data: [
       [12, 13, 14],
       [15, 19, 15],
