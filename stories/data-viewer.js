a611cc15722a88a55d52db02d439f3d25aa5feea
Adrien Di Mascio <adrien.dimascio@logilab.fr>
2019-04-30 23:36:54 +0200

    [DataViewer] use the new DataSet type
    make the `DataViewer` component expect a `DataSet` object instead of a
    simple array of documents.
    
    Related to https://github.com/ToucanToco/vue-query-builder/issues/26

--- stories/data-viewer.js
+++ stories/data-viewer.js
@@ -1,29 +1,37 @@
-import { storiesOf } from '@storybook/vue';
+import {storiesOf} from '@storybook/vue';
 
-import { DataViewer } from '../dist/storybook/components';
+import {DataViewer} from '../dist/storybook/components';
 
 const stories = storiesOf('DataViewer', module);
 
 stories.add('empty', () => ({
-  components: { DataViewer },
-  template: `
+                       components: {DataViewer},
+                       template: `
     <data-viewer>
     </data-viewer>
   `,
-}));
+                     }));
 
 stories.add('simple', () => ({
-  components: { DataViewer },
+  components: {DataViewer},
   props: {
     dataset: {
       default() {
-        return [
-          { columnA: 'value1', columnB: 'value2', columnC: 'value3' },
-          { columnA: 'value4', columnB: 'value5', columnC: 'value6' },
-          { columnA: 'value7', columnB: 'value8', columnC: 'value9' },
-          { columnA: 'value10', columnB: 'value11', columnC: 'value12' },
-          { columnA: 'value13', columnB: { obj: 'value14' }, columnC: undefined },
-        ];
+        return {
+          columns:
+              [
+                {name: 'columnA'},
+                {name: 'columnB'},
+                {name: 'columnC'},
+              ],
+              data: [
+                ['value1', 'value2', 'value3'],
+                ['value4', 'value5', 'value6'],
+                ['value7', 'value8', 'value9'],
+                ['value10', 'value11', 'value12'],
+                ['value10', {obj: 'value14'}, undefined],
+              ]
+        }
       },
     },
   },
