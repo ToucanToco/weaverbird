20e97e08002fe5de9f22c67261d776063b953633
Adrien Di Mascio <adrien.dimascio@logilab.fr>
2019-05-07 12:04:42 +0200

    [playground] use DataViewer instead of a raw HTML table

--- playground/App.vue
+++ playground/App.vue
@@ -7,18 +7,7 @@
         :domains-list="domainsList"
         @selectedPipeline="setSteps"
       />
-      <table slot="right-panel">
-        <thead>
-          <tr>
-            <th :key="col" v-for="col in dataset.columns">{{ col.name }}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr :key="rowidx" v-for="(row, rowidx) in dataset.data">
-            <td :key="colidx" v-for="(value, colidx) in row">{{ value }}</td>
-          </tr>
-        </tbody>
-      </table>
+      <DataViewer slot="right-panel" :dataset="dataset"/>
     </ResizablePanels>
     <pre><code>{{ code }}</code></pre>
   </div>
@@ -27,7 +16,9 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { PipelineStep } from '@/lib/steps';
-import { Pipeline, ResizablePanels, getTranslator } from '../dist/vue-query-builder.common.js';
+import { DataSet } from '@/lib/dataset';
+import { mongoResultsToDataset } from '@/lib/dataset/mongo';
+import { DataViewer, Pipeline, ResizablePanels, getTranslator } from '../dist/vue-query-builder.common.js';
 
 const mongo36translator = getTranslator('mongo36');
 const pipeline: Array<PipelineStep> = [
@@ -36,51 +27,9 @@ const pipeline: Array<PipelineStep> = [
   { name: 'filter', column: 'id', value: 'yolo', operator: 'eq' },
 ];
 
-type DataSetColumn = {
-  name: string;
-  type?: 'integer' | 'float' | 'boolean' | 'string' | 'date' | 'object';
-};
-
-type DataSet = {
-  columns: Array<DataSetColumn>;
-  data: Array<Array<any>>;
-};
-
-/**
- * implement set union
- * > setUnion(new Set([1, 2, 3]), new Set([2, 4, 6]))
- * Set([1, 2, 3, 4, 6])
- */
-function setUnion<T>(set1: Set<T>, set2: Set<T>): Set<T> {
-  const result = new Set(set1);
-  set2.forEach(val => result.add(val));
-  return result;
-}
-
-
-/**
- * transform a mongo resultset (i.e. a list of json documents) into
- * a `DataSet` structure
- */
-function formatDataset(results: Array<{ [k: string]: any }>): DataSet {
-  const dataset: DataSet = { columns: [], data: [] };
-  if (results.length) {
-    // each document migh have a different set of keys therefore we need
-    // to loop over all documents and make the union of all keys
-    const colnames = results
-      .map(row => new Set(Object.keys(row)))
-      .reduce(setUnion, new Set());
-    // transform set of names to list of DataSetColumn objects
-    dataset.columns = Array.from(colnames).map(name => ({ name }));
-    for (const row of results) {
-      dataset.data.push(dataset.columns.map(coldef => row[coldef.name] || null));
-    }
-  }
-  return dataset;
-}
-
 @Component({
   components: {
+    DataViewer,
     Pipeline,
     ResizablePanels,
   },
@@ -90,7 +39,7 @@ export default class App extends Vue {
   domainsList = [];
   code: string = JSON.stringify(mongo36translator.translate(pipeline), null, 2);
   dataset: DataSet = {
-    columns: [{ name: 'col1' }, { name: 'col2' }, { name: 'col3' }],
+    headers: [{ name: 'col1' }, { name: 'col2' }, { name: 'col3' }],
     data: [
       [12, 13, 14],
       [15, 19, 15],
@@ -120,7 +69,7 @@ export default class App extends Vue {
       }
     })
       .then(res => res.json())
-      .then(dataset => this.dataset = formatDataset(dataset));
+      .then(dataset => this.dataset = mongoResultsToDataset(dataset));
   }
 }
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 export { mongoToPipe } from './lib/pipeline';
 export { getTranslator } from './lib/translators';
 
+import DataViewer from './components/DataViewer.vue';
 import Pipeline from './components/Pipeline.vue';
 import ResizablePanels from './components/ResizablePanels.vue';
-export { Pipeline, ResizablePanels };
+export { DataViewer, Pipeline, ResizablePanels };
