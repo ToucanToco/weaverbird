43ddaf3f89306e1726e8d156dfd0fec3d6d30e06
Adrien Di Mascio <adrien.dimascio@logilab.fr>
2019-05-10 23:03:09 +0200

    [lib] expose filterOutDomain
    `filterOutDomain` extracts the requested domain from the first step and
    return the rest of the pipeline unchanged. This is useful in the GUI
    where the domain steps is handled differently from the rest of the
    pipeline. It's also useful in backends such as mongo where the first
    step corresponds to the collection that has to be queried.

--- tests/unit/pipebuild.spec.ts
+++ tests/unit/pipebuild.spec.ts
@@ -1,5 +1,6 @@
+import { Pipeline } from '@/lib/steps';
 import { MongoStep } from '@/lib/translators/mongo';
-import { mongoToPipe } from '@/lib/pipeline';
+import { filterOutDomain, mongoToPipe } from '@/lib/pipeline';
 
 describe('Pipebuild translator', () => {
   it('generate domain step', () => {
@@ -128,5 +129,26 @@ describe('Pipebuild translator', () => {
         },
       },
     ]);
+  });
+});
+
+describe('helpers tests', () => {
+  it('should extract domain and subpipelines correctly', () => {
+    const pipeline: Pipeline = [
+      { name: 'domain', domain: 'test_cube' },
+      { name: 'select', columns: ['Region'] },
+    ];
+    const { domain, pipeline: subpipeline } = filterOutDomain(pipeline);
+    expect(domain).toEqual('test_cube');
+    expect(subpipeline).toEqual([{ name: 'select', columns: ['Region'] }]);
+  });
+
+  it('should raise an error if pipeline is empty', () => {
+    expect(() => filterOutDomain([])).toThrow();
+  });
+
+  it('should raise an error if first step is not a domain step', () => {
+    const pipeline: Pipeline = [{ name: 'select', columns: ['Region'] }];
+    expect(() => filterOutDomain(pipeline)).toThrow();
   });
 });
