7deddb720b7b3160243baa931bf5c8affc4b7468
Adrien Di Mascio <adrien.dimascio@logilab.fr>
2019-05-11 20:00:47 +0200

    [store] adapt unit tests to use Vuex and store

--- tests/unit/pipeline.spec.ts
+++ tests/unit/pipeline.spec.ts
@@ -1,13 +1,21 @@
-import { shallowMount } from '@vue/test-utils';
-import Pipeline from '@/components/Pipeline.vue';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import { Pipeline } from '@/lib/steps';
+import { setupStore } from '@/store';
+import PipelineComponent from '@/components/Pipeline.vue';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
 
 describe('Pipeline.vue', () => {
   it('renders steps', () => {
-    const wrapper = shallowMount(Pipeline, {
-      propsData: {
-        steps: [{ name: 'domain' }, { name: 'rename' }, { name: 'group' }],
-      },
-    });
+    const pipeline: Pipeline = [
+      { name: 'domain', domain: 'GoT' },
+      { name: 'replace', search_column: 'characters', to_replace: [['Snow', 'Targaryen']] },
+      { name: 'sort', columns: ['death'] },
+    ];
+    const store = setupStore({ pipeline });
+    const wrapper = shallowMount(PipelineComponent, { store, localVue });
     const steps = wrapper.findAll('step-stub');
     // first step is the domain step and is processed separately by the
     // Pipeline component. There should only be the 2 generated <Step>
@@ -15,14 +23,14 @@ describe('Pipeline.vue', () => {
     expect(steps.length).toBe(2);
     const [step1, step2] = steps.wrappers.map(stub => stub.props());
     expect(step1).toEqual({
-      step: { name: 'rename' },
+      step: pipeline[1],
       isActive: true,
       isDisabled: false,
       isFirst: true,
       isLast: false,
     });
     expect(step2).toEqual({
-      step: { name: 'group' },
+      step: pipeline[2],
       isActive: true,
       isDisabled: false,
       isFirst: false,
