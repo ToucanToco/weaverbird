71aad978456492cd7915b3fe1b29713549eb1955
Adrien Di Mascio <adrien.dimascio@logilab.fr>
2019-05-15 14:33:02 +0200

    [storybook] use our Vuex store

--- stories/data-viewer.js
+++ stories/data-viewer.js
@@ -1,44 +1,40 @@
-import {storiesOf} from '@storybook/vue';
+import { storiesOf } from '@storybook/vue';
 
-import {DataViewer} from '../dist/storybook/components';
-
+import { DataViewer, setupStore } from '../dist/storybook/components';
 const stories = storiesOf('DataViewer', module);
 
 stories.add('empty', () => ({
-                       components: {DataViewer},
-                       template: `
+  store: setupStore({}, [], true),
+  components: { DataViewer },
+  template: `
     <data-viewer>
     </data-viewer>
   `,
-                     }));
+}));
 
 stories.add('simple', () => ({
-                        components: {DataViewer},
-                        props: {
-                          dataset: {
-                            default() {
-                              return {
-                                headers:
-                                    [
-                                      {name: 'columnA'},
-                                      {name: 'columnB'},
-                                      {name: 'columnC'},
-                                    ],
-                                    data: [
-                                      ['value1', 'value2', 'value3'],
-                                      ['value4', 'value5', 'value6'],
-                                      ['value7', 'value8', 'value9'],
-                                      ['value10', 'value11', 'value12'],
-                                      ['value10', {obj: 'value14'}, null],
-                                    ]
-                              }
-                            },
-                          },
-                        },
-                        template: `
+  store: setupStore({
+    dataset: {
+      headers:
+        [
+          { name: 'columnA' },
+          { name: 'columnB' },
+          { name: 'columnC' },
+        ],
+      data: [
+        ['value1', 'value2', 'value3'],
+        ['value4', 'value5', 'value6'],
+        ['value7', 'value8', 'value9'],
+        ['value10', 'value11', 'value12'],
+        ['value10', { obj: 'value14' }, null],
+      ]
+    },
+  }, [], true),
+  components: { DataViewer },
+  template: `
       <data-viewer
         :dataset="dataset"
       >
       </data-viewer>
     `,
-                      }));
+}));
