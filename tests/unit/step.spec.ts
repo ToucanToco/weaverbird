f88f14c574d7584b4aa6b62aabc75dd7c63efade
vincent_d <vincent.destrait@toucantoco.com>
2019-05-31 13:04:35 +0200

    Add edit click event test for Step.vue

--- tests/unit/step.spec.ts
+++ tests/unit/step.spec.ts
@@ -8,8 +8,8 @@ import Step from '@/components/Step.vue';
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
-describe('Pipeline.vue', () => {
-  it('renders steps', () => {
+describe('Step.vue', () => {
+  it('should delete a step when clicking on the trash icon', () => {
     const pipeline: Pipeline = [
       { name: 'replace', search_column: 'characters', to_replace: [['Snow', 'Targaryen']] },
       { name: 'sort', columns: ['death'] },
@@ -17,7 +17,26 @@ describe('Pipeline.vue', () => {
     const store = setupStore({ pipeline });
     const wrapper = mount(PipelineComponent, { store, localVue });
     const step = wrapper.find(Step);
-    step.find('i[class="fas fa-trash-alt"]').trigger('click');
+    step.find('.fa-trash-alt').trigger('click');
     expect(store.state.pipeline.length).toEqual(1);
+  });
+
+  it('should toggle the edit mode when clicking on the edit icon and emit editStep', () => {
+    const pipeline: Pipeline = [
+      { name: 'domain', domain: 'GoT' },
+      { name: 'replace', search_column: 'characters', to_replace: [['Snow', 'Targaryen']] },
+      { name: 'rename', oldname: 'region', newname: 'kingdom' },
+      { name: 'sort', columns: ['death'] },
+    ];
+    const store = setupStore({ pipeline, isEditingStep: false });
+    const wrapper = mount(PipelineComponent, { store, localVue });
+    const stepsArray = wrapper.findAll(Step);
+    const renameStep = stepsArray.at(1);
+    renameStep.find('.fa-cog').trigger('click');
+    expect(renameStep.emitted()).toEqual({
+      editStep: [[{ name: 'rename', oldname: 'region', newname: 'kingdom' }, 2]],
+    });
+    // expect(store.state.isEditingStep).toBeTruthy();
+    // expect(store.state.selectedStepIndex).toEqual(0);
   });
 });
