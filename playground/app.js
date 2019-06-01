f980570f40a24bcab16f37b75de7c7e75aba24dc
Adrien Di Mascio <adrien.dimascio@logilab.fr>
2019-06-01 17:30:01 +0200

    [playground] deprecrate `playground:build-app`
    No need to use a build step anymore to build the playground application.
    It's now a pure JS module that uses the vqb+vue+vuex bundles directly.
    
    Just start the server and reload your web page!

---
+++ playground/app.js
@@ -0,0 +1,76 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { Vqb, filterOutDomain, getTranslator, mongoResultsToDataset, servicePluginFactory, setupStore } = vqb;
+
+const mongo36translator = getTranslator('mongo36');
+
+
+class MongoService {
+  constructor() {
+    this.translator = getTranslator('mongo36');
+  }
+
+  async listCollections() {
+    const response = await fetch('/collections');
+    return response.json();
+  }
+
+  async executePipeline(pipeline) {
+    const { domain, pipeline: subpipeline } = filterOutDomain(pipeline);
+    const rset = await this.executeQuery(this.translator.translate(subpipeline), domain);
+    return mongoResultsToDataset(rset);
+  }
+
+  async executeQuery(query, collection) {
+    const response = await fetch('/query', {
+      method: 'POST',
+      body: JSON.stringify({ query, collection }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return response.json();
+  }
+}
+
+const mongoservice = new MongoService();
+const mongoBackendPlugin = servicePluginFactory(mongoservice);
+const initialPipeline = [
+  { name: 'domain', domain: 'test-collection' },
+  { name: 'filter', column: 'Value4', value: 1, operator: 'gt' },
+  { name: 'replace', search_column: 'Value2', to_replace: [[2, 20], [13, 24]] },
+  { name: 'top', rank_on: 'Value2', sort: 'asc', limit: 3 },
+  {
+    name: 'pivot',
+    index: ['Groups'],
+    column_to_pivot: 'Label',
+    value_column: 'Value2',
+    agg_function: 'sum',
+  },
+];
+
+
+async function buildVueApp() {
+  Vue.use(Vuex);
+  // Vue.config.productionTip = false;
+  const store = setupStore({ pipeline: initialPipeline, currentDomain: 'test-collection' }, [
+    mongoBackendPlugin,
+  ]);
+
+  new Vue({
+    el: '#app',
+    components: { Vqb },
+    store,
+    computed: {
+      code: function () {
+        const query = mongo36translator.translate(this.$store.getters.activePipeline);
+        return JSON.stringify(query, null, 2);
+      }
+    }
+  });
+  const collections = await mongoservice.listCollections();
+  store.commit('setDomains', { domains: collections });
+  const dataset = await mongoservice.executePipeline(initialPipeline);
+  store.commit('setDataset', { dataset });
+}
+
+document.addEventListener('DOMContentLoaded', () => buildVueApp());
---
+++ playground/dist/vue-query-builder.browser.js
@@ -0,0 +1,1 @@
+../../dist/vue-query-builder.browser.js
