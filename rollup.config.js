b2f627e2b03257144e5f4d6d462176a1d5847a56
Adrien Di Mascio <adrien.dimascio@logilab.fr>
2019-06-01 17:30:01 +0200

    [playground] deprecrate `playground:build-app`
    No need to use a build step anymore to build the playground application.
    It's now a pure JS module that uses the vqb+vue+vuex bundles directly.
    
    Just start the server and reload your web page!

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import alias from 'rollup-plugin-alias';
 import commonjs from 'rollup-plugin-commonjs';
 import css from 'rollup-plugin-css-only';
 import json from 'rollup-plugin-json';
+import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript';
 import vue from 'rollup-plugin-vue';
@@ -16,8 +17,13 @@ export default {
     file: 'dist/vue-query-builder.esm.js',
     format: 'esm',
   },
+  {
+    file: 'dist/vue-query-builder.browser.js',
+    format: 'umd',
+    name: 'vqb',
+  }
   ],
-  external: ['vue'],
+  external: ['vue', 'vuex'],
   plugins: [
     resolve(),
     typescript({
@@ -34,6 +40,9 @@ export default {
     }),
     css({
       output: 'dist/vue-query-builder.css',
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
     }),
     vue({
       css: false,
