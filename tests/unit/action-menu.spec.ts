a9fc47d2c62dce6a69f6ad520ef00ff5aef005c4
Jordan Lorho <jordan.lorho@gmail.com>
2019-06-03 16:39:22 +0200

    [action-menu] :: create a ActionMenu Component and its  tests
    That component just implement the Popover component and add style to have a menu action on dataviewer when clicking on headers

---
+++ tests/unit/action-menu.spec.ts
@@ -0,0 +1,52 @@
+import { mount } from '@vue/test-utils';
+import ActionMenu from '@/components/ActionMenu.vue';
+
+describe('Action Menu', () => {
+  it('should instantiate with its popover hidden', () => {
+    const wrapper = mount(ActionMenu);
+
+    expect(wrapper.exists()).toBeTruthy();
+    expect(wrapper.classes()).toContain('popover');
+  });
+
+  it('should instantiate with the popover active', () => {
+    const wrapper = mount(ActionMenu, {
+      propsData: {
+        isActive: true,
+      },
+    });
+
+    expect(wrapper.classes()).toContain('popover--active');
+  });
+
+  describe('when click on "Rename column"', () => {
+    it('should have an "Rename column" action', () => {
+      const wrapper = mount(ActionMenu);
+      const actionsWrapper = wrapper.findAll('.action-menu__option');
+
+      expect(actionsWrapper.at(1).text()).toEqual('Rename column');
+    });
+
+    it('should emit an "actionClicked" event', () => {
+      const wrapper = mount(ActionMenu);
+      const actionsWrapper = wrapper.findAll('.action-menu__option');
+      actionsWrapper.at(1).trigger('click');
+
+      expect(wrapper.emitted().actionClicked).toBeTruthy();
+    });
+
+    it('shoud emit an "actionClicked" with the oldname already filled', () => {
+      const wrapper = mount(ActionMenu, {
+        propsData: {
+          columnName: 'dreamfall',
+        },
+      });
+      const actionsWrapper = wrapper.findAll('.action-menu__option');
+      actionsWrapper.at(1).trigger('click');
+
+      expect(wrapper.emitted().actionClicked[0]).toEqual([
+        { name: 'rename', oldname: 'dreamfall' },
+      ]);
+    });
+  });
+});
