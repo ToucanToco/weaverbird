8a5f43cab88de4209bc63757d1404acf50fd8196
vincent_d <vincent.destrait@toucantoco.com>
2019-06-03 09:27:40 +0200

    Remove unused lodash import in dataviewer

--- src/components/DataViewer.vue
+++ src/components/DataViewer.vue
@@ -30,7 +30,6 @@
   <div v-else>No data available</div>
 </template>
 <script lang="ts">
-import _ from 'lodash';
 import Vue from 'vue';
 import { Getter, Mutation, State } from 'vuex-class';
 
@@ -49,7 +48,7 @@ import { CATEGORY_BUTTONS } from './constants';
   name: 'data-viewer',
   components: {
     DataViewerCell,
-    ActionToolbar
+    ActionToolbar,
   },
 })
 export default class DataViewer extends Vue {
