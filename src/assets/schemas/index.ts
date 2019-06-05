3d8435318e4f3e2eb11171e301863118546dba36
vincent_d <vincent.destrait@toucantoco.com>
2019-06-05 18:02:57 +0200

    Create FillnaStep boilerplate

--- src/assets/schemas/index.ts
+++ src/assets/schemas/index.ts
@@ -1,2 +1,3 @@
 import renameStepSchema from './rename-step__schema.json';
-export { renameStepSchema };
+import fillnaStepSchema from './fillna-step__schema.json';
+export { renameStepSchema, fillnaStepSchema };
