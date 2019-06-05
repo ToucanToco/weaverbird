79a4ec3ee7af2429c2efaed4ba849a37ea5d04da
vincent_d <vincent.destrait@toucantoco.com>
2019-06-05 18:02:57 +0200

    Create FillnaStep boilerplate

---
+++ src/components/FormFillnaStep.vue
@@ -0,0 +1,139 @@
+<template>
+  <div>
+    <div class="step-edit-form">
+      <h1 class="step-edit-form__title">FILL NULL VALUES STEP</h1>
+    </div>
+    <WidgetAutocomplete
+      id="columnInput"
+      v-model="step.column"
+      name="Fill null values in:"
+      :options="columnNames"
+      @input="setSelectedColumns({ column: step.column })"
+      placeholder="Enter a column"
+    ></WidgetAutocomplete>
+    <WidgetInputText id="valueInput" v-model="step.value" name="With:" placeholder="Enter a value"></WidgetInputText>
+    <div class="widget-form-action">
+      <button
+        class="widget-form-action__button widget-form-action__button--validate"
+        @click="validateStep"
+      >OK</button>
+      <button
+        class="widget-form-action__button widget-form-action__button--cancel"
+        @click="cancelEdition"
+      >Cancel</button>
+    </div>
+    <div v-if="errors" class="errors">
+      <ul>
+        <li v-for="(error, index) in errors" :key="index">{{ error.dataPath }}: {{ error.message }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import _ from 'lodash';
+import { Component, Mixins, Prop, Watch } from 'vue-property-decorator';
+import FormMixin from '@/mixins/FormMixin.vue';
+import { Pipeline } from '@/lib/steps';
+import fillnaSchema from '@/assets/schemas/fillna-step__schema.json';
+import WidgetInputText from './WidgetInputText.vue';
+import WidgetAutocomplete from '@/components/WidgetAutocomplete.vue';
+import { Getter, Mutation, State } from 'vuex-class';
+
+interface FillnaStepConf {
+  column: string;
+  value: string;
+}
+
+@Component({
+  name: 'form-fillna-step',
+  components: {
+    WidgetAutocomplete,
+    WidgetInputText,
+  },
+})
+export default class FormRenameStep extends Mixins(FormMixin) {
+  @Prop({
+    type: Object,
+    default: () => ({
+      column: '',
+      value: '',
+    }),
+  })
+  initialValue!: FillnaStepConf;
+
+  @Prop({
+    type: Boolean,
+    default: () => true,
+  })
+  isStepCreation!: boolean;
+
+  step: FillnaStepConf = { ...this.initialValue };
+
+  @State pipeline!: Pipeline;
+  @State selectedStepIndex!: number;
+
+  @Mutation selectStep!: (payload: { index: number }) => void;
+  @Mutation setSelectedColumns!: (payload: { column: string }) => void;
+
+  @Getter selectedColumns!: string[];
+  @Getter columnNames!: string[];
+  @Getter computedActiveStepIndex!: number;
+
+  @Watch('selectedColumns')
+  onSelectedColumnsChanged(val: string[], oldVal: string[]) {
+    if (!_.isEqual(val, oldVal)) {
+      this.step.column = val[0];
+    }
+  }
+
+  created() {
+    this.schema = fillnaSchema;
+    this.setSelectedColumns({ column: this.initialValue.column });
+  }
+
+  validateStep() {
+    const ret = this.validator(this.step);
+    if (ret === false) {
+      this.errors = this.validator.errors;
+    } else {
+      this.errors = null;
+      this.$emit('formSaved', { name: 'fillna', ...this.step });
+    }
+  }
+
+  cancelEdition() {
+    this.$emit('cancel');
+    const idx = this.isStepCreation ? this.computedActiveStepIndex : this.selectedStepIndex + 1;
+    this.selectStep({ index: idx });
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import '../styles/_variables';
+
+.widget-form-action__button {
+  @extend %button-default;
+}
+
+.widget-form-action__button--validate {
+  background-color: $active-color;
+}
+
+.step-edit-form {
+  border-bottom: 1px solid $grey;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding-bottom: 20px;
+  margin: 10px 0 15px;
+  width: 100%;
+}
+
+.step-edit-form__title {
+  color: $base-color;
+  font-weight: 600;
+  font-size: 14px;
+  margin: 0;
+}
+</style>
