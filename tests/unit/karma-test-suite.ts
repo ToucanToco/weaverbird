02c5053e6c21decea9d8a8c361f8d2d1ab1dd89e
vincent_d <vincent.destrait@toucantoco.com>
2019-06-28 20:11:36 +0200

    Introduce a WidgetCheckbox component

--- tests/unit/karma-test-suite.ts
+++ tests/unit/karma-test-suite.ts
@@ -24,6 +24,7 @@ import './unpivot-step-form.spec';
 import './vqb.spec';
 import './widget-aggregation.spec';
 import './widget-autocomplete.spec';
+import './widget-checkbox.spec';
 import './widget-input-text.spec';
 import './widget-list.spec';
 import './widget-multiselect.spec.ts';
