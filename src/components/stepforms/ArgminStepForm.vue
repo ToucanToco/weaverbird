dd67ad36670f5d37484240cbfe02c03e11b83a6a
Adrien Di Mascio <adrien.dimascio@logilab.fr>
2019-06-30 21:57:32 +0200

    [forms] remove style from arg{max,min} steps
    The style is already defined in base form.

--- src/components/stepforms/ArgminStepForm.vue
+++ src/components/stepforms/ArgminStepForm.vue
@@ -46,27 +46,3 @@ export default class ArgminStepForm extends BaseStepForm<ArgminStep> {
   editedStepModel = argminSchema;
 }
 </script>
-<style lang="scss" scoped>
-@import '../../styles/_variables';
-.widget-form-action__button {
-  @extend %button-default;
-}
-.widget-form-action__button--validate {
-  background-color: $active-color;
-}
-.step-edit-form {
-  border-bottom: 1px solid $grey;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  padding-bottom: 20px;
-  margin: 10px 0 15px;
-  width: 100%;
-}
-.step-edit-form__title {
-  color: $base-color;
-  font-weight: 600;
-  font-size: 14px;
-  margin: 0;
-}
-</style>
