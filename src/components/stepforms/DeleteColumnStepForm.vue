66ad6774fd8e685031a900a856286967f7f5ef84
vincent_d <vincent.destrait@toucantoco.com>
2019-07-03 19:03:14 +0200

    Replace autocomplete by multiselect in delete step form

--- src/components/stepforms/DeleteColumnStepForm.vue
+++ src/components/stepforms/DeleteColumnStepForm.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <step-form-title :title="title"></step-form-title>
-    <WidgetAutocomplete
-      id="columnInput"
-      v-model="column"
-      name="Delete column..."
+    <WidgetMultiselect
+      id="columnsInput"
+      v-model="editedStep.columns"
+      name="Delete columns..."
       :options="columnNames"
-      @input="setSelectedColumns({ column })"
-      placeholder="Enter a column"
-    ></WidgetAutocomplete>
+      @input="setSelectedColumns({ column: editedStep.columns[editedStep.columns.length-1] })"
+      placeholder="Add columns"
+    ></WidgetMultiselect>
     <step-form-buttonbar :errors="errors" :cancel="cancelEdition" :submit="submit"></step-form-buttonbar>
   </div>
 </template>
@@ -16,7 +16,7 @@
 <script lang="ts">
 import { Prop } from 'vue-property-decorator';
 import { StepFormComponent } from '@/components/formlib';
-import WidgetAutocomplete from '@/components/stepforms/WidgetAutocomplete.vue';
+import WidgetMultiselect from '@/components/stepforms/WidgetMultiselect.vue';
 import BaseStepForm from './StepForm.vue';
 import { DeleteStep } from '@/lib/steps';
 
@@ -24,40 +24,13 @@ import { DeleteStep } from '@/lib/steps';
   vqbstep: 'delete',
   name: 'delete-step-form',
   components: {
-    WidgetAutocomplete,
+    WidgetMultiselect,
   },
 })
 export default class DeleteStepForm extends BaseStepForm<DeleteStep> {
-  @Prop({ type: Object, default: () => ({ name: 'delete', columns: [''] }) })
+  @Prop({ type: Object, default: () => ({ name: 'delete', columns: [] }) })
   initialStepValue!: DeleteStep;
 
-  readonly title: string = 'Delete Column';
-
-  // Only manage the deletion of 1 column at once at this stage
-  column: string = this.initialStepValue.columns[0];
-
-  stepToValidate() {
-    return { name: 'delete', column: this.column };
-  }
-
-  rebuildStep() {
-    // HACK: the `as 'delete'` is here to please TypeScript which cannot
-    // assign type "string" of `"delete"` to type "delete".
-    // check https://github.com/microsoft/TypeScript/issues/11465
-    return { name: 'delete' as 'delete', columns: [this.column] };
-  }
-
-  get stepSelectedColumn() {
-    return this.column;
-  }
-
-  set stepSelectedColumn(colname: string | null) {
-    if (colname === null) {
-      throw new Error('should not try to set null on delete "column" field');
-    }
-    if (colname !== null) {
-      this.column = colname;
-    }
-  }
+  readonly title: string = 'Delete Columns';
 }
 </script>
