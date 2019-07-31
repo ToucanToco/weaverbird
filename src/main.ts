1f6e3af654b1a9d6f355fdc14e416e34cda91fb3
Adrien Di Mascio <adrien.dimascio@logilab.fr>
2019-07-31 21:40:47 +0200

    [package] no need to export internal components
    `DataViewer`, `ResizablePanels` and `Pipeline` are not meant to be used
    directly outside this package

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,5 @@ export { servicePluginFactory } from '@/store/backend-plugin';
 export { setupStore } from '@/store';
 
 // export Vue components
-import DataViewer from './components/DataViewer.vue';
-import Pipeline from './components/Pipeline.vue';
-import ResizablePanels from './components/ResizablePanels.vue';
 import Vqb from './components/Vqb.vue';
-export { DataViewer, Pipeline, ResizablePanels, Vqb };
+export { Vqb };
