89954c2a1831fb597f19dd81fa1707bdd8ac1295
Pierre Gonzalez <pierre.gonzalez@toucantoco.com>
2019-09-12 18:25:19 +0200

    add tests

--- tests/unit/data-viewer.spec.ts
+++ tests/unit/data-viewer.spec.ts
@@ -19,6 +19,22 @@ describe('Data Viewer', () => {
     expect(wrapper.text()).toEqual('No data available');
   });
 
+  it('should display a loader spinner when data is loading', () => {
+    const wrapper = shallowMount(DataViewer, { store: setupStore({
+      isLoading: true
+    }), localVue });
+    const wrapperLoaderSpinner = wrapper.find('.data-viewer-loader-spinner');
+    expect(wrapperLoaderSpinner.exists()).toBeTruthy();
+  });
+
+  it('should not display data viewer table container while data is loading', () => {
+    const wrapper = shallowMount(DataViewer, { store: setupStore({
+      isLoading: true
+    }), localVue });
+    const wrapperDataViewerContainer = wrapper.find('.data-viewer-container');
+    expect(wrapperDataViewerContainer.exists()).toBeFalsy();
+  });
+
   describe('header', () => {
     it('should have one row', () => {
       const store = setupStore({
