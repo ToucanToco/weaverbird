5082f030d88b0e821e6aedd13a2f3a7f212d59bd
Pierre Gonzalez <pierre.gonzalez@toucantoco.com>
2019-10-09 16:31:08 +0200

    reset all gh-pages branch files

--- stories/resizable-panel.js
+++
@@ -1,9 +0,0 @@
-import { ResizablePanels } from '../dist/storybook/components';
-import { storiesOf } from '@storybook/vue';
-
-const stories = storiesOf('ResizablePanels', module);
-
-stories.add('default', () => ({
-  components: { ResizablePanels },
-  template: '<resizable-panels style="height: 600px;"></resizable-panels>',
-}));
--- tests/unit/utils.ts
+++
@@ -1,9 +0,0 @@
-import Vuex from 'vuex';
-import { registerModule } from '@/store';
-
-export function setupMockStore(initialState: object = {}, plugins: any[] = []) {
-  const store = new Vuex.Store({ plugins });
-  registerModule(store, initialState);
-
-  return store;
-}
