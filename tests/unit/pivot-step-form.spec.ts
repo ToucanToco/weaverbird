efe2683cb57daa8382a46e837f2c76e107693637
Adrien Di Mascio <adrien.dimascio@toucantoco.com>
2020-01-18 23:31:50 +0100

    [tests] use test runner in pivotstep-form.spec.ts

--- tests/unit/pivot-step-form.spec.ts
+++ tests/unit/pivot-step-form.spec.ts
@@ -1,160 +1,46 @@
-import { mount, shallowMount, createLocalVue } from '@vue/test-utils';
-import Vuex, { Store } from 'vuex';
-
 import PivotStepForm from '@/components/stepforms/PivotStepForm.vue';
 import { VQBnamespace } from '@/store';
 
-import { setupMockStore, RootState, ValidationError } from './utils';
-
-const localVue = createLocalVue();
-localVue.use(Vuex);
+import { setupMockStore, BasicStepFormTestRunner } from './utils';
 
 describe('Pivot Step Form', () => {
-  let emptyStore: Store<RootState>;
-  beforeEach(() => {
-    emptyStore = setupMockStore({});
+  const runner = new BasicStepFormTestRunner(PivotStepForm, 'pivot');
+  runner.testInstantiate();
+  runner.testExpectedComponents({
+    'multiselectwidget-stub': 1,
+    'columnpicker-stub': 1,
+    'autocompletewidget-stub': 2,
   });
-
-  it('should instantiate', () => {
-    const wrapper = shallowMount(PivotStepForm, { store: emptyStore, localVue });
-    expect(wrapper.exists()).toBeTruthy();
-  });
-
-  it('should have 4 input components', () => {
-    const wrapper = shallowMount(PivotStepForm, { store: emptyStore, localVue });
-    const multiselectWrappers = wrapper.findAll('multiselectwidget-stub');
-    const columnpickerWrappers = wrapper.findAll('columnpicker-stub');
-    const autocompleteWrappers = wrapper.findAll('autocompletewidget-stub');
-    expect(multiselectWrappers.length).toEqual(1);
-    expect(columnpickerWrappers.length).toEqual(1);
-    expect(autocompleteWrappers.length).toEqual(2);
-  });
-
-  it('should pass down props to widgets', () => {
-    const wrapper = shallowMount(PivotStepForm, {
-      store: emptyStore,
-      localVue,
-      data: () => {
-        return {
-          editedStep: {
-            name: 'pivot',
-            index: ['label'],
-            column_to_pivot: 'country',
-            value_column: 'value',
-            agg_function: 'sum',
-          },
-        };
+  runner.testValidate({
+    testlabel: 'submitted data is valid',
+    props: {
+      initialStepValue: {
+        name: 'pivot',
+        index: ['columnA', 'columnB'],
+        column_to_pivot: 'foo',
+        value_column: 'bar',
+        agg_function: 'sum',
       },
-    });
-    expect(wrapper.find('#indexInput').props('value')).toEqual(['label']);
-    expect(wrapper.find('#valueColumnInput').props('value')).toEqual('value');
-    expect(wrapper.find('#aggregationFunctionInput').props('value')).toEqual('sum');
+    },
   });
-
-  it('should instantiate indexInput widget multiselect with column names', () => {
-    const store = setupMockStore({
-      dataset: {
-        headers: [{ name: 'columnA' }, { name: 'columnB' }, { name: 'columnC' }],
-        data: [],
-      },
-    });
-    const wrapper = shallowMount(PivotStepForm, { store, localVue });
-    expect(wrapper.find('#indexInput').attributes('options')).toEqual('columnA,columnB,columnC');
-  });
-
-  it('should instantiate valueColumnInput widget autocomplete with column names', () => {
-    const store = setupMockStore({
-      dataset: {
-        headers: [{ name: 'columnA' }, { name: 'columnB' }, { name: 'columnC' }],
-        data: [],
-      },
-    });
-    const wrapper = shallowMount(PivotStepForm, { store, localVue });
-    expect(wrapper.find('#valueColumnInput').attributes('options')).toEqual(
-      'columnA,columnB,columnC',
-    );
-  });
-
-  it('should instantiate aggregationFunctionInput widget autocomplete with the right aggregation function names', () => {
-    const wrapper = shallowMount(PivotStepForm, { store: emptyStore, localVue });
-    expect(wrapper.find('#aggregationFunctionInput').attributes('options')).toEqual(
-      'sum,avg,count,min,max',
-    );
-  });
-
-  it('should validate and emit "formSaved" when submitted data is valid', async () => {
-    const wrapper = mount(PivotStepForm, {
-      store: emptyStore,
-      localVue,
-      propsData: {
-        initialStepValue: {
-          name: 'pivot',
-          index: ['columnA', 'columnB'],
-          column_to_pivot: 'foo',
-          value_column: 'bar',
-          agg_function: 'sum',
-        },
-      },
-    });
-    wrapper.find('.widget-form-action__button--validate').trigger('click');
-    await localVue.nextTick();
-    expect(wrapper.vm.$data.errors).toBeNull();
-    expect(wrapper.emitted()).toEqual({
-      formSaved: [
-        [
-          {
-            name: 'pivot',
-            index: ['columnA', 'columnB'],
-            column_to_pivot: 'foo',
-            value_column: 'bar',
-            agg_function: 'sum',
-          },
-        ],
-      ],
-    });
-  });
-
-  it('should update step when selectedColumn is changed', async () => {
-    const store = setupMockStore({
-      dataset: {
-        headers: [{ name: 'columnA' }, { name: 'columnB' }, { name: 'columnC' }],
-        data: [],
-      },
-    });
-    const wrapper = shallowMount(PivotStepForm, { store, localVue });
-    expect(wrapper.vm.$data.editedStep.column_to_pivot).toEqual('');
-    store.commit(VQBnamespace('toggleColumnSelection'), { column: 'columnB' });
-    await localVue.nextTick();
-    expect(wrapper.vm.$data.editedStep.column_to_pivot).toEqual('columnB');
-  });
-
-  describe('Errors', () => {
-    it('should fire errors when fields are missing', () => {
-      const wrapper = mount(PivotStepForm, { store: emptyStore, localVue });
-
-      wrapper.find('.widget-form-action__button--validate').trigger('click');
-      const errors = wrapper.vm.$data.errors
-        .map((err: ValidationError) => ({ keyword: err.keyword, dataPath: err.dataPath }))
-        .sort((err1: ValidationError, err2: ValidationError) =>
-          err1.dataPath.localeCompare(err2.dataPath),
-        );
-
-      expect(errors).toEqual([
+  runner.testValidationErrors([
+    {
+      testlabel: 'submitted data is not valid',
+      errors: [
         { keyword: 'minLength', dataPath: '.column_to_pivot' },
         { keyword: 'minItems', dataPath: '.index' },
         { keyword: 'minLength', dataPath: '.value_column' },
-      ]);
-    });
-
-    it('should fire errors when index and column_to_pivot column names overlap', async () => {
-      const store = setupMockStore({
+      ],
+    },
+    {
+      testlabel: 'index and column_to_pivot column names overlap',
+      store: setupMockStore({
         dataset: {
           headers: [{ name: 'columnA' }, { name: 'columnB' }, { name: 'columnC' }],
           data: [],
         },
-      });
-      const wrapper = mount(PivotStepForm, { store, localVue });
-      wrapper.setData({
+      }),
+      data: {
         editedStep: {
           name: 'pivot',
           index: ['columnA', 'columnC'],
@@ -162,32 +48,23 @@ describe('Pivot Step Form', () => {
           value_column: 'columnB',
           agg_function: 'sum',
         },
-      });
-      wrapper.find('.widget-form-action__button--validate').trigger('click');
-      await localVue.nextTick();
-      const errors = wrapper.vm.$data.errors.map((err: ValidationError) => ({
-        keyword: err.keyword,
-        dataPath: err.dataPath,
-        message: err.message,
-      }));
-      expect(errors).toEqual([
+      },
+      errors: [
         {
           keyword: 'columnNameConflict',
           dataPath: '.column_to_pivot',
-          message: 'Column name columnA is used at least twice but should be unique',
         },
-      ]);
-    });
-
-    it('should fire errors when index and value_column column names overlap', async () => {
-      const store = setupMockStore({
+      ],
+    },
+    {
+      testlabel: 'index and value_column column names overlap',
+      store: setupMockStore({
         dataset: {
           headers: [{ name: 'columnA' }, { name: 'columnB' }, { name: 'columnC' }],
           data: [],
         },
-      });
-      const wrapper = mount(PivotStepForm, { store, localVue });
-      wrapper.setData({
+      }),
+      data: {
         editedStep: {
           name: 'pivot',
           index: ['columnA', 'columnC'],
@@ -195,32 +72,23 @@ describe('Pivot Step Form', () => {
           value_column: 'columnA',
           agg_function: 'sum',
         },
-      });
-      wrapper.find('.widget-form-action__button--validate').trigger('click');
-      await localVue.nextTick();
-      const errors = wrapper.vm.$data.errors.map((err: ValidationError) => ({
-        keyword: err.keyword,
-        dataPath: err.dataPath,
-        message: err.message,
-      }));
-      expect(errors).toEqual([
+      },
+      errors: [
         {
           keyword: 'columnNameConflict',
           dataPath: '.value_column',
-          message: 'Column name columnA is used at least twice but should be unique',
         },
-      ]);
-    });
-
-    it('should fire errors when column_to_pivot and value_column are equal', async () => {
-      const store = setupMockStore({
+      ],
+    },
+    {
+      testlabel: 'column_to_pivot and value_column are equal',
+      store: setupMockStore({
         dataset: {
           headers: [{ name: 'columnA' }, { name: 'columnB' }, { name: 'columnC' }],
           data: [],
         },
-      });
-      const wrapper = mount(PivotStepForm, { store, localVue });
-      wrapper.setData({
+      }),
+      data: {
         editedStep: {
           name: 'pivot',
           index: ['columnA', 'columnC'],
@@ -228,21 +96,76 @@ describe('Pivot Step Form', () => {
           value_column: 'columnB',
           agg_function: 'sum',
         },
-      });
-      wrapper.find('.widget-form-action__button--validate').trigger('click');
-      await localVue.nextTick();
-      const errors = wrapper.vm.$data.errors.map((err: ValidationError) => ({
-        keyword: err.keyword,
-        dataPath: err.dataPath,
-        message: err.message,
-      }));
-      expect(errors).toEqual([
+      },
+      errors: [
         {
           keyword: 'columnNameConflict',
           dataPath: '.column_to_pivot',
-          message: 'Column name columnB is used at least twice but should be unique',
         },
-      ]);
+      ],
+    },
+  ]);
+
+  it('should pass down props to widgets', async () => {
+    const wrapper = runner.shallowMount(undefined, {
+      data: {
+        editedStep: {
+          name: 'pivot',
+          index: ['label'],
+          column_to_pivot: 'country',
+          value_column: 'value',
+          agg_function: 'sum',
+        },
+      },
     });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.find('#indexInput').props('value')).toEqual(['label']);
+    expect(wrapper.find('#valueColumnInput').props('value')).toEqual('value');
+    expect(wrapper.find('#aggregationFunctionInput').props('value')).toEqual('sum');
+  });
+
+  it('should instantiate indexInput widget multiselect with column names', () => {
+    const initialState = {
+      dataset: {
+        headers: [{ name: 'columnA' }, { name: 'columnB' }, { name: 'columnC' }],
+        data: [],
+      },
+    };
+    const wrapper = runner.shallowMount(initialState);
+    expect(wrapper.find('#indexInput').attributes('options')).toEqual('columnA,columnB,columnC');
+  });
+
+  it('should instantiate valueColumnInput widget autocomplete with column names', () => {
+    const initialState = {
+      dataset: {
+        headers: [{ name: 'columnA' }, { name: 'columnB' }, { name: 'columnC' }],
+        data: [],
+      },
+    };
+    const wrapper = runner.shallowMount(initialState);
+    expect(wrapper.find('#valueColumnInput').attributes('options')).toEqual(
+      'columnA,columnB,columnC',
+    );
+  });
+
+  it('should instantiate aggregationFunctionInput widget autocomplete with the right aggregation function names', () => {
+    const wrapper = runner.shallowMount();
+    expect(wrapper.find('#aggregationFunctionInput').attributes('options')).toEqual(
+      'sum,avg,count,min,max',
+    );
+  });
+
+  it('should update step when selectedColumn is changed', async () => {
+    const initialState = {
+      dataset: {
+        headers: [{ name: 'columnA' }, { name: 'columnB' }, { name: 'columnC' }],
+        data: [],
+      },
+    };
+    const wrapper = runner.shallowMount(initialState);
+    expect(wrapper.vm.$data.editedStep.column_to_pivot).toEqual('');
+    wrapper.vm.$store.commit(VQBnamespace('toggleColumnSelection'), { column: 'columnB' });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.vm.$data.editedStep.column_to_pivot).toEqual('columnB');
   });
 });
