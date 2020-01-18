a9f8bfbdea8ad9bd13381d8ced586495243de77b
Adrien Di Mascio <adrien.dimascio@toucantoco.com>
2020-01-18 14:15:57 +0100

    [tests] use test runner in delete-step-form.spec.ts

--- tests/unit/delete-column-step-form.spec.ts
+++ tests/unit/delete-column-step-form.spec.ts
@@ -1,30 +1,49 @@
 import { mount, shallowMount, createLocalVue } from '@vue/test-utils';
-import Vuex, { Store } from 'vuex';
+import Vuex from 'vuex';
 
 import DeleteColumnStepForm from '@/components/stepforms/DeleteColumnStepForm.vue';
 import MultiselectWidget from '@/components/stepforms/widgets/Multiselect.vue';
-import { setupMockStore, RootState, ValidationError } from './utils';
-import { Pipeline } from '@/lib/steps';
+import { setupMockStore, BasicStepFormTestRunner } from './utils';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
 describe('Delete Column Step Form', () => {
-  let emptyStore: Store<RootState>;
-  beforeEach(() => {
-    emptyStore = setupMockStore({});
+  const runner = new BasicStepFormTestRunner(DeleteColumnStepForm, 'delete', localVue);
+  runner.testInstantiate();
+  runner.testExpectedComponents({
+    'multiselectwidget-stub': 1,
+  });
+  runner.testValidationErrors([
+    {
+      testlabel: 'submitted data is not valid',
+      errors: [{ dataPath: '.columns', keyword: 'minItems' }],
+    },
+  ]);
+
+  runner.testValidate({
+    testlabel: 'submitted data is valid',
+    props: {
+      initialStepValue: { name: 'delete', columns: ['foo'] },
+    },
   });
 
-  it('should instantiate', () => {
-    const wrapper = shallowMount(DeleteColumnStepForm, { store: emptyStore, localVue });
-    expect(wrapper.exists()).toBeTruthy();
-    expect(wrapper.vm.$data.stepname).toEqual('delete');
+  runner.testCancel({
+    pipeline: [
+      { name: 'domain', domain: 'foo' },
+      { name: 'rename', oldname: 'foo', newname: 'bar' },
+    ],
+    selectedStepIndex: 1,
   });
 
-  it('should have a widget multiselect', () => {
-    const wrapper = shallowMount(DeleteColumnStepForm, { store: emptyStore, localVue });
-
-    expect(wrapper.find('multiselectwidget-stub').exists()).toBeTruthy();
+  runner.testResetSelectedIndex({
+    pipeline: [
+      { name: 'domain', domain: 'foo' },
+      { name: 'rename', oldname: 'foo', newname: 'bar' },
+      { name: 'rename', oldname: 'baz', newname: 'spam' },
+      { name: 'rename', oldname: 'tic', newname: 'tac' },
+    ],
+    selectedStepIndex: 2,
   });
 
   it('should instantiate a multiselect widget with proper options from the store', () => {
@@ -38,51 +57,6 @@ describe('Delete Column Step Form', () => {
     const widgetAutocomplete = wrapper.find('multiselectwidget-stub');
 
     expect(widgetAutocomplete.attributes('options')).toEqual('columnA,columnB,columnC');
-  });
-
-  it('should report errors when submitted data is not valid', () => {
-    const wrapper = mount(DeleteColumnStepForm, { store: emptyStore, localVue });
-    wrapper.find('.widget-form-action__button--validate').trigger('click');
-    const errors = wrapper.vm.$data.errors.map((err: ValidationError) => ({
-      keyword: err.keyword,
-      dataPath: err.dataPath,
-    }));
-    expect(errors).toEqual([{ keyword: 'minItems', dataPath: '.columns' }]);
-  });
-
-  it('should validate and emit "formSaved" when submitted data is valid', () => {
-    const wrapper = mount(DeleteColumnStepForm, {
-      store: emptyStore,
-      localVue,
-      propsData: {
-        initialStepValue: { name: 'delete', columns: ['foo'] },
-      },
-    });
-    wrapper.find('.widget-form-action__button--validate').trigger('click');
-    expect(wrapper.vm.$data.errors).toBeNull();
-    expect(wrapper.emitted()).toEqual({
-      formSaved: [[{ name: 'delete', columns: ['foo'] }]],
-    });
-  });
-
-  it('should emit "cancel" event when edition is canceled', () => {
-    const pipeline: Pipeline = [
-      { name: 'domain', domain: 'foo' },
-      { name: 'rename', oldname: 'foo', newname: 'bar' },
-    ];
-    const store = setupMockStore({
-      pipeline,
-      selectedStepIndex: 1,
-    });
-
-    const wrapper = mount(DeleteColumnStepForm, { store, localVue });
-    wrapper.find('.step-edit-form__back-button').trigger('click');
-    expect(wrapper.emitted()).toEqual({ cancel: [[]] });
-    expect(store.state.vqb.selectedStepIndex).toEqual(1);
-    expect(store.state.vqb.pipeline).toEqual([
-      { name: 'domain', domain: 'foo' },
-      { name: 'rename', oldname: 'foo', newname: 'bar' },
-    ]);
   });
 
   it('should update selectedColumn when column is changed', async () => {
@@ -103,27 +77,8 @@ describe('Delete Column Step Form', () => {
       localVue,
     });
     wrapper.setData({ editedStep: { columns: ['columnB'] } });
-    await wrapper.find(MultiselectWidget).trigger('input');
+    wrapper.find(MultiselectWidget).trigger('input');
+    await wrapper.vm.$nextTick();
     expect(store.state.vqb.selectedColumns).toEqual(['columnB']);
-  });
-
-  it('should reset selectedStepIndex correctly on cancel depending on isStepCreation', () => {
-    const pipeline: Pipeline = [
-      { name: 'domain', domain: 'foo' },
-      { name: 'rename', oldname: 'foo', newname: 'bar' },
-      { name: 'rename', oldname: 'baz', newname: 'spam' },
-      { name: 'rename', oldname: 'tic', newname: 'tac' },
-    ];
-    const store = setupMockStore({
-      pipeline,
-      selectedStepIndex: 2,
-    });
-    const wrapper = mount(DeleteColumnStepForm, { store, localVue });
-    wrapper.setProps({ isStepCreation: true });
-    wrapper.find('.step-edit-form__back-button').trigger('click');
-    expect(store.state.vqb.selectedStepIndex).toEqual(2);
-    wrapper.setProps({ isStepCreation: false });
-    wrapper.find('.step-edit-form__back-button').trigger('click');
-    expect(store.state.vqb.selectedStepIndex).toEqual(3);
   });
 });
