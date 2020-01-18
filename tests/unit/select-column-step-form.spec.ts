3edfa39842aba762fc43742790f7df445b8e4da8
Adrien Di Mascio <adrien.dimascio@toucantoco.com>
2020-01-18 23:37:36 +0100

    [tests] use test runner in select-column-form.spec.ts

--- tests/unit/select-column-step-form.spec.ts
+++ tests/unit/select-column-step-form.spec.ts
@@ -1,130 +1,62 @@
-import { mount, shallowMount, createLocalVue } from '@vue/test-utils';
-import Vuex, { Store } from 'vuex';
-
 import SelectColumnStepForm from '@/components/stepforms/SelectColumnStepForm.vue';
 import MultiselectWidget from '@/components/stepforms/widgets/Multiselect.vue';
-import { Pipeline } from '@/lib/steps';
 
-import { setupMockStore, ValidationError } from './utils';
-
-const localVue = createLocalVue();
-localVue.use(Vuex);
+import { BasicStepFormTestRunner } from './utils';
 
 describe('Select Column Step Form', () => {
-  let emptyStore: Store<any>;
-  beforeEach(() => {
-    emptyStore = setupMockStore({});
+  const runner = new BasicStepFormTestRunner(SelectColumnStepForm, 'select');
+  runner.testInstantiate();
+  runner.testExpectedComponents({
+    'multiselectwidget-stub': 1,
+  });
+  runner.testValidationErrors([
+    {
+      testlabel: 'submitted data is not valid',
+      errors: [{ keyword: 'minItems', dataPath: '.columns' }],
+    },
+  ]);
+
+  runner.testValidate({
+    testlabel: 'submitted data is valid',
+    props: {
+      initialStepValue: { name: 'select', columns: ['foo'] },
+    },
   });
 
-  it('should instantiate', () => {
-    const wrapper = shallowMount(SelectColumnStepForm, { store: emptyStore, localVue });
-    expect(wrapper.exists()).toBeTruthy();
-    expect(wrapper.vm.$data.stepname).toEqual('select');
-  });
-
-  it('should have a widget multiselect', () => {
-    const wrapper = shallowMount(SelectColumnStepForm, { store: emptyStore, localVue });
-
-    expect(wrapper.find('multiselectwidget-stub').exists()).toBeTruthy();
-  });
+  runner.testCancel();
+  runner.testResetSelectedIndex();
 
   it('should instantiate a multiselect widget with proper options from the store', () => {
-    const store = setupMockStore({
+    const initialState = {
       dataset: {
         headers: [{ name: 'columnA' }, { name: 'columnB' }, { name: 'columnC' }],
         data: [],
       },
-    });
-    const wrapper = shallowMount(SelectColumnStepForm, { store, localVue });
+    };
+    const wrapper = runner.shallowMount(initialState);
     const widgetAutocomplete = wrapper.find('multiselectwidget-stub');
 
     expect(widgetAutocomplete.attributes('options')).toEqual('columnA,columnB,columnC');
   });
 
-  it('should report errors when submitted data is not valid', () => {
-    const wrapper = mount(SelectColumnStepForm, { store: emptyStore, localVue });
-    wrapper.find('.widget-form-action__button--validate').trigger('click');
-    const errors = wrapper.vm.$data.errors.map((err: ValidationError) => ({
-      keyword: err.keyword,
-      dataPath: err.dataPath,
-    }));
-    expect(errors).toEqual([{ keyword: 'minItems', dataPath: '.columns' }]);
-  });
-
-  it('should validate and emit "formSaved" when submitted data is valid', () => {
-    const wrapper = mount(SelectColumnStepForm, {
-      store: emptyStore,
-      localVue,
-      propsData: {
-        initialStepValue: { name: 'select', columns: ['foo'] },
-      },
-    });
-    wrapper.find('.widget-form-action__button--validate').trigger('click');
-    expect(wrapper.vm.$data.errors).toBeNull();
-    expect(wrapper.emitted()).toEqual({
-      formSaved: [[{ name: 'select', columns: ['foo'] }]],
-    });
-  });
-
-  it('should emit "cancel" event when edition is canceled', () => {
-    const pipeline: Pipeline = [
-      { name: 'domain', domain: 'foo' },
-      { name: 'rename', oldname: 'foo', newname: 'bar' },
-    ];
-    const store = setupMockStore({
-      pipeline,
-      selectedStepIndex: 1,
-    });
-
-    const wrapper = mount(SelectColumnStepForm, { store, localVue });
-    wrapper.find('.step-edit-form__back-button').trigger('click');
-    expect(wrapper.emitted()).toEqual({ cancel: [[]] });
-    expect(store.state.vqb.selectedStepIndex).toEqual(1);
-    expect(store.state.vqb.pipeline).toEqual([
-      { name: 'domain', domain: 'foo' },
-      { name: 'rename', oldname: 'foo', newname: 'bar' },
-    ]);
-  });
-
   it('should update selectedColumn when column is changed', async () => {
-    const store = setupMockStore({
+    const initialState = {
       dataset: {
         headers: [{ name: 'columnA' }, { name: 'columnB' }, { name: 'columnC' }],
         data: [],
       },
       selectedColumns: ['columnA'],
-    });
-    const wrapper = mount(SelectColumnStepForm, {
+    };
+    const wrapper = runner.mount(initialState, {
       propsData: {
         initialValue: {
           columns: ['columnA'],
         },
       },
-      store,
-      localVue,
     });
     wrapper.setData({ editedStep: { columns: ['columnB'] } });
-    await wrapper.find(MultiselectWidget).trigger('input');
-    expect(store.state.vqb.selectedColumns).toEqual(['columnB']);
-  });
-
-  it('should reset selectedStepIndex correctly on cancel depending on isStepCreation', () => {
-    const pipeline: Pipeline = [
-      { name: 'domain', domain: 'foo' },
-      { name: 'rename', oldname: 'foo', newname: 'bar' },
-      { name: 'rename', oldname: 'baz', newname: 'spam' },
-      { name: 'rename', oldname: 'tic', newname: 'tac' },
-    ];
-    const store = setupMockStore({
-      pipeline,
-      selectedStepIndex: 2,
-    });
-    const wrapper = mount(SelectColumnStepForm, { store, localVue });
-    wrapper.setProps({ isStepCreation: true });
-    wrapper.find('.step-edit-form__back-button').trigger('click');
-    expect(store.state.vqb.selectedStepIndex).toEqual(2);
-    wrapper.setProps({ isStepCreation: false });
-    wrapper.find('.step-edit-form__back-button').trigger('click');
-    expect(store.state.vqb.selectedStepIndex).toEqual(3);
+    wrapper.find(MultiselectWidget).trigger('input');
+    await wrapper.vm.$nextTick();
+    expect(wrapper.vm.$store.state.vqb.selectedColumns).toEqual(['columnB']);
   });
 });
