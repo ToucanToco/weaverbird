531badea92779bf013fed20c2b2b2bd438b21640
Adrien Di Mascio <adrien.dimascio@toucantoco.com>
2020-01-18 14:03:36 +0100

    [tests] use test runner in argmin-step-form.spec.ts

--- tests/unit/argmin-step-form.spec.ts
+++ tests/unit/argmin-step-form.spec.ts
@@ -1,9 +1,8 @@
-import { mount, shallowMount, createLocalVue } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Vuex, { Store } from 'vuex';
 
 import ArgminStepForm from '@/components/stepforms/ArgminStepForm.vue';
-import { setupMockStore, RootState, ValidationError } from './utils';
-import { Pipeline } from '@/lib/steps';
+import { setupMockStore, BasicStepFormTestRunner, RootState } from './utils';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
@@ -14,18 +13,36 @@ describe('Argmin Step Form', () => {
     emptyStore = setupMockStore({});
   });
 
-  it('should instantiate', () => {
-    const wrapper = shallowMount(ArgminStepForm, { store: emptyStore, localVue });
-    expect(wrapper.exists()).toBeTruthy();
-    expect(wrapper.vm.$data.stepname).toEqual('argmin');
+  const runner = new BasicStepFormTestRunner(ArgminStepForm, 'argmin', localVue);
+  runner.testInstantiate();
+  runner.testExpectedComponents({
+    'columnpicker-stub': 1,
+    'multiselectwidget-stub': 1,
   });
 
-  it('should have exactly 2 input components', () => {
-    const wrapper = shallowMount(ArgminStepForm, { store: emptyStore, localVue });
-    const autocompleteWrappers = wrapper.findAll('columnpicker-stub');
-    const multiselectWrappers = wrapper.findAll('multiselectwidget-stub');
-    expect(autocompleteWrappers.length).toEqual(1);
-    expect(multiselectWrappers.length).toEqual(1);
+  runner.testValidationErrors([
+    {
+      testlabel: 'submitted data is not valid',
+      errors: [{ dataPath: '.column', keyword: 'minLength' }],
+    },
+  ]);
+
+  runner.testValidate({
+    testlabel: 'submitted data is valid',
+    props: {
+      initialStepValue: { name: 'argmin', column: 'foo', groups: ['bar'] },
+    },
+  });
+
+  runner.testCancel();
+  runner.testResetSelectedIndex({
+    pipeline: [
+      { name: 'domain', domain: 'foo' },
+      { name: 'rename', oldname: 'foo', newname: 'bar' },
+      { name: 'rename', oldname: 'baz', newname: 'spam' },
+      { name: 'rename', oldname: 'tic', newname: 'tac' },
+    ],
+    selectedStepIndex: 2,
   });
 
   it('should pass down the properties to the input components', async () => {
@@ -35,62 +52,5 @@ describe('Argmin Step Form', () => {
     });
     await localVue.nextTick();
     expect(wrapper.find('multiselectwidget-stub').props('value')).toEqual(['bar']);
-  });
-
-  it('should report errors if column is empty', async () => {
-    const wrapper = mount(ArgminStepForm, { store: emptyStore, localVue });
-    wrapper.find('.widget-form-action__button--validate').trigger('click');
-    await localVue.nextTick();
-    const errors = wrapper.vm.$data.errors
-      .map((err: ValidationError) => ({ keyword: err.keyword, dataPath: err.dataPath }))
-      .sort((err1: ValidationError, err2: ValidationError) =>
-        err1.dataPath.localeCompare(err2.dataPath),
-      );
-    expect(errors).toEqual([{ keyword: 'minLength', dataPath: '.column' }]);
-  });
-
-  it('should validate and emit "formSaved" when submitted data is valid', async () => {
-    const wrapper = mount(ArgminStepForm, {
-      store: emptyStore,
-      localVue,
-      propsData: {
-        initialStepValue: { name: 'argmin', column: 'foo', groups: ['bar'] },
-      },
-    });
-    wrapper.find('.widget-form-action__button--validate').trigger('click');
-    await localVue.nextTick();
-    expect(wrapper.vm.$data.errors).toBeNull();
-    expect(wrapper.emitted()).toEqual({
-      formSaved: [[{ name: 'argmin', column: 'foo', groups: ['bar'] }]],
-    });
-  });
-
-  it('should emit "cancel" event when edition is cancelled', async () => {
-    const wrapper = mount(ArgminStepForm, { store: emptyStore, localVue });
-    wrapper.find('.step-edit-form__back-button').trigger('click');
-    await localVue.nextTick();
-    expect(wrapper.emitted()).toEqual({
-      cancel: [[]],
-    });
-  });
-
-  it('should reset selectedStepIndex correctly on cancel depending on isStepCreation', () => {
-    const pipeline: Pipeline = [
-      { name: 'domain', domain: 'foo' },
-      { name: 'argmin', column: 'foo' },
-      { name: 'argmin', column: 'baz' },
-      { name: 'argmin', column: 'tic' },
-    ];
-    const store = setupMockStore({
-      pipeline,
-      selectedStepIndex: 2,
-    });
-    const wrapper = mount(ArgminStepForm, { store, localVue });
-    wrapper.setProps({ isStepCreation: true });
-    wrapper.find('.step-edit-form__back-button').trigger('click');
-    expect(store.state.vqb.selectedStepIndex).toEqual(2);
-    wrapper.setProps({ isStepCreation: false });
-    wrapper.find('.step-edit-form__back-button').trigger('click');
-    expect(store.state.vqb.selectedStepIndex).toEqual(3);
   });
 });
