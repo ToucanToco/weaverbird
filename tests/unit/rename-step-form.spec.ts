8eb52bb5320b06f73ed4c7f0cee1aeec5173a903
Adrien Di Mascio <adrien.dimascio@toucantoco.com>
2020-01-19 14:13:57 +0100

    [tests] use runner mount utils in rename-step-form.spec.ts

--- tests/unit/rename-step-form.spec.ts
+++ tests/unit/rename-step-form.spec.ts
@@ -1,20 +1,9 @@
-import { mount, shallowMount, createLocalVue } from '@vue/test-utils';
-import Vuex, { Store } from 'vuex';
-
 import RenameStepForm from '@/components/stepforms/RenameStepForm.vue';
 import { VQBnamespace } from '@/store';
-import { setupMockStore, BasicStepFormTestRunner, RootState } from './utils';
-
-const localVue = createLocalVue();
-localVue.use(Vuex);
+import { setupMockStore, BasicStepFormTestRunner } from './utils';
 
 describe('Rename Step Form', () => {
-  let emptyStore: Store<RootState>;
-  beforeEach(() => {
-    emptyStore = setupMockStore({});
-  });
-
-  const runner = new BasicStepFormTestRunner(RenameStepForm, 'rename', localVue);
+  const runner = new BasicStepFormTestRunner(RenameStepForm, 'rename');
   runner.testInstantiate();
   runner.testExpectedComponents({ 'inputtextwidget-stub': 1, 'columnpicker-stub': 1 });
 
@@ -59,70 +48,74 @@ describe('Rename Step Form', () => {
   runner.testResetSelectedIndex();
 
   it('should pass down the newname prop to widget value prop', async () => {
-    const wrapper = shallowMount(RenameStepForm, { store: emptyStore, localVue });
-    wrapper.setData({ editedStep: { name: 'rename', oldname: '', newname: 'foo' } });
-    await localVue.nextTick();
+    const wrapper = runner.shallowMount(
+      {},
+      {
+        data: { editedStep: { name: 'rename', oldname: '', newname: 'foo' } },
+      },
+    );
+    await wrapper.vm.$nextTick();
     expect(wrapper.find('inputtextwidget-stub').props('value')).toEqual('foo');
   });
 
   it('should update step when selectedColumn is changed', async () => {
-    const store = setupMockStore({
+    const initialState = {
       dataset: {
         headers: [{ name: 'columnA' }, { name: 'columnB' }, { name: 'columnC' }],
         data: [],
       },
-    });
-    const wrapper = shallowMount(RenameStepForm, { store, localVue });
+    };
+    const wrapper = runner.shallowMount(initialState);
     expect(wrapper.vm.$data.editedStep.oldname).toEqual('');
-    store.commit(VQBnamespace('toggleColumnSelection'), { column: 'columnB' });
-    await localVue.nextTick();
+    wrapper.vm.$store.commit(VQBnamespace('toggleColumnSelection'), { column: 'columnB' });
+    await wrapper.vm.$nextTick();
     expect(wrapper.vm.$data.editedStep.oldname).toEqual('columnB');
   });
 
   it('should reset selectedStepIndex correctly on cancel depending on isStepCreation', () => {
-    const pipeline = [
-      { name: 'domain', domain: 'foo' },
-      { name: 'rename', oldname: 'foo', newname: 'bar' },
-      { name: 'rename', oldname: 'baz', newname: 'spam' },
-      { name: 'rename', oldname: 'tic', newname: 'tac' },
-    ];
-    const store = setupMockStore({
-      pipeline,
+    const initialState = {
+      pipeline: [
+        { name: 'domain', domain: 'foo' },
+        { name: 'rename', oldname: 'foo', newname: 'bar' },
+        { name: 'rename', oldname: 'baz', newname: 'spam' },
+        { name: 'rename', oldname: 'tic', newname: 'tac' },
+      ],
       selectedStepIndex: 2,
-    });
-    const wrapper = mount(RenameStepForm, { store, localVue });
-    wrapper.setProps({ isStepCreation: true });
+    };
+    const wrapper = runner.mount(initialState, { propsData: { isStepCreation: true } });
     wrapper.find('.step-edit-form__back-button').trigger('click');
-    expect(store.state.vqb.selectedStepIndex).toEqual(2);
+    expect(wrapper.vm.$store.state.vqb.selectedStepIndex).toEqual(2);
     wrapper.setProps({ isStepCreation: false });
     wrapper.find('.step-edit-form__back-button').trigger('click');
-    expect(store.state.vqb.selectedStepIndex).toEqual(3);
+    expect(wrapper.vm.$store.state.vqb.selectedStepIndex).toEqual(3);
   });
 
   it('should make the focus on the column modified after rename validation', () => {
-    const store = setupMockStore({
+    const initialState = {
       dataset: {
         headers: [{ name: 'columnA' }, { name: 'columnB' }, { name: 'columnC' }],
         data: [],
       },
+    };
+    const wrapper = runner.mount(initialState, {
+      data: { editedStep: { name: 'rename', oldname: 'columnA', newname: 'toto' } },
     });
-    const wrapper = mount(RenameStepForm, { store, localVue });
-    wrapper.setData({ editedStep: { name: 'rename', oldname: 'columnA', newname: 'toto' } });
     wrapper.find('.widget-form-action__button--validate').trigger('click');
-    expect(store.state.vqb.selectedColumns).toEqual(['toto']);
+    expect(wrapper.vm.$store.state.vqb.selectedColumns).toEqual(['toto']);
   });
 
   it('should not change the column focus if validation fails', () => {
-    const store = setupMockStore({
+    const initialState = {
       dataset: {
         headers: [{ name: 'columnA' }, { name: 'columnB' }, { name: 'columnC' }],
         data: [],
       },
       selectedColumns: ['columnA'],
+    };
+    const wrapper = runner.mount(initialState, {
+      data: { editedStep: { name: 'rename', oldname: 'columnA', newname: 'columnB' } },
     });
-    const wrapper = mount(RenameStepForm, { store, localVue });
-    wrapper.setData({ editedStep: { name: 'rename', oldname: 'columnA', newname: 'columnB' } });
     wrapper.find('.widget-form-action__button--validate').trigger('click');
-    expect(store.state.vqb.selectedColumns).toEqual(['columnA']);
+    expect(wrapper.vm.$store.state.vqb.selectedColumns).toEqual(['columnA']);
   });
 });
