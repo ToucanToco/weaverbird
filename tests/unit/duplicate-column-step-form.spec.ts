a794290b6f16246b8cc7c90e48d2d16c13232613
Adrien Di Mascio <adrien.dimascio@toucantoco.com>
2020-01-19 13:54:09 +0100

    [tests] use test runner in duplicate-step-form.spec.ts

--- tests/unit/duplicate-column-step-form.spec.ts
+++ tests/unit/duplicate-column-step-form.spec.ts
@@ -1,125 +1,67 @@
-import { mount, shallowMount, createLocalVue } from '@vue/test-utils';
-import Vuex, { Store } from 'vuex';
+import { createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
 
 import DuplicateColumnStepForm from '@/components/stepforms/DuplicateColumnStepForm.vue';
-import { setupMockStore, RootState, ValidationError } from './utils';
-import { Pipeline } from '@/lib/steps';
+import { setupMockStore, BasicStepFormTestRunner } from './utils';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
 describe('Duplicate Column Step Form', () => {
-  let emptyStore: Store<RootState>;
-  beforeEach(() => {
-    emptyStore = setupMockStore({});
+
+  const runner = new BasicStepFormTestRunner(DuplicateColumnStepForm, 'duplicate', localVue);
+  runner.testInstantiate();
+  runner.testExpectedComponents({
+    'columnpicker-stub': 1,
+    'inputtextwidget-stub': 1,
   });
 
-  it('should instantiate', () => {
-    const wrapper = shallowMount(DuplicateColumnStepForm, { store: emptyStore, localVue });
-
-    expect(wrapper.exists()).toBeTruthy();
-  });
-
-  it('should have exactly 2 input components', () => {
-    const wrapper = shallowMount(DuplicateColumnStepForm, { store: emptyStore, localVue });
-
-    expect(wrapper.findAll('columnpicker-stub').length).toEqual(1);
-    expect(wrapper.findAll('inputtextwidget-stub').length).toEqual(1);
-  });
-
-  describe('Errors', () => {
-    it('should report errors when submitted data is not valid', () => {
-      const wrapper = mount(DuplicateColumnStepForm, {
-        store: emptyStore,
-        localVue,
-        propsData: {
-          initialStepValue: { name: 'duplicate', column: '', new_column_name: '' },
-        },
-      });
-      wrapper.find('.widget-form-action__button--validate').trigger('click');
-      const errors = wrapper.vm.$data.errors.map((err: ValidationError) => ({
-        keyword: err.keyword,
-        dataPath: err.dataPath,
-      }));
-      expect(errors).toEqual([
+  runner.testValidationErrors([
+    {
+      testlabel: 'no submitted data',
+      props: { initialStepValue: { name: 'duplicate', column: '', new_column_name: '' } },
+      errors: [
         { keyword: 'minLength', dataPath: '.column' },
         { keyword: 'minLength', dataPath: '.new_column_name' },
-      ]);
-    });
-  });
+      ],
+    },
+    {
+      testlabel: 'existing column name',
+      store: setupMockStore({
+        dataset: {
+          headers: [{ name: 'columnA' }, { name: 'columnB' }, { name: 'columnC' }],
+          data: [],
+        },
+      }),
+      data: { editedStep: { name: 'duplicate', column: 'foo', new_column_name: 'columnA' } },
+      errors: [
+        { keyword: 'columnNameAlreadyUsed', dataPath: '.new_column_name' },
+      ]
+    },
+  ]);
 
-  it('should report errors when new_column_name is an already existing column name', async () => {
-    const store = setupMockStore({
-      dataset: {
-        headers: [{ name: 'columnA' }, { name: 'columnB' }, { name: 'columnC' }],
-        data: [],
-      },
-    });
-    const wrapper = mount(DuplicateColumnStepForm, { store, localVue });
-    wrapper.setData({
-      editedStep: { name: 'duplicate', column: 'foo', new_column_name: 'columnA' },
-    });
-    wrapper.find('.widget-form-action__button--validate').trigger('click');
-    await localVue.nextTick();
-    const errors = wrapper.vm.$data.errors.map((err: ValidationError) => ({
-      keyword: err.keyword,
-      dataPath: err.dataPath,
-    }));
-    expect(errors).toEqual([{ keyword: 'columnNameAlreadyUsed', dataPath: '.new_column_name' }]);
-  });
+  runner.testValidate(
+    {
+      testlabel: 'submitted data is valid',
+      props: { initialStepValue: { name: 'duplicate', column: 'foo', new_column_name: 'bar' } },
+    }
+  );
 
-  it('should validate and emit "formSaved" when submitted data is valid', () => {
-    const wrapper = mount(DuplicateColumnStepForm, {
-      store: emptyStore,
-      localVue,
-      propsData: {
-        initialStepValue: { name: 'duplicate', column: 'foo', new_column_name: 'bar' },
-      },
-    });
-    wrapper.find('.widget-form-action__button--validate').trigger('click');
-    expect(wrapper.vm.$data.errors).toBeNull();
-    expect(wrapper.emitted()).toEqual({
-      formSaved: [[{ name: 'duplicate', column: 'foo', new_column_name: 'bar' }]],
-    });
-  });
-
-  it('should emit "cancel" event when edition is canceled', () => {
-    const pipeline: Pipeline = [
+  runner.testCancel({
+    pipeline: [
       { name: 'domain', domain: 'foo' },
       { name: 'rename', oldname: 'foo', newname: 'bar' },
-    ];
-    const store = setupMockStore({
-      pipeline,
-      selectedStepIndex: 1,
-    });
-
-    const wrapper = mount(DuplicateColumnStepForm, { store, localVue });
-    wrapper.find('.step-edit-form__back-button').trigger('click');
-    expect(wrapper.emitted()).toEqual({ cancel: [[]] });
-    expect(store.state.vqb.selectedStepIndex).toEqual(1);
-    expect(store.state.vqb.pipeline).toEqual([
-      { name: 'domain', domain: 'foo' },
-      { name: 'rename', oldname: 'foo', newname: 'bar' },
-    ]);
+    ],
+    selectedStepIndex: 1,
   });
 
-  it('should reset selectedStepIndex correctly on cancel depending on isStepCreation', () => {
-    const pipeline: Pipeline = [
+  runner.testResetSelectedIndex({
+    pipeline: [
       { name: 'domain', domain: 'foo' },
       { name: 'rename', oldname: 'foo', newname: 'bar' },
       { name: 'rename', oldname: 'baz', newname: 'spam' },
       { name: 'rename', oldname: 'tic', newname: 'tac' },
-    ];
-    const store = setupMockStore({
-      pipeline,
-      selectedStepIndex: 2,
-    });
-    const wrapper = mount(DuplicateColumnStepForm, { store, localVue });
-    wrapper.setProps({ isStepCreation: true });
-    wrapper.find('.step-edit-form__back-button').trigger('click');
-    expect(store.state.vqb.selectedStepIndex).toEqual(2);
-    wrapper.setProps({ isStepCreation: false });
-    wrapper.find('.step-edit-form__back-button').trigger('click');
-    expect(store.state.vqb.selectedStepIndex).toEqual(3);
-  });
+    ],
+    selectedStepIndex: 2,
+  })
 });
