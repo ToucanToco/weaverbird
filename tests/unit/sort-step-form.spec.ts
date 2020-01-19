e0d6ea120dc8981fd7734542db1a4f576ce7aa89
Adrien Di Mascio <adrien.dimascio@toucantoco.com>
2020-01-19 13:54:21 +0100

    [tests] use test runner in sort-step-form.spec.ts

--- tests/unit/sort-step-form.spec.ts
+++ tests/unit/sort-step-form.spec.ts
@@ -1,29 +1,72 @@
-import { mount, shallowMount, createLocalVue } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Vuex, { Store } from 'vuex';
 
 import SortStepForm from '@/components/stepforms/SortStepForm.vue';
-import { setupMockStore, RootState, ValidationError } from './utils';
-import { Pipeline } from '@/lib/steps';
+import { setupMockStore, BasicStepFormTestRunner, RootState } from './utils';
+
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
 describe('Sort Step Form', () => {
-  let emptyStore: Store<RootState>;
-  beforeEach(() => {
-    emptyStore = setupMockStore({});
+
+  const runner = new BasicStepFormTestRunner(SortStepForm, 'sort', localVue);
+  runner.testInstantiate();
+  runner.testExpectedComponents({ 'listwidget-stub': 1 });
+
+  runner.testValidationErrors([
+    {
+      testlabel: 'a column parameter is an empty string',
+      props: {
+        editedStep: {
+          name: 'sort',
+          columns: [{ column: '', order: 'desc' }],
+        },
+      },
+      errors: [{ keyword: 'minLength', dataPath: '.columns[0].column' }],
+    }
+  ]);
+
+  runner.testValidate(
+    {
+      testlabel: 'submitted data is validd',
+      data: {
+        editedStep: {
+          name: 'sort',
+          columns: [{ column: 'amazing', order: 'desc' }],
+        },
+      },
+    },
+    {
+      name: 'sort',
+      columns: [{ column: 'amazing', order: 'desc' }],
+    },
+  );
+
+  runner.testCancel({
+    pipeline: [
+      { name: 'domain', domain: 'foo' },
+      { name: 'rename', oldname: 'foo', newname: 'bar' },
+      { name: 'rename', oldname: 'baz', newname: 'spam' },
+      { name: 'rename', oldname: 'tic', newname: 'tac' },
+    ],
+    selectedStepIndex: 2,
   });
 
-  it('should instantiate', () => {
-    const wrapper = shallowMount(SortStepForm, { store: emptyStore, localVue, sync: false });
-    expect(wrapper.exists()).toBeTruthy();
+  runner.testResetSelectedIndex({
+    pipeline: [
+      { name: 'domain', domain: 'foo' },
+      { name: 'rename', oldname: 'foo', newname: 'bar' },
+      { name: 'rename', oldname: 'baz', newname: 'spam' },
+      { name: 'rename', oldname: 'tic', newname: 'tac' },
+    ],
+    selectedStepIndex: 2,
   });
 
   describe('ListWidget', () => {
-    it('should have one widgetList component', () => {
-      const wrapper = shallowMount(SortStepForm, { store: emptyStore, localVue, sync: false });
-      const widgetListWrapper = wrapper.findAll('listwidget-stub');
-      expect(widgetListWrapper.length).toEqual(1);
+    let emptyStore: Store<RootState>;
+    beforeEach(() => {
+      emptyStore = setupMockStore({});
     });
 
     it('should pass the defaultSortColumn props to widgetList', async () => {
@@ -45,85 +88,5 @@ describe('Sort Step Form', () => {
         { column: 'amazing', order: 'desc' },
       ]);
     });
-  });
-
-  describe('Validation', () => {
-    it('should report errors when a column parameter is an empty string', () => {
-      const wrapper = mount(SortStepForm, {
-        store: emptyStore,
-        localVue,
-        sync: false,
-      });
-      wrapper.setData({
-        editedStep: {
-          name: 'sort',
-          columns: [{ column: '', order: 'desc' }],
-        },
-      });
-      wrapper.find('.widget-form-action__button--validate').trigger('click');
-      // await localVue.nextTick();
-      const errors = wrapper.vm.$data.errors
-        .map((err: ValidationError) => ({ keyword: err.keyword, dataPath: err.dataPath }))
-        .sort((err1: ValidationError, err2: ValidationError) =>
-          err1.dataPath.localeCompare(err2.dataPath),
-        );
-      expect(errors).toEqual([{ keyword: 'minLength', dataPath: '.columns[0].column' }]);
-    });
-
-    it('should validate and emit "formSaved" when submitted data is valid', async () => {
-      const wrapper = mount(SortStepForm, { store: emptyStore, localVue, sync: false });
-      wrapper.setData({
-        editedStep: {
-          name: 'sort',
-          columns: [{ column: 'amazing', order: 'desc' }],
-        },
-      });
-      wrapper.find('.widget-form-action__button--validate').trigger('click');
-      await localVue.nextTick();
-      expect(wrapper.vm.$data.errors).toBeNull();
-      expect(wrapper.emitted()).toEqual({
-        formSaved: [
-          [
-            {
-              name: 'sort',
-              columns: [{ column: 'amazing', order: 'desc' }],
-            },
-          ],
-        ],
-      });
-    });
-  });
-
-  it('should emit "cancel" event when edition is cancelled', async () => {
-    const wrapper = mount(SortStepForm, { store: emptyStore, localVue, sync: false });
-    wrapper.find('.step-edit-form__back-button').trigger('click');
-    await localVue.nextTick();
-    expect(wrapper.emitted()).toEqual({
-      cancel: [[]],
-    });
-  });
-
-  it('should reset selectedStepIndex correctly on cancel depending on isStepCreation', () => {
-    const pipeline: Pipeline = [
-      { name: 'domain', domain: 'foo' },
-      { name: 'rename', oldname: 'foo', newname: 'bar' },
-      { name: 'rename', oldname: 'baz', newname: 'spam' },
-      { name: 'rename', oldname: 'tic', newname: 'tac' },
-    ];
-    const store = setupMockStore({
-      pipeline,
-      selectedStepIndex: 2,
-    });
-    const wrapper = mount(SortStepForm, {
-      store,
-      localVue,
-      propsData: { isStepCreation: true },
-      sync: false,
-    });
-    wrapper.find('.step-edit-form__back-button').trigger('click');
-    expect(store.state.vqb.selectedStepIndex).toEqual(2);
-    wrapper.setProps({ isStepCreation: false });
-    wrapper.find('.step-edit-form__back-button').trigger('click');
-    expect(store.state.vqb.selectedStepIndex).toEqual(3);
   });
 });
