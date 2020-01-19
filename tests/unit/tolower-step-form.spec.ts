511cd8642bfbcf9c263c631f5b525224c46a7da9
Adrien Di Mascio <adrien.dimascio@toucantoco.com>
2020-01-19 14:32:51 +0100

    [tests] use runner in tolower-step-form.spec.ts

--- tests/unit/tolower-step-form.spec.ts
+++ tests/unit/tolower-step-form.spec.ts
@@ -1,39 +1,23 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils';
-import Vuex, { Store } from 'vuex';
-
 import ToLowerStepForm from '@/components/stepforms/ToLowerStepForm.vue';
 
-import { setupMockStore, RootState } from './utils';
+import { BasicStepFormTestRunner } from './utils';
 
-const localVue = createLocalVue();
-localVue.use(Vuex);
-
-describe('To Uppercase Step Form', () => {
-  let emptyStore: Store<RootState>;
-  beforeEach(() => {
-    emptyStore = setupMockStore({});
-  });
-
-  it('should instantiate', () => {
-    const wrapper = shallowMount(ToLowerStepForm, { store: emptyStore, localVue });
-
-    expect(wrapper.exists()).toBeTruthy();
-  });
-
-  it('should have exactly 1 input component', () => {
-    const wrapper = shallowMount(ToLowerStepForm, { store: emptyStore, localVue });
-
-    expect(wrapper.findAll('columnpicker-stub').length).toEqual(1);
+describe('To Lowercase Step Form', () => {
+  const runner = new BasicStepFormTestRunner(ToLowerStepForm, 'lowercase');
+  runner.testInstantiate();
+  runner.testExpectedComponents({
+    'columnpicker-stub': 1,
   });
 
   it('should display step column on edition', () => {
-    const wrapper = shallowMount(ToLowerStepForm, {
-      propsData: {
-        initialStepValue: { name: 'lowercase', column: 'foo' },
+    const wrapper = runner.shallowMount(
+      {},
+      {
+        propsData: {
+          initialStepValue: { name: 'lowercase', column: 'foo' },
+        },
       },
-      store: emptyStore,
-      localVue,
-    });
+    );
     const columnPicker = wrapper.find('columnpicker-stub');
     expect(columnPicker.attributes('value')).toEqual('foo');
   });
