969e786aded0b975701ea3fe15f5946b8d49c6d1
Adrien Di Mascio <adrien.dimascio@toucantoco.com>
2020-01-27 13:28:02 +0100

    [config] extract eslint in its own files
    It will be easier to maintain, read, bisect out of the package.json file.
    
    Update to last versions of eslint, eslint-typescript-plugin and upgrade
    corresponding rules where required (e.g. `@typescript-eslint/explicit-member-accessibility`,
    or `@typescript-eslint/prefer-interface`).
    
    Add import plugin to be able to sort imports

---
+++ .eslintrc.js
@@ -0,0 +1,115 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/typescript",
+    "import",
+  ],
+  settings: {
+    "import/internal-regex": "^@/",
+    "import/resolver": {
+      "alias": {
+        "map": [
+          [
+            "@",
+            "./src"
+          ]
+        ],
+        "extensions": [
+          ".ts",
+          ".js",
+          ".vue",
+          ".d.ts"
+        ]
+      }
+    }
+  },
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-function": [
+      "error",
+      {
+        "allow": [
+          "arrowFunctions"
+        ]
+      }
+    ],
+    "@typescript-eslint/explicit-member-accessibility": [
+      "error", { "accessibility": "no-public" }
+    ],
+    "@typescript-eslint/indent": [
+      "error",
+      2
+    ],
+    "@typescript-eslint/camelcase": [
+      "error",
+      {
+        "properties": "never"
+      }
+    ],
+    "@typescript-eslint/prefer-interface": "off",
+    "no-unused-vars": [
+      "error",
+      {
+        "argsIgnorePattern": "^_"
+      }
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        "argsIgnorePattern": "^_"
+      }
+    ],
+    "@typescript-eslint/no-unused-vars-experimental": ["error"],
+    "space-before-function-paren": [
+      "error",
+      {
+        "anonymous": "never",
+        "named": "never",
+        "asyncArrow": "always"
+      }
+    ],
+    "prefer-const": [
+      "error"
+    ],
+    "no-console": [
+      "error",
+      {
+        "allow": [
+          "warn",
+          "error"
+        ]
+      }
+    ],
+    "vue/html-self-closing": [
+      "error",
+      {
+        "html": {
+          "void": "never",
+          "normal": "always",
+          "component": "always"
+        },
+        "svg": "always",
+        "math": "always"
+      }
+    ],
+    "vue/return-in-computed-property": ["error", { treatUndefinedAsUnspecified: false }],
+    "simple-import-sort/sort": "error",
+  },
+
+  plugins: ['import', 'simple-import-sort', 'vue'],
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    project: 'tsconfig.json',
+    extraFileExtensions: ['.vue'],
+  }
+};
