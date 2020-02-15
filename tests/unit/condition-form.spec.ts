9b78dce88768e1bb4f3ad0324980d6971e29cdea
David Nowinsky <david@nowinsky.net>
2020-02-15 15:17:51 +0100

    Remove debounce for ConditionForm updates

--- tests/unit/condition-form.spec.ts
+++ tests/unit/condition-form.spec.ts
@@ -3,21 +3,18 @@ import { shallowMount } from '@vue/test-utils';
 import ConditionForm from '@/components/stepforms/ConditionsEditor/ConditionForm.vue';
 
 describe('ConditionForm', () => {
-  jest.useFakeTimers();
-
   it('should instantiate', () => {
     const wrapper = shallowMount(ConditionForm);
     expect(wrapper.exists()).toBeTruthy();
   });
 
-  it.skip('should emit "conditionUpdated" event with the right args', async () => {
+  it('should emit "conditionUpdated" event with the right args', async () => {
     const wrapper = shallowMount(ConditionForm, {
       propsData: {
         condition: { column: '', operator: 'eq', value: '' },
       },
     });
     wrapper.find('.condition-form__input--column').setValue('my_col');
-    jest.advanceTimersByTime(500); // flush debounce
     expect(wrapper.emitted().conditionUpdated).toBeDefined();
     expect(wrapper.emitted().conditionUpdated[0]).toEqual([
       { column: 'my_col', operator: 'eq', value: '' },
