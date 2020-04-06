b575483a5fd243c70c60a9f7fcc8a4dbb2884493
raphael <raphael.huille@toucantoco.com>
2020-04-06 14:51:48 +0200

    New component: ListUniqueValue This component display a column's unique values. It allows to search among values and to select and unselect values. Notes: This is a visual component, it does not trigger any action nor mutationhimself. It is meant to be used inside "ActionMenu".
    This commit introduce `ListUniqueValue` component, tests and a storybook. For now, the component is not used inside the project.

---
+++ stories/list-unique-values.js
@@ -0,0 +1,22 @@
+import { storiesOf } from '@storybook/vue';
+
+import { ListUniqueValues } from '../dist/storybook/components';
+const stories = storiesOf('ListUniqueValues', module);
+
+stories.add('default', () => ({
+  components: { ListUniqueValues },
+  data(){
+    return {
+        options: [
+            {value: 'France', count: 12},
+            {value: 'UK', count: 1},
+            {value: 'China', count: 14},
+            {value: 'Framboise', count: 10},
+            {value: 'Italy', count: 23},
+        ],
+        filter: { column: "col1", operator: "in", value: []},
+        loaded: false,
+    }
+  },
+  template: `<div style="font-family: 'Montserrat', sans-serif;"><ListUniqueValues :loaded="loaded" :filter="filter" @input="filter = $event" :options="options"/></div>`,
+}));
