d711c4310be51e0e08dfbb7eedc1e25466a454f7
raphael <raphael.huille@toucantoco.com>
2020-04-26 18:00:58 +0200

    New action to load a column unique values from the whole database in backend. It add to the active pipeline an aggregation step on the requested column to count its unique values. Then, the result is processed by the new dataset helper `updateLocalUniquesFromDatabase` and inserted into the dataset headers.
    Also, this action needs to notify the store that a column is loading. I updated the  action decorator "loading" to make it take into account the "loading column" case.

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,24 +1,38 @@
 import { ActionContext, ActionTree } from 'vuex';
 
-import { addLocalUniquesToDataset } from '@/lib/dataset/helpers.ts';
+import { addLocalUniquesToDataset, updateLocalUniquesFromDatabase } from '@/lib/dataset/helpers.ts';
 import { pageOffset } from '@/lib/dataset/pagination';
+import { Pipeline } from '@/lib/steps';
 
 import { backendService } from './backend-plugin';
 import { preparePipeline, VQBState } from './state';
+
 /**
  * Action wrapper so that the state loading is set to true at the start of
- *
+ * In weaverbird there are two loading states:
+ * - a "global" loading state which means that the whole dataset is currently loading
+ * - a "column" loading state which means that unique values of this column are currently loading
  */
-function loading(_target: any, _propertyKey: string, descriptor: PropertyDescriptor) {
-  const originalMethod = descriptor.value;
+function loading(column = false) {
+  return function loading(_target: any, _propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
 
-  descriptor.value = async function(context: ActionContext<VQBState, any>, ...args: any[]) {
-    context.commit('setLoading', { isLoading: true });
-    await originalMethod(context, ...args);
-    context.commit('setLoading', { isLoading: false });
+    if (!column) {
+      descriptor.value = async function(context: ActionContext<VQBState, any>, ...args: any[]) {
+        context.commit('setLoading', { isLoading: true });
+        await originalMethod(context, ...args);
+        context.commit('setLoading', { isLoading: false });
+      };
+    } else {
+      descriptor.value = async function(context: ActionContext<VQBState, any>, ...args: any[]) {
+        context.commit('setUniqueValuesLoading', { isLoading: true, column: args[0].column });
+        await originalMethod(context, ...args);
+        context.commit('setUniqueValuesLoading', { isLoading: false, column: args[0].column });
+      };
+    }
+
+    return descriptor;
   };
-
-  return descriptor;
 }
 
 /**
@@ -33,7 +47,7 @@ class Actions {
     context.commit('selectStep', { index: -1 });
   }
 
-  @loading
+  @loading()
   async updateDataset(context: ActionContext<VQBState, any>) {
     const response = await backendService.executePipeline(
       preparePipeline(context.getters.activePipeline, context.state),
@@ -43,6 +57,46 @@ class Actions {
     if (!response.error) {
       context.commit('setDataset', {
         dataset: addLocalUniquesToDataset(response.data),
+      });
+    }
+  }
+
+  /**
+   * Call backend with a special pipeline to retrieve unique values of the requested column.
+   * The current pipeline is completed with a "count aggregation" on the requested column.
+   * The result is loaded in store.
+   */
+  @loading(true)
+  async loadColumnUniqueValues(
+    context: ActionContext<VQBState, any>,
+    { column }: { column: string },
+  ) {
+    const activePipeline = context.getters.activePipeline;
+    if (!activePipeline || !activePipeline.length) {
+      return;
+    }
+    const loadUniqueValuesPipeline: Pipeline = [
+      ...activePipeline,
+      {
+        name: 'aggregate',
+        aggregations: [
+          {
+            column,
+            aggfunction: 'count',
+            newcolumn: '__vqb_count__',
+          },
+        ],
+        on: [column],
+      },
+    ];
+    const response = await backendService.executePipeline(
+      preparePipeline(loadUniqueValuesPipeline, context.state),
+      10000, // FIXME: limit is hard-coded
+      0,
+    );
+    if (!response.error) {
+      context.commit('setDataset', {
+        dataset: updateLocalUniquesFromDatabase(context.state.dataset, response.data),
       });
     }
   }
