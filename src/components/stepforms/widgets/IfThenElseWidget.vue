37f370c1951b9e405388d6f05fc4d0c1b58a6972
Éric Renard <timmy469@gmail.com>
2020-07-10 17:57:57 +0200

    fix: ifthenelse style

--- src/components/stepforms/widgets/IfThenElseWidget.vue
+++ src/components/stepforms/widgets/IfThenElseWidget.vue
@@ -1,43 +1,63 @@
 <template>
   <div class="ifthenelse-widget">
-    <div class="ifthenelse-container">
-      <div class="ifthenelse-widget__if">{{ isRoot ? 'IF' : 'ELSE IF' }}</div>
-      <FilterEditor
-        :filter-tree="value.if"
-        :errors="errors"
-        :data-path="`${dataPath}.if`"
-        @filterTreeUpdated="updateFilterTree"
-      />
-      <div class="ifthenelse-widget__then">THEN</div>
-      <InputTextWidget
-        class="formulaInput"
-        :value="value.then"
-        @input="updateThenFormula"
-        placeholder='Enter a "Text" with quotes, or a formula'
-        :data-path="`${dataPath}.then`"
-        :errors="errors"
-      />
-      <div v-if="elseMode === 'ELSE:'" class="ifthenelse-widget__else">ELSE</div>
-      <AutocompleteWidget
-        class="ifthenelse-widget__else"
-        v-model="elseMode"
-        @input="updateElseMode"
-        :options="elseModes"
-      />
-      <InputTextWidget
-        v-if="elseMode === 'ELSE:'"
-        class="formulaInput"
-        :value="value.else"
-        @input="updateElseFormula"
-        placeholder='Enter a "Text" with quotes, or a formula'
-        :data-path="`${dataPath}.else`"
-        :errors="errors"
-      />
+    <div class="ifthenelse-widget__container">
+      <div class="ifthenelse-widget__row">
+        <div class="ifthenelse-widget__cell">
+          <div class="ifthenelse-widget__if">{{ isRoot ? 'IF' : 'ELSE IF' }}</div>
+        </div>
+      </div>
+      <div class="ifthenelse-widget__row">
+        <div class="ifthenelse-widget__cell">
+          <FilterEditor
+            :filter-tree="value.if"
+            :errors="errors"
+            :data-path="`${dataPath}.if`"
+            @filterTreeUpdated="updateFilterTree"
+          />
+        </div>
+      </div>
+      <div class="ifthenelse-widget__row ifthenelse-widget__row--step">
+        <div class="ifthenelse-widget__cell">
+          <div class="ifthenelse-widget__then">THEN</div>
+        </div>
+      </div>
+      <div class="ifthenelse-widget__row">
+        <div class="ifthenelse-widget__cell">
+          <InputTextWidget
+            class="ifthenelse-widget__input"
+            :value="value.then"
+            @input="updateThenFormula"
+            placeholder='Enter a "Text" with quotes, or a formula'
+            :data-path="`${dataPath}.then`"
+            :errors="errors"
+          />
+        </div>
+      </div>
+      <template v-if="elseMode === 'ELSE:'">
+        <div class="ifthenelse-widget__row ifthenelse-widget__row--step">
+          <div class="ifthenelse-widget__cell">
+            <div class="ifthenelse-widget__else">ELSE</div>
+          </div>
+        </div>
+        <div class="ifthenelse-widget__row">
+          <div class="ifthenelse-widget__cell">
+            <InputTextWidget
+              class="ifthenelse-widget__input"
+              :value="value.else"
+              @input="updateElseFormula"
+              placeholder='Enter a "Text" with quotes, or a formula'
+              :data-path="`${dataPath}.else`"
+              :errors="errors"
+            />
+          </div>
+        </div>
+      </template>
     </div>
     <ifthenelse-widget
       v-if="elseMode === 'ELSE IF:'"
       :value="value.else"
       @input="updateElseObject"
+      @delete="updateElseMode('ELSE:')"
       :data-path="`${dataPath}.else`"
       :errors="errors"
     />
@@ -156,6 +176,25 @@ export default class IfThenElseWidget extends Vue {
 }
 </style>
 <style lang="scss">
+%ifthenelse-widget__text {
+  font-family: Montserrat;
+  font-weight: 600;
+  letter-spacing: 0.25px;
+  text-transform: uppercase;
+  color: #2e69a3;
+}
+
+%ifthenelse-widget__tag {
+  @extend %ifthenelse-widget__text;
+  font-size: 12px;
+  line-height: 1.67;
+  display: inline-flex;
+  border-radius: 2px;
+  padding: 0 6px;
+  background-color: #e2ebf5;
+  margin-bottom: 10px;
+}
+
 @import '../../../styles/_variables';
 
 .filter-form {
@@ -178,44 +217,59 @@ export default class IfThenElseWidget extends Vue {
   }
 }
 
-.formulaInput {
-  @extend %form-widget__container;
-  margin-top: 10px;
-  margin-bottom: 0px;
+.ifthenelse-widget {
+  .ifthenelse-widget__input {
+    margin: 0;
+    background-color: white;
+  }
+  .conditions-group {
+    padding-bottom: 0;
+    &:not(.conditions-group--with-switch) {
+      padding-top: 0;
+    }
+  }
 }
 
-.ifthenelse-container {
-  border-left: 3px solid $active-color-faded;
-  padding: 5px 0px 5px 10px;
-  margin-bottom: 10px;
+.ifthenelse-widget__container {
+  margin-bottom: 15px;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: #f9fbfc;
 }
 
-%ifthenelse-widget__tag {
-  font-family: Montserrat;
-  font-size: 12px;
-  font-weight: 600;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: 1.67;
-  letter-spacing: normal;
-  text-transform: uppercase;
-  color: #2e69a3;
-  display: inline-flex;
-  border-radius: 2px;
-  padding: 0 6px;
-  background-color: #d7e5f3;
+.ifthenelse-widget__row {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 10px;
 }
 
+.ifthenelse-widget__row--step {
+  padding-top: 15px;
+}
+
+.ifthenelse-widget__cell {
+  flex: 1;
+}
+
+.ifthenelse-widget__if,
+.ifthenelse-widget__then,
 .ifthenelse-widget__else {
   @extend %ifthenelse-widget__tag;
-  margin-bottom: 0px;
 }
 
 .ifthenelse-widget__if {
-  @extend %ifthenelse-widget__tag;
+  margin-bottom: 0px;
 }
 
-.ifthenelse-widget__then {
-  @extend %ifthenelse-widget__tag;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
