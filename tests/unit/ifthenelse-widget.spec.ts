bbf911d3d8eb5ff28615a85f5b526b40acaae65f
alice.sevin <alice.sevin@toucantoco.com>
2020-07-22 12:46:30 +0200

    fix: change update to add/remove formula

--- tests/unit/ifthenelse-widget.spec.ts
+++ tests/unit/ifthenelse-widget.spec.ts
@@ -8,14 +8,12 @@ describe('IfThenElseWidget', () => {
     expect(wrapper.exists()).toBeTruthy();
   });
 
-  it('should have exactly 4 input components by default', () => {
+  it('should have exactly 3 input components by default', () => {
     const wrapper = shallowMount(IfThenElseWidget);
     const filtereditorWrappers = wrapper.findAll('filtereditor-stub');
     expect(filtereditorWrappers.length).toEqual(1);
     const inputtextWrappers = wrapper.findAll('inputtextwidget-stub');
     expect(inputtextWrappers.length).toEqual(2);
-    const autocompleteWrappers = wrapper.findAll('autocompletewidget-stub');
-    expect(autocompleteWrappers.length).toEqual(1);
   });
 
   it('should be able to nest itself', () => {
@@ -32,8 +30,6 @@ describe('IfThenElseWidget', () => {
     expect(filtereditorWrappers.length).toEqual(1);
     const inputtextWrappers = wrapper.findAll('inputtextwidget-stub');
     expect(inputtextWrappers.length).toEqual(1);
-    const autocompleteWrappers = wrapper.findAll('autocompletewidget-stub');
-    expect(autocompleteWrappers.length).toEqual(1);
     const testWrappers = wrapper.findAll('ifthenelse-widget-stub');
     expect(testWrappers.length).toEqual(1);
   });
@@ -85,9 +81,45 @@ describe('IfThenElseWidget', () => {
     ]);
   });
 
-  it('should reset the "else" value properly depending on the "else" mode', () => {
+  it('should add nested formula when clicking on add button', () => {
     const wrapper = shallowMount(IfThenElseWidget);
-    wrapper.find('autocompletewidget-stub').vm.$emit('input', 'ELSE:');
+    const button = wrapper.findAll('.ifthenelse-widget__add');
+    expect(button.length).toEqual(1);
+    button.trigger('click');
+    expect(wrapper.emitted().input).toBeDefined();
+    expect(wrapper.emitted().input[0]).toEqual([
+      {
+        if: { column: '', value: '', operator: 'eq' },
+        then: '',
+        else: { if: { column: '', value: '', operator: 'eq' }, then: '', else: '' },
+      },
+    ]);
+  });
+
+  it('should not have delete button if root', () => {
+    const wrapper = shallowMount(IfThenElseWidget, { propsData: { isRoot: true } });
+    expect(wrapper.findAll('.ifthenelse-widget__remove').length).toEqual(0);
+  });
+
+  it('should emit delete nested formula when clicking on delete button', () => {
+    const wrapper = shallowMount(IfThenElseWidget, { propsData: { isRoot: false } });
+    const button = wrapper.findAll('.ifthenelse-widget__remove');
+    expect(button.length).toEqual(1);
+    button.trigger('click');
+    expect(wrapper.emitted().delete).toBeDefined();
+  });
+
+  it('should delete nested formula when a nested emitted delete', () => {
+    const wrapper = shallowMount(IfThenElseWidget, {
+      propsData: {
+        value: {
+          if: { column: '', value: '', operator: 'eq' },
+          then: '',
+          else: { if: { column: '', value: '', operator: 'eq' }, then: '', else: '' },
+        },
+      },
+    });
+    wrapper.find('ifthenelse-widget-stub').vm.$emit('delete');
     expect(wrapper.emitted().input).toBeDefined();
     expect(wrapper.emitted().input[0]).toEqual([
       {
@@ -96,23 +128,9 @@ describe('IfThenElseWidget', () => {
         else: '',
       },
     ]);
-
-    wrapper.find('autocompletewidget-stub').vm.$emit('input', 'ELSE IF:');
-    expect(wrapper.emitted().input).toBeDefined();
-    expect(wrapper.emitted().input[1]).toEqual([
-      {
-        if: { column: '', value: '', operator: 'eq' },
-        then: '',
-        else: {
-          if: { column: '', value: '', operator: 'eq' },
-          then: '',
-          else: '',
-        },
-      },
-    ]);
   });
 
-  it('should emit the right value when a nsted "else" is updated', () => {
+  it('should emit the right value when a nested "else" is updated', () => {
     const wrapper = shallowMount(IfThenElseWidget, {
       propsData: {
         value: {
