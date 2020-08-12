af32a1899899a05d99f596a7f6846bf4ecf87721
alice.sevin <alice.sevin@toucantoco.com>
2020-08-12 13:20:09 +0200

    chore: export variable tag in SFC

---
+++ src/components/stepforms/widgets/VariableInputs/VariableTag.vue
@@ -0,0 +1,104 @@
+<template>
+  <div
+    class="widget-variable__tag"
+    v-tooltip="{
+      targetClasses: 'has-weaverbird__tooltip',
+      classes: 'weaverbird__tooltip',
+      content: variableValue,
+      placement: 'bottom-center',
+    }"
+  >
+    <span class="widget-variable__tag-icon">{}</span>
+    <span class="widget-variable__tag-name">{{ variableLabel }}</span>
+    <i class="widget-variable__tag-close fa fa-times" @click.stop="removeVariableTag" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+import { extractVariableIdentifier, VariableDelimiters, VariablesBucket } from '@/lib/variables';
+
+/**
+ * This component wraps an input of any type and allow replacing its value by a variable chosen from a list or an
+ * expression.
+ */
+@Component({
+  name: 'variable-input',
+})
+export default class VariableInput extends Vue {
+  @Prop()
+  value!: string;
+
+  @Prop({ default: () => [] })
+  availableVariables!: VariablesBucket;
+
+  @Prop({ default: () => ({ start: '{{', end: '}}' }) })
+  variableDelimiters!: VariableDelimiters;
+
+  get variableIdentifier() {
+    return extractVariableIdentifier(this.value, this.variableDelimiters);
+  }
+
+  /**
+   * Retrieve variable in available variables by identifier.
+   */
+  get variable() {
+    return this.availableVariables.find(aV => aV.identifier === this.variableIdentifier);
+  }
+
+  /**
+   * Display label rather than identifier if available.
+   */
+  get variableLabel() {
+    if (this.variable) {
+      return this.variable.label;
+    } else {
+      return this.variableIdentifier;
+    }
+  }
+
+  /**
+   * Display as list if value is an array.
+   */
+  get variableValue() {
+    const value = this.variable?.value || '';
+    return Array.isArray(value) ? value.join(', ') : value;
+  }
+
+  removeVariableTag() {
+    this.$emit('removed');
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../../../../styles/utils/v-tooltip.scss';
+</style>
+
+<style scoped lang="scss">
+.widget-variable__tag {
+  border-radius: 4px;
+  background-color: rgba(42, 102, 161, 0.05);
+  padding: 0 10px;
+  color: #2a66a1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.widget-variable__tag-close {
+  cursor: pointer;
+}
+
+.widget-variable__tag-icon {
+  margin-right: 1em;
+}
+
+.widget-variable__tag-name {
+  flex: 1;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+</style>
---
+++ tests/unit/variable-tag.spec.ts
@@ -0,0 +1,91 @@
+import { shallowMount, Wrapper } from '@vue/test-utils';
+import { VTooltip } from 'v-tooltip';
+
+import VariableTag from '@/components/stepforms/widgets/VariableInputs/VariableTag.vue';
+
+describe('Variable Tag', () => {
+  let wrapper: Wrapper<VariableTag>;
+
+  beforeEach(() => {
+    wrapper = shallowMount(VariableTag, {
+      sync: false,
+      directives: {
+        tooltip: VTooltip,
+      },
+      propsData: {
+        value: '{{ appRequesters.view }}',
+        variableDelimiters: { start: '{{', end: '}}' },
+        availableVariables: [
+          {
+            category: 'App variables',
+            label: 'view',
+            identifier: 'appRequesters.view',
+            value: 'Product 123',
+          },
+          {
+            category: 'App variables',
+            label: 'date.month',
+            identifier: 'appRequesters.date.month',
+            value: 'Apr',
+          },
+        ],
+      },
+    });
+  });
+
+  it('should instantiate', () => {
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  it('should compute the variable identifier', () => {
+    expect((wrapper as any).vm.variableIdentifier).toBe('appRequesters.view');
+  });
+
+  it('should compute the variable', () => {
+    expect((wrapper as any).vm.variable).toStrictEqual({
+      category: 'App variables',
+      label: 'view',
+      identifier: 'appRequesters.view',
+      value: 'Product 123',
+    });
+  });
+
+  it('should compute the variable value', () => {
+    expect((wrapper as any).vm.variableValue).toBe('Product 123');
+  });
+
+  it('should display a tooltip on hover', () => {
+    expect(wrapper.classes()).toContain('has-weaverbird__tooltip');
+  });
+
+  it('should display the tag label', () => {
+    const name = wrapper.find('.widget-variable__tag-name');
+    expect(name.exists()).toBe(true);
+    expect(name.text()).toBe('view');
+  });
+
+  it('should display the remove button', () => {
+    expect(wrapper.find('.widget-variable__tag-close').exists()).toBe(true);
+  });
+
+  it('should remove the tag when clicking on the remove button', async () => {
+    const removeButton = wrapper.find('.widget-variable__tag-close');
+    removeButton.trigger('click');
+    await wrapper.vm.$nextTick();
+    expect(wrapper.emitted().removed).toBeTruthy();
+  });
+
+  describe('if the variable is not listed in available variables', () => {
+    beforeEach(() => {
+      wrapper.setProps({
+        value: '{{ toto }}',
+      });
+    });
+    it('should display the identifier instead of the human-friendly label', () => {
+      expect(wrapper.find('.widget-variable__tag-name').text()).toBe('toto');
+    });
+    it('should display nothing instead of the human-friendly value', () => {
+      expect((wrapper as any).vm.variableValue).toBe('');
+    });
+  });
+});
