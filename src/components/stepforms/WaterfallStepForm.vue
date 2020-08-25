93e68cb16a5a39b89947053d769ef1c6ef898846
vincent_d <vincent.destrait@toucantoco.com>
2020-08-25 19:20:53 +0200

    waterfall step :: add UI

---
+++ src/components/stepforms/WaterfallStepForm.vue
@@ -0,0 +1,143 @@
+<template>
+  <div>
+    <StepFormHeader :title="title" :stepName="this.editedStep.name" />
+    <ColumnPicker
+      class="valueColumnInput"
+      v-model="editedStep.valueColumn"
+      name="Value column name:"
+      placeholder="Select a column"
+      data-path=".valueColumn"
+      :syncWithSelectedColumn="false"
+      :errors="errors"
+      :available-variables="availableVariables"
+      :variable-delimiters="variableDelimiters"
+    />
+    <ColumnPicker
+      class="milestonesColumnInput"
+      v-model="editedStep.milestonesColumn"
+      name="Column incl. start and end labels (usually dates):"
+      placeholder="Select a column"
+      data-path=".milestonesColumn"
+      :syncWithSelectedColumn="false"
+      :errors="errors"
+      :available-variables="availableVariables"
+      :variable-delimiters="variableDelimiters"
+    />
+    <InputTextWidget
+      class="startInput"
+      v-model="editedStep.start"
+      name="Starting block label:"
+      placeholder="To be found in the column above"
+      data-path=".start"
+      :errors="errors"
+      :available-variables="availableVariables"
+      :variable-delimiters="variableDelimiters"
+    />
+    <InputTextWidget
+      class="endInput"
+      v-model="editedStep.end"
+      name="Ending block label:"
+      placeholder="To be found in the column above"
+      data-path=".end"
+      :errors="errors"
+      :available-variables="availableVariables"
+      :variable-delimiters="variableDelimiters"
+    />
+    <ColumnPicker
+      class="childrenColumnInput"
+      v-model="editedStep.labelsColumn"
+      name="Labels columns (for intermediate blocks):"
+      placeholder="Select a column"
+      data-path=".labelsColumn"
+      :syncWithSelectedColumn="false"
+      :errors="errors"
+      :available-variables="availableVariables"
+      :variable-delimiters="variableDelimiters"
+    />
+    <ColumnPicker
+      class="parentsColumnInput"
+      v-model="editedStep.parentsColumn"
+      name="(Optional) Parents labels column (for drill-down):"
+      placeholder="Select a column"
+      data-path=".parentsColumn"
+      :syncWithSelectedColumn="false"
+      :errors="errors"
+      :available-variables="availableVariables"
+      :variable-delimiters="variableDelimiters"
+    />
+    <MultiselectWidget
+      class="groupbyInput"
+      v-model="editedStep.groupby"
+      name="(Optional) Group waterfall by:"
+      :options="columnNames"
+      placeholder="Add columns"
+      data-path=".groupby"
+      :errors="errors"
+      :available-variables="availableVariables"
+      :variable-delimiters="variableDelimiters"
+    />
+    <AutocompleteWidget
+      class="sortByInput"
+      name="Sort by:"
+      v-model="editedStep.sortBy"
+      :options="['label', 'value']"
+      data-path=".sortBy"
+    />
+    <AutocompleteWidget
+      class="orderInput"
+      name="Sort order:"
+      v-model="editedStep.order"
+      :options="['asc', 'desc']"
+      data-path=".order"
+    />
+    <StepFormButtonbar />
+  </div>
+</template>
+
+<script lang="ts">
+import { Prop } from 'vue-property-decorator';
+
+import { StepFormComponent } from '@/components/formlib';
+import AutocompleteWidget from '@/components/stepforms/widgets/Autocomplete.vue';
+import InputTextWidget from '@/components/stepforms/widgets/InputText.vue';
+import { WaterfallStep } from '@/lib/steps';
+import { VariableDelimiters, VariablesBucket } from '@/lib/variables';
+import { VQBModule } from '@/store';
+
+import ColumnPicker from './ColumnPicker.vue';
+import BaseStepForm from './StepForm.vue';
+import MultiselectWidget from './widgets/Multiselect.vue';
+
+@StepFormComponent({
+  vqbstep: 'waterfall',
+  name: 'waterfall-step-form',
+  components: {
+    AutocompleteWidget,
+    ColumnPicker,
+    InputTextWidget,
+    MultiselectWidget,
+  },
+})
+export default class WaterfallStepForm extends BaseStepForm<WaterfallStep> {
+  @VQBModule.State availableVariables!: VariablesBucket;
+
+  @VQBModule.State variableDelimiters!: VariableDelimiters;
+
+  @Prop({
+    type: Object,
+    default: () => ({
+      name: 'waterfall',
+      valueColumn: '',
+      milestonesColumn: '',
+      start: '',
+      end: '',
+      labelsColumn: '',
+      sortBy: 'value',
+      order: 'desc',
+    }),
+  })
+  initialStepValue!: WaterfallStep;
+
+  readonly title: string = 'Compute waterfall';
+}
+</script>
---
+++ tests/unit/waterfall-step-form.spec.ts
@@ -0,0 +1,129 @@
+import WaterfallStepForm from '@/components/stepforms/WaterfallStepForm.vue';
+
+import { BasicStepFormTestRunner, setupMockStore } from './utils';
+
+describe('Cumsum Step Form', () => {
+  const runner = new BasicStepFormTestRunner(WaterfallStepForm, 'waterfall');
+  runner.testInstantiate();
+  runner.testExpectedComponents({
+    'autocompletewidget-stub': 2,
+    'columnpicker-stub': 4,
+    'multiselectwidget-stub': 1,
+    'inputtextwidget-stub': 2,
+  });
+
+  describe('MultiselectWidget', () => {
+    it('should instantiate a MultiselectWidget widget with proper options from the store', () => {
+      const initialState = {
+        dataset: {
+          headers: [{ name: 'columnA' }, { name: 'columnB' }, { name: 'columnC' }],
+          data: [],
+        },
+      };
+      const wrapper = runner.shallowMount(initialState);
+      const widgetMultiselect = wrapper.find('multiselectwidget-stub');
+      expect(widgetMultiselect.attributes('options')).toEqual('columnA,columnB,columnC');
+    });
+
+    it('should pass down the props to the MultiselectWidget value prop', async () => {
+      const wrapper = runner.shallowMount(undefined, {
+        data: {
+          editedStep: {
+            name: 'waterfall',
+            valueColumn: 'VALUE',
+            milestonesColumn: 'DATE',
+            start: '2019',
+            end: '2020',
+            labelsColumn: 'PRODUCT',
+            groupby: ['COUNTRY'],
+            sortBy: 'value',
+            order: 'desc',
+          },
+        },
+      });
+      await wrapper.vm.$nextTick();
+      const widgetMultiselect = wrapper.find('multiselectwidget-stub');
+      expect(widgetMultiselect.props().value).toEqual(['COUNTRY']);
+    });
+  });
+
+  describe('Validation', () => {
+    runner.testValidationErrors([
+      {
+        testlabel: '"groupby" parameter is an empty string',
+        props: {
+          initialStepValue: {
+            name: 'waterfall',
+            valueColumn: 'VALUE',
+            milestonesColumn: 'DATE',
+            start: '2019',
+            end: '2020',
+            labelsColumn: 'PRODUCT',
+            groupby: [''],
+            sortBy: 'value',
+            order: 'desc',
+          },
+        },
+        errors: [{ keyword: 'minLength', dataPath: '.groupby[0]' }],
+      },
+      {
+        testlabel: '"start" parameter is an empty string',
+        props: {
+          initialStepValue: {
+            name: 'waterfall',
+            valueColumn: 'VALUE',
+            milestonesColumn: 'DATE',
+            start: '',
+            end: '2020',
+            labelsColumn: 'PRODUCT',
+            sortBy: 'value',
+            order: 'desc',
+          },
+        },
+        errors: [{ keyword: 'minLength', dataPath: '.start' }],
+      },
+      {
+        testlabel: '"end" parameter is an empty string',
+        props: {
+          initialStepValue: {
+            name: 'waterfall',
+            valueColumn: 'VALUE',
+            milestonesColumn: 'DATE',
+            start: '2019',
+            end: '',
+            labelsColumn: 'PRODUCT',
+            sortBy: 'value',
+            order: 'desc',
+          },
+        },
+        errors: [{ keyword: 'minLength', dataPath: '.end' }],
+      },
+    ]);
+
+    runner.testValidate({
+      testlabel: 'submitted data is valid',
+      store: setupMockStore({
+        dataset: {
+          headers: [{ name: 'columnA' }],
+          data: [],
+        },
+      }),
+      props: {
+        initialStepValue: {
+          name: 'waterfall',
+          valueColumn: 'VALUE',
+          milestonesColumn: 'DATE',
+          start: '2019',
+          end: '2020',
+          labelsColumn: 'PRODUCT',
+          groupby: ['COUNTRY'],
+          sortBy: 'value',
+          order: 'desc',
+        },
+      },
+    });
+  });
+
+  runner.testCancel();
+  runner.testResetSelectedIndex();
+});
