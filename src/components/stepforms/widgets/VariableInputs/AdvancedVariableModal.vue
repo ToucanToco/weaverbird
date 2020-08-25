ad36e0c52efb839579c91157f97bd4580e422b08
alice.sevin <alice.sevin@toucantoco.com>
2020-08-25 13:31:56 +0200

    feat(variables): add an advanced variable modal component

---
+++ src/components/stepforms/widgets/VariableInputs/AdvancedVariableModal.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="vqb-modal" v-if="isOpened">
+    <div class="vqb-modal__backdrop" />
+    <div class="vqb-modal__container">
+      <div class="vqb-modal__body" style="width: 500px;">
+        <i class="vqb-modal__close fas fa-times" @click="close" />
+        <div class="vqb-modal__header">
+          <div class="vqb-modal__title">Advanced variable</div>
+        </div>
+        <div class="vqb-modal__section" />
+        <div class="vqb-modal__footer">
+          <div class="vqb-modal__action vqb-modal__action--secondary" @click="close">
+            cancel
+          </div>
+          <div class="vqb-modal__action vqb-modal__action--primary" @click="confirm">
+            confirm
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+/**
+ * This component allow to add an advanced variable
+ */
+@Component({
+  name: 'advanced-variable-modal',
+})
+export default class AdvancedVariableModal extends Vue {
+  @Prop({ default: false })
+  isOpened!: boolean;
+
+  close() {
+    this.$emit('closed');
+  }
+
+  confirm() {
+    this.close();
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.vqb-modal {
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  left: 0;
+  overflow: auto;
+  position: fixed;
+  right: 0;
+  top: 0;
+  z-index: 999;
+}
+
+.vqb-modal__backdrop {
+  background-color: rgba(0, 0, 0, 0.54);
+  bottom: 0;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 0;
+}
+
+.vqb-modal__container {
+  align-items: center;
+  display: flex;
+  flex-shrink: 0;
+  justify-content: center;
+  min-height: 100%;
+  padding: 40px 40px 20px;
+  width: 100%;
+}
+
+.vqb-modal__body {
+  background-color: #fff;
+  border-radius: 20px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  position: relative;
+}
+
+.vqb-modal__close {
+  color: #4c4c4c;
+  font-size: 30px;
+  position: absolute;
+  top: 15px;
+  right: 30px;
+}
+
+.fa-times {
+  cursor: pointer;
+}
+
+.vqb-modal__header {
+  background-color: #f5f5f5;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  display: flex;
+  padding: 20px 30px;
+}
+
+.vqb-modal__title {
+  font-size: 12px;
+  letter-spacing: 0.25px;
+  line-height: 20px;
+  color: #4c4c4c;
+  font-weight: 700;
+  font-size: 18px;
+}
+
+.vqb-modal__section {
+  box-shadow: inset 0 -1px 0 0 #f5f5f5;
+  padding: 25px 30px;
+}
+
+.vqb-modal__text {
+  font-size: 14px;
+  letter-spacing: 0.25px;
+  line-height: 22px;
+  color: #4c4c4c;
+  font-weight: 400;
+}
+
+strong.vqb-modal__text {
+  display: block;
+  font-weight: 700;
+}
+
+.vqb-modal__footer {
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
+  display: flex;
+  justify-content: center;
+  padding: 30px;
+}
+
+.vqb-modal__action {
+  font-size: 12px;
+  letter-spacing: 0.25px;
+  line-height: 20px;
+  cursor: pointer;
+  font-weight: 700;
+  padding: 10px 30px;
+  text-transform: uppercase;
+}
+
+.vqb-modal__action--secondary {
+  background-color: #f5f5f5;
+  color: #a5a5a5;
+  margin-right: 20px;
+  border: none;
+}
+
+.vqb-modal__action--primary {
+  background-color: #4c4c4c;
+  color: #fff;
+  border: none;
+}
+</style>
---
+++ tests/unit/advanced-variable-modal.spec.ts
@@ -0,0 +1,80 @@
+import { shallowMount, Wrapper } from '@vue/test-utils';
+
+import AdvancedVariableModal from '@/components/stepforms/widgets/VariableInputs/AdvancedVariableModal.vue';
+
+describe('Variable Chooser', () => {
+  let wrapper: Wrapper<AdvancedVariableModal>;
+
+  beforeEach(() => {
+    wrapper = shallowMount(AdvancedVariableModal);
+  });
+
+  it('should instantiate', () => {
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  describe('when the modal is closed', () => {
+    it('should not show the modal container', () => {
+      expect(wrapper.find('.vqb-modal__container').exists()).toBe(false);
+    });
+  });
+
+  describe('when opening the modal', () => {
+    beforeEach(async () => {
+      wrapper.setProps({ isOpened: true });
+      await wrapper.vm.$nextTick();
+    });
+
+    it('should have a title', () => {
+      expect(wrapper.find('.vqb-modal__title').text()).toBe('Advanced variable');
+    });
+
+    it('should have a close button', () => {
+      expect(wrapper.find('.vqb-modal__close').exists()).toBe(true);
+    });
+
+    it('should have a save button', () => {
+      expect(wrapper.find('.vqb-modal__action--primary').exists()).toBe(true);
+    });
+
+    it('should have a cancel button', () => {
+      expect(wrapper.find('.vqb-modal__action--secondary').exists()).toBe(true);
+    });
+  });
+
+  describe('when clicking on the close button', () => {
+    beforeEach(async () => {
+      wrapper.setProps({ isOpened: true });
+      wrapper.find('.vqb-modal__close').trigger('click');
+      await wrapper.vm.$nextTick();
+    });
+
+    it('should emit close', () => {
+      expect(wrapper.emitted().closed).toBeTruthy();
+    });
+  });
+
+  describe('when clicking on the cancel button', () => {
+    beforeEach(async () => {
+      wrapper.setProps({ isOpened: true });
+      wrapper.find('.vqb-modal__action--secondary').trigger('click');
+      await wrapper.vm.$nextTick();
+    });
+
+    it('should emit close', () => {
+      expect(wrapper.emitted().closed).toBeTruthy();
+    });
+  });
+
+  describe('when clicking on the save button', () => {
+    beforeEach(async () => {
+      wrapper.setProps({ isOpened: true });
+      wrapper.find('.vqb-modal__action--primary').trigger('click');
+      await wrapper.vm.$nextTick();
+    });
+
+    it('should emit close', () => {
+      expect(wrapper.emitted().closed).toBeTruthy();
+    });
+  });
+});
