d193d66bc21c875e6574fb86ba6c33600896a7e0
alice.sevin <alice.sevin@toucantoco.com>
2020-09-02 10:28:15 +0200

    feat(advanced-variable): save value on emit

--- tests/unit/multi-variable-input.spec.ts
+++ tests/unit/multi-variable-input.spec.ts
@@ -56,4 +56,25 @@ describe('Variable Input', () => {
       expect(wrapper.emitted('input')[0][0]).toEqual([]);
     });
   });
+
+  describe('when choosing an advanced variable', () => {
+    it('should add the value to the list ...', async () => {
+      wrapper
+        .find('VariableInputBase-stub')
+        .vm.$emit('chooseAdvancedVariable', '{{ appRequesters.view }}');
+      await wrapper.vm.$nextTick();
+      expect(wrapper.emitted('input')).toHaveLength(1);
+      expect(wrapper.emitted('input')[0][0]).toEqual(['{{ appRequesters.view }}']);
+    });
+
+    it('... or remove it if already in', async () => {
+      wrapper.setProps({ value: ['{{ appRequesters.view }}'] });
+      wrapper
+        .find('VariableInputBase-stub')
+        .vm.$emit('chooseAdvancedVariable', '{{ appRequesters.view }}');
+      await wrapper.vm.$nextTick();
+      expect(wrapper.emitted('input')).toHaveLength(1);
+      expect(wrapper.emitted('input')[0][0]).toEqual([]);
+    });
+  });
 });
