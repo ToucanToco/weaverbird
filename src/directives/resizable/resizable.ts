956ee82106d3d088e389743563070afb9d8884bd
alice.sevin <alice.sevin@toucantoco.com>
2020-10-15 10:02:56 +0200

    feat(resizable-directive): add empty directive file

---
+++ src/directives/resizable/resizable.ts
@@ -0,0 +1,13 @@
+/*
+ * ResizableDirective allow to resize columns width of a table
+ */
+
+import { DirectiveOptions } from 'vue';
+
+const directive: DirectiveOptions = {
+  inserted() {
+    // directive
+  },
+};
+
+export default directive;
