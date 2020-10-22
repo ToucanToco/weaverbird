0a63d766916c7c33ed8330a09ee0a1e16e75c16c
David Nowinsky <david@nowinsky.net>
2020-10-22 18:58:13 +0200

    chore(server): mini server to test weaverbird package
    Similar to playground

---
+++ server/playground.py
@@ -0,0 +1,35 @@
+"""
+A very simple webserver to test and develop weaverbird python module.
+
+The is only one route: `/`
+GET: returns the available domains
+POST: execute the pipeline in the body of the request and returns the transformed data
+
+Run it with `FLASK_APP=playground FLASK_ENV=development flask run`
+"""
+import json
+
+import pandas as pd
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def handle_request():
+    if request.method == 'GET':
+        return json.dumps(get_available_domains())
+    elif request.method == 'POST':
+        return execute_pipeline(request.get_json())
+
+
+# Load domains in memory
+DOMAINS = {'sales': pd.read_csv('../playground/datastore/sales.csv')}
+
+
+def get_available_domains():
+    return list(DOMAINS.keys())
+
+
+def execute_pipeline(pipeline):
+    raise NotImplemented
