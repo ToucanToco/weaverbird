70abd62a1cbbd2411a2bb5084a706c2f4ff59ac0
David Nowinsky <david@nowinsky.net>
2020-10-23 17:10:49 +0200

    feat(server): filter support for simple operators
    eq, ne, lt, le, gt, ge

--- server/tests/steps/filter.py
+++ server/tests/steps/filter.py
@@ -6,19 +6,9 @@ from weaverbird.steps import FilterStep
 from weaverbird.steps.filter import SimpleCondition
 
 
-def test_simple_eq_filter():
-    df = DataFrame({'colA': ['toto', 'tutu', 'tata'], 'colB': [1, 2, 3], 'colC': [100, 50, 25]})
-
-    df_result = FilterStep(
-        name='filter',
-        condition={
-            'column': 'colA',
-            'operator': 'eq',
-            'value': 'tutu',
-        },
-    ).execute(df, domain_retriever=None)
-
-    assert_dataframes_equals(df_result, DataFrame({'colA': ['tutu'], 'colB': [2], 'colC': [50]}))
+@pytest.fixture
+def sample_df():
+    return DataFrame({'colA': ['toto', 'tutu', 'tata'], 'colB': [1, 2, 3], 'colC': [100, 50, 25]})
 
 
 @pytest.mark.parametrize('value', [0, False, True, 0.0, 1, 1.5, '', '0', None, [], [0], '0.0'])
@@ -28,3 +18,87 @@ def test_simple_condition_valid_values(value) -> None:
     result_value = sc.value
     assert value == result_value
     assert type(value) == type(result_value)
+
+
+def test_simple_eq_filter(sample_df):
+    df_result = FilterStep(
+        name='filter',
+        condition={
+            'column': 'colA',
+            'operator': 'eq',
+            'value': 'tutu',
+        },
+    ).execute(sample_df, domain_retriever=None)
+
+    assert_dataframes_equals(df_result, DataFrame({'colA': ['tutu'], 'colB': [2], 'colC': [50]}))
+
+
+def test_simple_ne_filter(sample_df):
+    df_result = FilterStep(
+        name='filter',
+        condition={
+            'column': 'colA',
+            'operator': 'ne',
+            'value': 'tutu',
+        },
+    ).execute(sample_df, domain_retriever=None)
+
+    assert_dataframes_equals(
+        df_result, DataFrame({'colA': ['toto', 'tata'], 'colB': [1, 3], 'colC': [100, 25]})
+    )
+
+
+def test_simple_gt_filter(sample_df):
+    df_result = FilterStep(
+        name='filter',
+        condition={
+            'column': 'colB',
+            'operator': 'gt',
+            'value': 2,
+        },
+    ).execute(sample_df, domain_retriever=None)
+
+    assert_dataframes_equals(df_result, DataFrame({'colA': ['tata'], 'colB': [3], 'colC': [25]}))
+
+
+def test_simple_ge_filter(sample_df):
+    df_result = FilterStep(
+        name='filter',
+        condition={
+            'column': 'colB',
+            'operator': 'ge',
+            'value': 2,
+        },
+    ).execute(sample_df, domain_retriever=None)
+
+    assert_dataframes_equals(
+        df_result, DataFrame({'colA': ['tutu', 'tata'], 'colB': [2, 3], 'colC': [50, 25]})
+    )
+
+
+def test_simple_lt_filter(sample_df):
+    df_result = FilterStep(
+        name='filter',
+        condition={
+            'column': 'colB',
+            'operator': 'lt',
+            'value': 2,
+        },
+    ).execute(sample_df, domain_retriever=None)
+
+    assert_dataframes_equals(df_result, DataFrame({'colA': ['toto'], 'colB': [1], 'colC': [100]}))
+
+
+def test_simple_le_filter(sample_df):
+    df_result = FilterStep(
+        name='filter',
+        condition={
+            'column': 'colB',
+            'operator': 'le',
+            'value': 2,
+        },
+    ).execute(sample_df, domain_retriever=None)
+
+    assert_dataframes_equals(
+        df_result, DataFrame({'colA': ['toto', 'tutu'], 'colB': [1, 2], 'colC': [100, 50]})
+    )
--- server/weaverbird/steps/filter.py
+++ server/weaverbird/steps/filter.py
@@ -1,6 +1,6 @@
 from typing import Any, Literal, Union
 
-from pandas import DataFrame
+from pandas import DataFrame, Series
 from pydantic import Field
 from pydantic.main import BaseModel
 
@@ -13,8 +13,8 @@ DataValue = Any  # FIXME use Any to prevent pydantic cast
 
 class SimpleCondition(BaseModel):
     column: ColumnName
-    # TODO support all operators
-    operator: Literal['eq']
+    # TODO support all operators (missing in, nin, matches, matches, null, notnull)
+    operator: Literal['eq', 'ne', 'lt', 'le', 'gt', 'ge']
     value: DataValue
 
 
@@ -24,4 +24,16 @@ class FilterStep(BaseStep):
     condition: SimpleCondition
 
     def execute(self, df: DataFrame, domain_retriever) -> DataFrame:
-        return df[df[self.condition.column] == self.condition.value]
+        return df[binary_operator_filter(df, **self.condition.dict())]
+
+
+BINARY_OPERATORS = ['eq', 'ne', 'lt', 'le', 'gt', 'ge']
+
+
+def binary_operator_filter(
+    df: DataFrame,
+    operator: Literal['eq', 'ne', 'lt', 'le', 'gt', 'ge'],
+    column: ColumnName,
+    value: DataValue,
+) -> Series:
+    return getattr(df[column], operator)(value)
