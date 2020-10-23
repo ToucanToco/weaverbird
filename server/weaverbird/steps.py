5859124306891180fa39b5206cd95aac09af07a6
David Nowinsky <david@nowinsky.net>
2020-10-23 12:05:47 +0200

    refactor(server): add execute method to step
    And implement it for DomainStep

--- server/weaverbird/steps.py
+++ server/weaverbird/steps.py
@@ -1,16 +1,26 @@
-from abc import ABC
-from typing import Union
+from abc import ABC, abstractmethod
+from typing import Callable, Union
 
+import pandas as pd
 from pydantic import BaseModel, Field
+
+DomainRetriever = Callable[[str], pd.DataFrame]
 
 
 class BaseStep(BaseModel, ABC):
     name: str
 
+    @abstractmethod
+    def execute(self, df: pd.DataFrame, domain_retriever: DomainRetriever) -> pd.DataFrame:
+        ...
+
 
 class DomainStep(BaseStep):
     name = Field('domain', const=True)
     domain: str
+
+    def execute(self, _df, domain_retriever: DomainRetriever) -> pd.DataFrame:
+        return domain_retriever(self.domain)
 
 
 PipelineStep = Union[DomainStep]
