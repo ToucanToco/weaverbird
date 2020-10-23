3eafaa366be2bb57a901c3e1ec669535a407fd9c
David Nowinsky <david@nowinsky.net>
2020-10-23 17:10:49 +0200

    feat(server): filter support for simple operators
    eq, ne, lt, le, gt, ge

--- server/tests/steps/filter.py
+++ server/tests/steps/filter.py
@@ -1,12 +1,16 @@
+import pytest
 from pandas import DataFrame
 
 from tests.utils import assert_dataframes_equals
 from weaverbird.steps import FilterStep
 
 
-def test_simple_eq_filter():
-    df = DataFrame({'colA': ['toto', 'tutu', 'tata'], 'colB': [1, 2, 3], 'colC': [100, 50, 25]})
+@pytest.fixture
+def sample_df():
+    return DataFrame({'colA': ['toto', 'tutu', 'tata'], 'colB': [1, 2, 3], 'colC': [100, 50, 25]})
 
+
+def test_simple_eq_filter(sample_df):
     df_result = FilterStep(
         name='filter',
         condition={
@@ -14,6 +18,77 @@ def test_simple_eq_filter():
             'operator': 'eq',
             'value': 'tutu',
         },
-    ).execute(df, domain_retriever=None)
+    ).execute(sample_df, domain_retriever=None)
 
     assert_dataframes_equals(df_result, DataFrame({'colA': ['tutu'], 'colB': [2], 'colC': [50]}))
+
+
+def test_simple_ne_filter(sample_df):
+    df_result = FilterStep(
+        name='filter',
+        condition={
+            'column': 'colA',
+            'operator': 'ne',
+            'value': 'tutu',
+        },
+    ).execute(sample_df, domain_retriever=None)
+
+    assert_dataframes_equals(
+        df_result, DataFrame({'colA': ['toto', 'tata'], 'colB': [1, 3], 'colC': [100, 25]})
+    )
+
+
+def test_simple_gt_filter(sample_df):
+    df_result = FilterStep(
+        name='filter',
+        condition={
+            'column': 'colB',
+            'operator': 'gt',
+            'value': 2,
+        },
+    ).execute(sample_df, domain_retriever=None)
+
+    assert_dataframes_equals(df_result, DataFrame({'colA': ['tata'], 'colB': [3], 'colC': [25]}))
+
+
+def test_simple_ge_filter(sample_df):
+    df_result = FilterStep(
+        name='filter',
+        condition={
+            'column': 'colB',
+            'operator': 'ge',
+            'value': 2,
+        },
+    ).execute(sample_df, domain_retriever=None)
+
+    assert_dataframes_equals(
+        df_result, DataFrame({'colA': ['tutu', 'tata'], 'colB': [2, 3], 'colC': [50, 25]})
+    )
+
+
+def test_simple_lt_filter(sample_df):
+    df_result = FilterStep(
+        name='filter',
+        condition={
+            'column': 'colB',
+            'operator': 'lt',
+            'value': 2,
+        },
+    ).execute(sample_df, domain_retriever=None)
+
+    assert_dataframes_equals(df_result, DataFrame({'colA': ['toto'], 'colB': [1], 'colC': [100]}))
+
+
+def test_simple_le_filter(sample_df):
+    df_result = FilterStep(
+        name='filter',
+        condition={
+            'column': 'colB',
+            'operator': 'le',
+            'value': 2,
+        },
+    ).execute(sample_df, domain_retriever=None)
+
+    assert_dataframes_equals(
+        df_result, DataFrame({'colA': ['toto', 'tutu'], 'colB': [1, 2], 'colC': [100, 50]})
+    )
