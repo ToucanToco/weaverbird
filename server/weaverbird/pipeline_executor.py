ddb7936fb7f06a25419d94230a1d6e8a46452f71
David Nowinsky <david@nowinsky.net>
2020-10-23 12:44:50 +0200

    feat(server): support multiple steps and simple filter
    For the filter: only eq operator for now

--- server/weaverbird/pipeline_executor.py
+++ server/weaverbird/pipeline_executor.py
@@ -2,7 +2,7 @@ from typing import Callable, List
 
 import pandas as pd
 
-from weaverbird.steps import DomainStep
+from weaverbird.steps import Pipeline
 
 
 class PipelineExecutor:
@@ -15,10 +15,12 @@ class PipelineExecutor:
     def __init__(self, domain_retriever: Callable[[str], pd.DataFrame]):
         self.retrieve_domain = domain_retriever
 
-    def execute_pipeline(self, pipeline: List[dict]) -> pd.DataFrame:
+    def execute_pipeline(self, pipeline_steps: List[dict]) -> pd.DataFrame:
         # TODO validate the pipeline, e.g. the first step should always be a domain step
         # self.validate_pipeline()
-        domain_step = DomainStep(**pipeline[0])
-        df = domain_step.execute(None, domain_retriever=self.retrieve_domain)
+        steps = Pipeline(steps=pipeline_steps).steps
+        df = None
+        for step in steps:
+            df = step.execute(df, domain_retriever=self.retrieve_domain)
         # TODO validate and apply all other steps
         return df
--- server/weaverbird/steps.py
+++ server/weaverbird/steps.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Callable, Union
+from typing import Any, Callable, List, Literal, Union
 
 import pandas as pd
 from pydantic import BaseModel, Field
@@ -23,4 +23,28 @@ class DomainStep(BaseStep):
         return domain_retriever(self.domain)
 
 
-PipelineStep = Union[DomainStep]
+ColumnName = Union[str, int, float]
+DataValue = Union[bool, float, int, str, list, None]
+
+
+class SimpleCondition(BaseModel):
+    column: ColumnName
+    # TODO support all operators
+    operator: Literal['eq']
+    value: DataValue
+
+
+class FilterStep(BaseStep):
+    name = Field('filter', const=True)
+    # TODO support and/or nesting
+    condition: SimpleCondition
+
+    def execute(self, df: pd.DataFrame, domain_retriever) -> pd.DataFrame:
+        return df[df[self.condition.column] == self.condition.value]
+
+
+PipelineStep = Union[DomainStep, FilterStep]
+
+
+class Pipeline(BaseModel):
+    steps: List[PipelineStep]
