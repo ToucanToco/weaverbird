62ae9ad517253363c687a712f62eacb9206b193d
Luc Leonard <git@luc-leonard.fr>
2020-10-25 13:09:13 +0100

    chores(aggregate): format and lint

--- server/weaverbird/steps/aggregate.py
+++ server/weaverbird/steps/aggregate.py
@@ -1,14 +1,12 @@
-from abc import ABC, abstractmethod
-from typing import List, Literal, Union, Dict
+from typing import Iterator, List, Literal, Tuple, Union
 
+import pandas as pd
 from mypy.applytype import Optional
-from numpy.ma import logical_and, logical_or
-from pandas import DataFrame, Series
+from pandas import DataFrame
 from pydantic import Field
 from pydantic.main import BaseModel
 
 from weaverbird.steps.base import BaseStep
-import pandas as pd
 
 ColumnName = Union[str, int, float]
 AggregateFn = Literal['avg', 'sum', 'min', 'max']
@@ -31,10 +29,19 @@ class AggregateStep(BaseStep):
         all_results = pd.DataFrame()
         for aggregation in self.aggregations:
             aggregation_result = grouped_by_df.agg(
-                {column_name: aggregation.agg_function for column_name in aggregation.columns})
-            for (old_column_name, new_column_name) in zip(aggregation.columns, aggregation.new_columns):
-                aggregation_result[new_column_name] = aggregation_result[old_column_name]
-                del aggregation_result[old_column_name]
-            for column in aggregation_result.columns:
-                all_results[column] = aggregation_result[column]
+                {column_name: aggregation.agg_function for column_name in aggregation.columns}
+            )
+            rename_columns(aggregation_result, zip(aggregation.columns, aggregation.new_columns))
+            add_to_results(aggregation_result, all_results)
         return all_results
+
+
+def add_to_results(aggregation_result, all_results):
+    for column in aggregation_result.columns:
+        all_results[column] = aggregation_result[column]
+
+
+def rename_columns(aggregation_result: DataFrame, renames: Iterator[Tuple[ColumnName, ColumnName]]):
+    for (old_column_name, new_column_name) in renames:
+        aggregation_result[new_column_name] = aggregation_result[old_column_name]
+        del aggregation_result[old_column_name]
