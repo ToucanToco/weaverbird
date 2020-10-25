40bc70863f95fb0256f0d7a37c9fa65721e2220d
Luc Leonard <git@luc-leonard.fr>
2020-10-25 10:42:47 +0100

    feature(aggregate): updated the test to match the existing API

--- server/tests/steps/aggregate.py
+++ server/tests/steps/aggregate.py
@@ -3,22 +3,35 @@ from pandas import DataFrame
 
 from tests.utils import assert_dataframes_equals
 from weaverbird.steps import FilterStep
+from weaverbird.steps.aggregate import AggregateStep, Aggregation
 
 
 @pytest.fixture
 def sample_df():
-    return DataFrame({'colA': ['toto', 'tutu', 'tata'], 'colB': [1, 2, 3], 'colC': [100, 50, 25]})
+    return DataFrame({'colA': ['toto', 'tutu', 'tata', 'toto'], 'colB': [1, 2, 3, 4], 'colC': [100, 50, 25, 0]})
 
 
 def test_simple_aggregate(sample_df):
     df_result = AggregateStep(
         name='aggregate',
-        condition={
-            'column': 'colA',
-            'operation': 'sum',
-            'name': 'result',
-            'group_by': []
-        },
+        on=['colA'],
+        aggregations=[Aggregation(
+            agg_function='sum',
+            columns=['colB'],
+            new_columns=['sum_colB']
+        )]
     ).execute(sample_df, domain_retriever=None)
 
-    assert_dataframes_equals(df_result, DataFrame({'result': [175]}))
+    assert_dataframes_equals(df_result, DataFrame({'colA': ['toto', 'tutu', 'tata'], 'sum_colB': [5, 2, 3]}))
+
+
+def test_multiple_aggregate(sample_df):
+    df_result = AggregateStep(
+        name='aggregate',
+        operations={
+            'colC': 'sum',
+            'colB': 'min'
+        }
+    ).execute(sample_df, domain_retriever=None)
+
+    assert_dataframes_equals(df_result, DataFrame({'colC': [175], 'colB': [1]}))
