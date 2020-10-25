b3e847b9f9949564dbb617cd6da45605c53666b1
Luc Leonard <git@luc-leonard.fr>
2020-10-25 12:57:52 +0100

    feature(aggregate): this version supports simple aggregations.

--- server/weaverbird/steps/aggregate.py
+++ server/weaverbird/steps/aggregate.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import List, Literal, Union, Dict
+from typing import List, Literal, Union, Dict, Tuple, Iterator
 
 from mypy.applytype import Optional
 from numpy.ma import logical_and, logical_or
@@ -27,4 +27,22 @@ class AggregateStep(BaseStep):
     keepOriginalGranularity: Optional[bool] = False
 
     def execute(self, df: DataFrame, domain_retriever) -> DataFrame:
-        return df.groupby(self.on).agg()
+        grouped_by_df = df.groupby(self.on, as_index=False)
+        all_results = pd.DataFrame()
+        for aggregation in self.aggregations:
+            aggregation_result = grouped_by_df.agg(
+                {column_name: aggregation.agg_function for column_name in aggregation.columns})
+            rename_columns(aggregation_result, zip(aggregation.columns, aggregation.new_columns))
+            add_to_results(aggregation_result, all_results)
+        return all_results
+
+
+def add_to_results(aggregation_result, all_results):
+    for column in aggregation_result.columns:
+        all_results[column] = aggregation_result[column]
+
+
+def rename_columns(aggregation_result: DataFrame, renames: Iterator[Tuple[ColumnName, ColumnName]]):
+    for (old_column_name, new_column_name) in renames:
+        aggregation_result[new_column_name] = aggregation_result[old_column_name]
+        del aggregation_result[old_column_name]
