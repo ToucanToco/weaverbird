2b7895f94b95596d956fad32802b50f7fcd315f5
Luc Leonard <github@luc-leonard.fr>
2020-10-26 18:35:42 +0100

    feat(aggregates): after talking with @davinov, aggregation with no grouping keys are not currently valid. meybe later.

--- server/tests/steps/aggregate.py
+++ server/tests/steps/aggregate.py
@@ -27,16 +27,15 @@ def test_simple_aggregate(sample_df):
     )
 
 
-def test_simple_aggregate_nogroup(sample_df):
-    df_result = AggregateStep(
-        name='aggregate',
-        on=[],
-        aggregations=[
-            Aggregation(agg_function='min', columns=['colB'], new_columns=['min_colB']),
-        ],
-    ).execute(sample_df, domain_retriever=None)
-
-    assert_dataframes_equals(df_result, DataFrame({'min_colB': [1]}))
+def test_aggregate_is_no_valid_without_on(sample_df):
+    with pytest.raises(ValueError):
+        AggregateStep(
+            name='aggregate',
+            on=[],
+            aggregations=[
+                Aggregation(agg_function='min', columns=['colB'], new_columns=['min_colB']),
+            ],
+        )
 
 
 def test_multiple_aggregate(sample_df):
@@ -51,17 +50,3 @@ def test_multiple_aggregate(sample_df):
 
     assert_dataframes_equals(df_result, DataFrame(
         {'colA': ['tata', 'toto', 'tutu'], 'min_colB': [3, 1, 2], 'max_colB': [3, 4, 2]}))
-
-
-def test_multiple_aggregate_nogroup(sample_df):
-    df_result = AggregateStep(
-        name='aggregate',
-        on=[],
-        aggregations=[
-            Aggregation(agg_function='min', columns=['colB'], new_columns=['min_colB']),
-            Aggregation(agg_function='max', columns=['colB'], new_columns=['max_colB']),
-        ],
-    ).execute(sample_df, domain_retriever=None)
-
-    assert_dataframes_equals(df_result, DataFrame(
-        {'min_colB': [1], 'max_colB': [4]}))
