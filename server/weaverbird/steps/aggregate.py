60fa64e31254b249c8735f3e3edb13ebe0d072ec
Luc Leonard <github@luc-leonard.fr>
2020-11-02 14:58:54 +0100

    feat(aggregates): added 'keepOriginalGranularity'. WIP: I am benchmarking the multiple algorithm for this

--- server/weaverbird/steps/aggregate.py
+++ server/weaverbird/steps/aggregate.py
@@ -12,6 +12,13 @@ AggregateFn = Literal['avg', 'sum', 'min', 'max']
 
 functions_aliases = {'avg': 'mean'}
 
+lambdas = {
+    'sum': lambda x: x.sum(),
+    'mean': lambda x: x.mean(),
+    'min': lambda x: x.min(),
+    'max': lambda x: x.max(),
+}
+
 
 class Aggregation(BaseModel):
     new_columns: List[ColumnName]
@@ -32,6 +39,8 @@ class AggregateStep(BaseStep):
     keepOriginalGranularity: Optional[bool] = False
 
     def execute(self, df: DataFrame, domain_retriever) -> DataFrame:
+        if self.keepOriginalGranularity:
+            return self.execute_with_original_granularity(df, domain_retriever)
         grouped_by_df = df.groupby(self.on, as_index=False)
         first_aggregation = self.aggregations[0]
         aggs = self.make_aggregation(first_aggregation)
@@ -46,8 +55,37 @@ class AggregateStep(BaseStep):
             aggs = self.make_aggregation(aggregation)
             all_results[self.on + aggregation.new_columns] = grouped_by_df.agg(aggs)[
                 self.on + aggregation.columns
-            ]
+                ]
+
         return all_results
+
+    def execute_merge(self, df, domain_retriever):
+        grouped_by_df = df.groupby(self.on, as_index=False)
+        first_aggregation = self.aggregations[0]
+        aggs = self.make_aggregation(first_aggregation)
+        all_results = grouped_by_df.agg(aggs).rename(
+            columns={
+                col: new_col
+                for col, new_col in zip(first_aggregation.columns, first_aggregation.new_columns)
+            }
+        )
+
+        for idx, aggregation in enumerate(self.aggregations[1:]):
+            aggs = self.make_aggregation(aggregation)
+            all_results[self.on + aggregation.new_columns] = grouped_by_df.agg(aggs)[
+                self.on + aggregation.columns
+                ]
+        if self.keepOriginalGranularity:
+            return df.merge(all_results, on=self.on, how='left')
+        return all_results
+
+    def execute_with_original_granularity(self, df, domain_retriever):
+        grouped_by_df = df.groupby(self.on, as_index=False)
+
+        for aggregation in self.aggregations:
+            df[aggregation.new_columns] = grouped_by_df[aggregation.columns].transform(
+                get_aggregate_fn(aggregation.agg_function))
+        return df
 
     def make_aggregation(self, aggregation):
         return {
