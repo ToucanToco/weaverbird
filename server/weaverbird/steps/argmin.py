ab10ee723125f98cd3a7e14db93fee9da1c808b1
Luc Leonard <github@luc-leonard.fr>
2020-11-03 12:17:32 +0100

    feat(argmin): added argmin step

---
+++ server/weaverbird/steps/argmin.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from pandas import DataFrame
+from pydantic import Field
+
+from weaverbird.steps.base import BaseStep
+
+
+class ArgMinStep(BaseStep):
+    name = Field('argmin', const=True)
+    column: str
+    groups: List[str] = []
+
+    def execute(self, df: DataFrame, domain_retriever=None, pipeline_executor=None) -> DataFrame:
+        if len(self.groups) == 0:
+            return df.assign(**{self.column: df[self.column].min()})
+        else:
+            grouped_by_df = df.groupby(self.groups, as_index=False)
+            return df.assign(**{self.column: grouped_by_df[self.column].transform('min')})
