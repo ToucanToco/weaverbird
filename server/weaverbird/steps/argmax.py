3d38c0154da4cd6f1ae88678df87d6cd322fc4a8
Luc Leonard <github@luc-leonard.fr>
2020-11-03 12:09:15 +0100

    chores(argmax): format and lint

--- server/weaverbird/steps/argmax.py
+++ server/weaverbird/steps/argmax.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List
 
 from pandas import DataFrame
 from pydantic import Field
@@ -11,11 +11,9 @@ class ArgMaxStep(BaseStep):
     column: str
     groups: List[str] = []
 
-    def execute(self, df: DataFrame, domain_retriever) -> DataFrame:
+    def execute(self, df: DataFrame, domain_retriever=None, pipeline_executor=None) -> DataFrame:
         if len(self.groups) == 0:
             return df.assign(**{self.column: df[self.column].max()})
         else:
             grouped_by_df = df.groupby(self.groups, as_index=False)
             return df.assign(**{self.column: grouped_by_df[self.column].transform('max')})
-
-
