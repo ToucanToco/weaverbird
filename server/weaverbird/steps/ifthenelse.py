3ccc5cef43928378830f990f58b2ec7a5769dd4e
Luc Leonard <github@luc-leonard.fr>
2020-11-06 10:48:48 +0100

    fix(ifthenelse): ifthenelse now cleans the formula

--- server/weaverbird/steps/ifthenelse.py
+++ server/weaverbird/steps/ifthenelse.py
@@ -3,10 +3,10 @@ from typing import Any
 import numpy
 from pandas import DataFrame
 from pydantic import Field
+
 from weaverbird.conditions import Condition
-
+from weaverbird.formula import clean_formula
 from weaverbird.steps import BaseStep
-
 from weaverbird.types import ColumnName, DomainRetriever, PipelineExecutor
 
 
@@ -27,7 +27,17 @@ class IfthenelseStep(BaseStep):
         return df.assign(
             **{
                 self.new_column: numpy.where(
-                    self.condition.filter(df), df.eval(self.then), df.eval(self.else_value)
+                    self.condition.filter(df),
+                    df.eval(clean_if_formula(self.then)),
+                    df.eval(clean_if_formula(self.else_value)),
                 )
             }
         )
+
+
+# ifthenelse can take as a parameter either a formula, or a value
+def clean_if_formula(formula_or_value: Any) -> Any:
+    if isinstance(formula_or_value, str):
+        return clean_formula(formula_or_value)
+    else:
+        return formula_or_value
