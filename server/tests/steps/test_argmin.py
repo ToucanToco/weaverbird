d477621bd7c227c00e503ba14b4eafd44fcd15c8
Luc Leonard <github@luc-leonard.fr>
2020-11-06 12:49:58 +0100

    tests(pandas argmin): I now use the data from documentation

--- server/tests/steps/test_argmin.py
+++ server/tests/steps/test_argmin.py
@@ -8,35 +8,37 @@ from weaverbird.steps.argmin import ArgminStep
 @pytest.fixture
 def sample_df():
     return DataFrame(
-        {'colA': ['toto', 'tutu', 'tata', 'toto'], 'colB': [1, 2, 3, 4], 'colC': [100, 50, 25, 0]}
+        {'label': ['label1', 'label2', 'label3', 'label4', 'label5', 'label6'],
+         'group': ['group 1', 'group 1', 'group 1', 'group 2', 'group 2', 'group 2'],
+         'value': [13, 7, 20, 1, 10, 5]}
     )
 
 
 def test_simple_argmin(sample_df):
-    step = ArgminStep(name='argmin', column='colB')
+    step = ArgminStep(name='argmin', column='value')
     result = step.execute(sample_df, domain_retriever=None)
     assert_dataframes_equals(
         result,
         DataFrame(
             {
-                'colA': ['toto'],
-                'colB': [1],
-                'colC': [100],
+                'label': ['label4'],
+                'group': ['group 2'],
+                'value': [1],
             }
         ),
     )
 
 
 def test_argmin_with_group(sample_df):
-    step = ArgminStep(name='argmin', column='colB', groups=['colA'])
+    step = ArgminStep(name='argmin', column='value', groups=['group'])
     result = step.execute(sample_df, domain_retriever=None)
     assert_dataframes_equals(
         result,
         DataFrame(
             {
-                'colA': ['toto', 'tutu', 'tata'],
-                'colB': [1, 2, 3],
-                'colC': [100, 50, 25],
+                'label': ['label2', 'label4'],
+                'group': ['group 1', 'group 2'],
+                'value': [7, 1],
             }
         ),
     )
