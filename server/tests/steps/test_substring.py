e5ac3fa10401642450069902dbb5688390600f3d
Luc Leonard <github@luc-leonard.fr>
2020-11-10 18:03:52 +0100

    feat(substring): step & test

---
+++ server/tests/steps/test_substring.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import pytest
+
+from tests.utils import assert_dataframes_equals
+from weaverbird.steps.substring import SubstringStep
+
+
+@pytest.fixture()
+def sample_df():
+    return pd.DataFrame(
+        {
+            'Label': ['foo', 'overflow', 'some_text', 'a_word', 'toucan', 'toco'],
+            'Value:': [13, 7, 20, 1, 10, 5],
+        }
+    )
+
+
+def test_substring_positive_start_end_idx(sample_df):
+    result_df = SubstringStep(name='substring', column='Label', start_index=1, end_index=4).execute(
+        sample_df
+    )
+
+    expected_df = pd.DataFrame(
+        {
+            'Label': ['foo', 'overflow', 'some_text', 'a_word', 'toucan', 'toco'],
+            'Value:': [13, 7, 20, 1, 10, 5],
+            'Label_PCT': ['foo', 'over', 'some', 'a_wo', 'touc', 'toco'],
+        }
+    )
+    assert_dataframes_equals(result_df, expected_df)
+
+
+def test_substring_positive_start_negative_end(sample_df):
+    result_df = SubstringStep(
+        name='substring', column='Label', start_index=2, end_index=-2
+    ).execute(sample_df)
+
+    expected_df = pd.DataFrame(
+        {
+            'Label': ['foo', 'overflow', 'some_text', 'a_word', 'toucan', 'toco'],
+            'Value:': [13, 7, 20, 1, 10, 5],
+            'Label_PCT': ['o', 'verflo', 'ome_tex', '_wor', 'ouca', 'oc'],
+        }
+    )
+    assert_dataframes_equals(result_df, expected_df)
+
+
+def test_substring_negative_start_negative_end(sample_df):
+    result_df = SubstringStep(
+        name='substring', column='Label', start_index=-3, end_index=-1
+    ).execute(sample_df)
+
+    expected_df = pd.DataFrame(
+        {
+            'Label': ['foo', 'overflow', 'some_text', 'a_word', 'toucan', 'toco'],
+            'Value:': [13, 7, 20, 1, 10, 5],
+            'Label_PCT': ['foo', 'low', 'ext', 'ord', 'can', 'oco'],
+        }
+    )
+    assert_dataframes_equals(result_df, expected_df)
