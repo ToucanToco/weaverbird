67e6ad6ead53c1b3f99d6d45480c7f0490bb318f
Luc Leonard <github@luc-leonard.fr>
2020-11-16 17:35:13 +0100

    feat(panas waterfall): simple case is working. TODO: groups

--- server/tests/steps/test_waterfall.py
+++ server/tests/steps/test_waterfall.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 from tests.utils import assert_dataframes_equals
 from weaverbird.steps.waterfall import WaterfallStep
 
@@ -6,24 +7,32 @@ from weaverbird.steps.waterfall import WaterfallStep
 def test_simple():
     sample_df = pd.DataFrame(
         {
-            'city': ['Bordeaux', 'Boston', 'Paris', 'New-York'] * 2,
+            'city': ['Bordeaux', 'Boston', 'New-York', 'Paris'] * 2,
             'year': [2019] * 4 + [2018] * 4,
-            'revenue': [135, 275, 115, 450, 98, 245, 103, 385]
+            'revenue': [135, 275, 115, 450, 98, 245, 103, 385],
         }
     )
-    result_df = WaterfallStep(name='waterfall',
-                              valueColumn='revenue',
-                              milestonesColumn='year',
-                              start=2018, end=2019,
-                              labelsColumn='city',
-                              sortBy='value',
-                              order='desc').execute(sample_df)
+    result_df = (
+        WaterfallStep(
+            name='waterfall',
+            valueColumn='revenue',
+            milestonesColumn='year',
+            start=2018,
+            end=2019,
+            labelsColumn='city',
+            sortBy='value',
+            order='desc',
+        )
+        .execute(sample_df)
+        .sort_values(by='LABEL_waterfall', ascending=True)
+    )
 
     expected_df = pd.DataFrame(
         {
-            'LABEL_waterfall': [2018, 'Paris', 'Bordeaux', 'Boston', 'New-York', 2019],
-            'TYPE_waterfall': [None, 'parent', 'parent', 'parent', 'parent', None],
-            'revenue': [831, 65, 37, 30, 12, 975]
-        })
+            'LABEL_waterfall': ['2018', 'Paris', 'Bordeaux', 'Boston', 'New-York', '2019'],
+            'TYPE_waterfall': [None, 'Parent', 'Parent', 'Parent', 'Parent', None],
+            'revenue': [831, 65, 37, 30, 12, 975],
+        }
+    ).sort_values(by='LABEL_waterfall', ascending=True)
 
     assert_dataframes_equals(result_df, expected_df)
--- server/weaverbird/steps/waterfall.py
+++ server/weaverbird/steps/waterfall.py
@@ -4,6 +4,7 @@ import numpy
 import pandas as pd
 from pandas import DataFrame
 from pydantic import Field
+
 from weaverbird.steps import BaseStep
 from weaverbird.types import ColumnName, DomainRetriever, PipelineExecutor
 
@@ -18,19 +19,38 @@ class WaterfallStep(BaseStep):
     sortBy: Literal['value', 'label']
     order: Literal['desc', 'asc']
 
-    def execute(self, df: DataFrame, domain_retriever: DomainRetriever = None,
-                execute_pipeline: PipelineExecutor = None) -> DataFrame:
+    def execute(
+        self,
+        df: DataFrame,
+        domain_retriever: DomainRetriever = None,
+        execute_pipeline: PipelineExecutor = None,
+    ) -> DataFrame:
         start_df = df[df[self.milestonesColumn] == self.start].rename(
-            columns={self.valueColumn: f'{self.valueColumn}_start'})
-        end_df = df[df[self.milestonesColumn] == self.end].rename(columns={self.valueColumn: f'{self.valueColumn}_end'})
+            columns={self.valueColumn: f'{self.valueColumn}_start'}
+        )
+        end_df = df[df[self.milestonesColumn] == self.end].rename(
+            columns={self.valueColumn: f'{self.valueColumn}_end'}
+        )
         merged_df = start_df.merge(end_df, on=self.labelsColumn)
-        merged_df['result'] = merged_df[f'{self.valueColumn}_end'] - merged_df[f'{self.valueColumn}_start']
+        merged_df['result'] = (
+            merged_df[f'{self.valueColumn}_end'] - merged_df[f'{self.valueColumn}_start']
+        )
         result_df = DataFrame(
             {
                 'LABEL_waterfall': pd.concat(
-                    [pd.Series([self.start]), merged_df[self.labelsColumn], pd.Series([self.end])])
+                    [pd.Series([self.start]), merged_df[self.labelsColumn], pd.Series([self.end])]
+                ).astype(str)
             }
         )
-        result_df['TYPE_waterfall'] = numpy.where(result_df['LABEL_waterfall'].isin([self.start, self.end]), None, 'Parent')
-        result_df[self.valueColumn] = 0
-        return merged_df
+        result_df['TYPE_waterfall'] = numpy.where(
+            result_df['LABEL_waterfall'].isin([str(self.start), str(self.end)]), [None], ['Parent']
+        )
+
+        result_df[self.valueColumn] = pd.concat(
+            [
+                pd.Series([start_df[f'{self.valueColumn}_start'].sum()]),
+                merged_df['result'],
+                pd.Series([end_df[f'{self.valueColumn}_end'].sum()]),
+            ]
+        )
+        return result_df
