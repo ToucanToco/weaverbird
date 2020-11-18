445aa40ee2d40ce9e56237d6b6b65616961ea86d
David Nowinsky <david@nowinsky.net>
2020-11-18 15:18:32 +0100

    feat(server): append step can revieve domain name (str)
    instead of a complete pipeline

--- server/weaverbird/types.py
+++ server/weaverbird/types.py
@@ -3,5 +3,6 @@ from typing import Callable, List
 from pandas import DataFrame
 
 ColumnName = str
+Pipeline = List[dict]
 DomainRetriever = Callable[[str], DataFrame]
 PipelineExecutor = Callable[[List[dict]], DataFrame]
