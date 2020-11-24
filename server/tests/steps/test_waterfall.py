b757ad9c8a0050ceb5fc0788e1f44e1e5e69a564
Luc Leonard <github@luc-leonard.fr>
2020-11-24 13:08:25 +0100

    fix(waterfall): we aggregate values for a given label/ group, resulting, avoiding to have several columns with the same label / group, but different values

--- server/tests/steps/test_waterfall.py
+++ server/tests/steps/test_waterfall.py
@@ -34,6 +34,35 @@ def test_simple():
     assert_dataframes_equals(result_df, expected_df)
 
 
+def test_simple_with_aggregation():
+    sample_df = pd.DataFrame(
+        {
+            'city': ['Bordeaux', 'Boston', 'New-York', 'Paris', 'Paris'] * 2,
+            'year': [2019] * 5 + [2018] * 5,
+            'revenue': [135, 275, 115, 450, 10, 98, 245, 103, 385, 10],
+        }
+    )
+    result_df = WaterfallStep(
+        name='waterfall',
+        valueColumn='revenue',
+        milestonesColumn='year',
+        start=2018,
+        end=2019,
+        labelsColumn='city',
+        sortBy='value',
+        order='desc',
+    ).execute(sample_df)
+
+    expected_df = pd.DataFrame(
+        {
+            'LABEL_waterfall': ['2018', 'Paris', 'Bordeaux', 'Boston', 'New-York', '2019'],
+            'TYPE_waterfall': [None, 'Parent', 'Parent', 'Parent', 'Parent', None],
+            'revenue': [841, 65, 37, 30, 12, 985],
+        }
+    )
+    assert_dataframes_equals(result_df, expected_df)
+
+
 def test_with_groups():
     sample_df = pd.DataFrame(
         {
