39c0a3eb2d145dc4f47690ebb7779629e70dffe5
Luc Leonard <github@luc-leonard.fr>
2020-12-21 19:28:12 +0100

    feat(pandas/duration-step): parametrized test

--- server/tests/steps/test_duration.py
+++ server/tests/steps/test_duration.py
@@ -1,36 +1,41 @@
-import datetime
 from datetime import datetime, timedelta
+from typing import Dict
 
 import pandas as pd
+import pytest
 
 from tests.utils import assert_dataframes_equals
 from weaverbird.steps.duration import DurationStep
 
 
-def test_duration():
+@pytest.mark.parametrize(
+    "time_delta_parameters,duration_in, expected_result",
+    [
+        ({'days': 30}, 'hours', 30.0 * 24),
+        ({'days': 1, 'hours': 12}, 'hours', 36),
+        ({'hours': 1}, 'days', 1 / 24.0),
+    ],
+)
+def test_duration(time_delta_parameters: Dict[str, int], duration_in: str, expected_result: float):
     step = DurationStep(
         name='duration',
         newColumnName='DURATION',
         startDateColumn='START_DATE',
         endDateColumn='END_DATE',
-        durationIn='days',
+        durationIn=duration_in,
     )
 
     now = datetime.now()
-    sample_df = pd.DataFrame(
-        {
-            'START_DATE': [now, now],
-            'END_DATE': [now + timedelta(days=30, hours=12), now + timedelta(days=365)],
-        }
-    )
+    delta = timedelta(**time_delta_parameters)
+    sample_df = pd.DataFrame({'START_DATE': [now], 'END_DATE': [now + delta]})
 
     result_df = step.execute(sample_df)
 
     expected_result = pd.DataFrame(
         {
-            'START_DATE': [now, now],
-            'END_DATE': [now + timedelta(days=30, hours=12), now + timedelta(days=365)],
-            'DURATION': [30.0, 365.0],
+            'START_DATE': [now],
+            'END_DATE': [now + delta],
+            'DURATION': [expected_result],
         }
     )
 
