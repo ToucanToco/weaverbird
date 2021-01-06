89a5f52025e0cc6a56357734c76c8b869e79c38d
Luc Leonard <github@luc-leonard.fr>
2021-01-06 16:16:42 +0100

    feat(pandas/addmissingdate): added dates are now always at the beginning of the period

--- server/tests/steps/test_addmissingdates.py
+++ server/tests/steps/test_addmissingdates.py
@@ -48,8 +48,7 @@ def test_missing_date_years(today):
 
     # dates added by pandas are at the beginning of the last day of the year
     missing_dates = [
-        datetime.datetime(year=missing_date.year, month=12, day=31)
-        for missing_date in missing_dates
+        datetime.datetime(year=missing_date.year, month=1, day=1) for missing_date in missing_dates
     ]
     values = [idx for (idx, value) in enumerate(dates)]
 
@@ -97,6 +96,45 @@ def test_missing_date_with_groups(today):
                     'country': cast(List[Optional[Any]], ['France'] * 2 + ['USA'] * 2),
                     'date': missing_dates * 2,
                     'value': [None, None] * 2,
+                }
+            ),
+        ]
+    ).sort_values(by=['country', 'date'])
+    assert_dataframes_equals(result, expected_result)
+
+
+def test_missing_date_with_groups_various_length(today):
+    dates = [
+        datetime.datetime(year=2020, month=nb_month, day=1)
+        for nb_month in list(range(1, 5)) + list(range(8, 10))
+    ]
+
+    missing_dates = [datetime.datetime(year=2020, month=nb_month, day=1) for nb_month in [5, 6, 7]]
+
+    values = [idx for (idx, value) in enumerate(dates)]
+    df = pd.DataFrame(
+        {
+            'date': dates + dates[0:-1],
+            'country': ['France'] * len(dates) + ['USA'] * (len(dates) - 1),
+            'value': values + values[0:-1],
+        }
+    )
+
+    step = AddMissingDatesStep(
+        name='addmissingdates', datesColumn='date', datesGranularity='month', groups=['country']
+    )
+    result = step.execute(df)
+    expected_result = pd.concat(
+        [
+            df,
+            pd.DataFrame(
+                {
+                    'country': cast(
+                        List[Optional[Any]],
+                        ['France'] * len(missing_dates) + ['USA'] * len(missing_dates),
+                    ),
+                    'date': missing_dates * 2,
+                    'value': [None] * len(missing_dates) * 2,
                 }
             ),
         ]
