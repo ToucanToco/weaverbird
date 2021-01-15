f4c537b9c101d8828094aaace5f8acd2d525cd13
Frédéric Matigot <fred@fspot.org>
2021-01-15 16:20:58 +0100

    fix(server): fix util rename_duplicated_columns (#817)
    * fix(server): fix util rename_duplicated_columns
    
    * chore: bump weaverbird to 0.1.3
    
    * fix: test without poetry cache?

--- server/tests/test_utils.py
+++ server/tests/test_utils.py
@@ -1,13 +1,56 @@
+import pytest
 from pandas import DataFrame
 
 from weaverbird.utils import rename_duplicated_columns
 
 
-def test_rename_duplicated_columns():
-    columns = ['plop', 'foo', 'bar', 'foo', 'foo', 'bar', 'plip']
+@pytest.mark.parametrize(
+    ('columns', 'expected_new_columns'),
+    [
+        (
+            ['foo'],
+            ['foo'],
+        ),
+        (
+            ['foo', 'foo'],
+            ['foo', 'foo_1'],
+        ),
+        (
+            ['foo', 'foo_1', 'foo_2', 'foo_3', 'foo'],
+            ['foo', 'foo_1', 'foo_2', 'foo_3', 'foo_4'],
+        ),
+        (
+            ['foo_1', 'foo_1'],
+            ['foo_1', 'foo_2'],
+        ),
+        (
+            ['foo_1', 'foo_1', 'foo_2'],
+            ['foo_1', 'foo_3', 'foo_2'],
+        ),
+        (
+            ['foo', 'foo', 'foo_1'],
+            ['foo', 'foo_2', 'foo_1'],
+        ),
+        (
+            ['foo', 'foo_3', 'foo'],
+            ['foo', 'foo_3', 'foo_4'],
+        ),
+        (
+            ['foo', 'foo', 'foo_5', 'foo_5'],
+            ['foo', 'foo_6', 'foo_5', 'foo_7'],
+        ),
+        (
+            ['plop', 'foo', 'bar', 'foo', 'foo', 'bar', 'plip', 'foo_2', 'bar_1'],
+            ['plop', 'foo', 'bar', 'foo_3', 'foo_4', 'bar_2', 'plip', 'foo_2', 'bar_1'],
+        ),
+    ],
+)
+def test_rename_duplicated_columns(columns, expected_new_columns):
+    # ensure there are no duplicates in the output:
+    assert len(expected_new_columns) == len(set(expected_new_columns))
+
     df = DataFrame(columns=columns)
-    assert (df.columns == columns).all()
+    assert list(df.columns) == columns
 
     df_renamed = rename_duplicated_columns(df)
-    expected_columns = ['plop', 'foo', 'bar', 'foo_1', 'foo_2', 'bar_1', 'plip']
-    assert (df_renamed.columns == expected_columns).all()
+    assert list(df_renamed.columns) == expected_new_columns
