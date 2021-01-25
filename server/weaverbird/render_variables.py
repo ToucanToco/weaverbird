544031d6a1e88b4f4d158d4964ee73be9aa65329
fspot <fred@fspot.org>
2021-01-25 11:37:31 +0100

    use nosql_apply_parameters_to_query

--- server/weaverbird/render_variables.py
+++ server/weaverbird/render_variables.py
@@ -1,20 +1,13 @@
-import json
 from typing import Any, Dict
 
-from jinja2 import Template
+from toucan_connectors.common import nosql_apply_parameters_to_query
 
 from weaverbird.steps.base import BaseStep
-
-
-def render_dict_with_variables(d: dict, variables: Dict[str, Any]) -> dict:
-    dict_as_json = json.dumps(d)
-    rendered = Template(dict_as_json).render(**variables)
-    return json.loads(rendered)
 
 
 class StepWithVariablesMixin:
     def render(self, variables: Dict[str, Any]) -> BaseStep:
         RenderedClass = self.__class__.__bases__[0]
         step_as_dict = self.dict()  # type: ignore
-        rendered_dict = render_dict_with_variables(step_as_dict, variables)
+        rendered_dict = nosql_apply_parameters_to_query(step_as_dict, variables)
         return RenderedClass(**rendered_dict)
