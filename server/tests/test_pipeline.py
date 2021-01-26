0aec45269b92cd87434b8dae1c7887bab02b1944
Luc Leonard <github@luc-leonard.fr>
2021-01-26 11:33:33 +0100

    feat(templating variables): better test organization

--- server/tests/test_pipeline.py
+++ server/tests/test_pipeline.py
@@ -1,17 +1,26 @@
 import json
-
+import glob
 import pytest
-
 from weaverbird.pipeline import Pipeline, PipelineWithVariables
-from weaverbird.steps import FilterStep, FilterStepWithVariables, TopStep, TopStepWithVariables
 
 
-with open('fixtures/fixtures_templating/aggregate.json') as json_file:
-    test_data_for_templates = json.load(json_file)
+def get_test_cases():
+    test_cases = []
+    globs = glob.glob('fixtures/fixtures_templating/*.json')
+    for file in globs:
+        print(file)
+        with open(file) as json_file:
+            file_content = json.load(json_file)
+            for test in file_content:
+                test_cases.append(test)
+    return test_cases
 
 
-@pytest.mark.parametrize('data, context, expected_result', test_data_for_templates)
+@pytest.mark.parametrize('data, context, expected_result', get_test_cases())
 def test_aggregate_step_with_variables(data, context, expected_result):
+    print(data)
+    print(context)
+    print(expected_result)
     pipeline_with_variables = PipelineWithVariables(**data)
 
     pipeline = pipeline_with_variables.render(context)
