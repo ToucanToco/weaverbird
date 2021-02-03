26cfa730305c299ad599c2f42dfb320e87f3d700
Luc Leonard <github@luc-leonard.fr>
2021-02-03 11:53:51 +0100

    chores(pipeline test): remove debug print

--- server/tests/test_pipeline.py
+++ server/tests/test_pipeline.py
@@ -11,7 +11,6 @@ def get_test_cases():
     test_cases = []
     globs = glob.glob('./tests/fixtures/fixtures_templating/*.json')
     for file in globs:
-        print(file)
         with open(file) as json_file:
             file_content = json.load(json_file)
             for test in file_content:
@@ -21,9 +20,6 @@ def get_test_cases():
 
 @pytest.mark.parametrize('data, context, expected_result', get_test_cases())
 def test_aggregate_step_with_variables(data, context, expected_result):
-    print(data)
-    print(context)
-    print(expected_result)
     pipeline_with_variables = PipelineWithVariables(**data)
 
     pipeline = pipeline_with_variables.render(context, renderer=nosql_apply_parameters_to_query)
