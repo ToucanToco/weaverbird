b79223d904db8247a9468cc76ee02ad3204f8cf3
Luc Leonard <github@luc-leonard.fr>
2021-02-05 13:28:09 +0100

    feat(pipeline with variables): when a parametrized test fail, it prints its name

--- server/tests/test_pipeline.py
+++ server/tests/test_pipeline.py
@@ -1,28 +1,44 @@
 import glob
 import json
+from typing import Dict, List
 
 import pytest
+from pydantic.main import BaseModel
 from toucan_connectors.common import nosql_apply_parameters_to_query
 
 from weaverbird.pipeline import Pipeline, PipelineWithVariables
 
 
-def get_test_cases():
+class Case(BaseModel):
+    filename: str
+    data: Dict
+    context: Dict
+    expected_result: List
+
+
+def get_render_variables_test_cases():
     test_cases = []
     globs = glob.glob('./tests/fixtures/fixtures_templating/*.json')
     for file in globs:
         with open(file) as json_file:
             file_content = json.load(json_file)
             for test in file_content:
-                test_cases.append(test)
+                case = Case(filename=file, data=test[0], context=test[1], expected_result=test[2])
+                test_cases.append(case)
     return test_cases
 
 
-@pytest.mark.parametrize('data, context, expected_result', get_test_cases())
-def test_aggregate_step_with_variables(data, context, expected_result):
-    pipeline_with_variables = PipelineWithVariables(**data)
+cases = get_render_variables_test_cases()
+ids = map(lambda x: x.filename, cases)
 
-    pipeline = pipeline_with_variables.render(context, renderer=nosql_apply_parameters_to_query)
 
-    expected_result = Pipeline(steps=expected_result)
+@pytest.mark.parametrize('case', cases, ids=ids)
+def test_step_with_variables(case: Case):
+    pipeline_with_variables = PipelineWithVariables(**case.data)
+
+    pipeline = pipeline_with_variables.render(
+        case.context, renderer=nosql_apply_parameters_to_query
+    )
+
+    expected_result = Pipeline(steps=case.expected_result)
     assert pipeline == expected_result
