4c171f9a4f1f9fa6ecf4f239050c7858ebe7cd0b
Luc Léonard <github@luc-leonard.fr>
2021-04-07 18:40:13 +0200

    feat(steps): better defaults arguments

--- server/weaverbird/steps/base.py
+++ server/weaverbird/steps/base.py
@@ -19,23 +19,5 @@ class BaseStep(BaseModel, ABC):
     ) -> DataFrame:
         ...
 
-    def dict(
-        self,
-        *,
-        include=None,
-        exclude=None,
-        by_alias: bool = False,
-        skip_defaults: bool = None,
-        exclude_unset: bool = False,
-        exclude_defaults: bool = False,
-        exclude_none: bool = True,
-    ) -> Dict:
-        return super().dict(
-            include=include,
-            exclude=exclude,
-            by_alias=by_alias,
-            skip_defaults=skip_defaults,
-            exclude_unset=exclude_unset,
-            exclude_defaults=exclude_defaults,
-            exclude_none=exclude_none,
-        )
+    def dict(self, *, exclude_none: bool = True, **kwargs) -> Dict:
+        return super().dict(exclude_none=exclude_none, **kwargs)
