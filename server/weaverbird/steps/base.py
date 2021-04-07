b6eba1efdfa5383331a0e19fda121d1be6b614f0
Luc Léonard <github@luc-leonard.fr>
2021-04-07 18:24:14 +0200

    feat(steps): the .dict() method of step should not contain None elements. It will avoid triggering validation error in the front end.

--- server/weaverbird/steps/base.py
+++ server/weaverbird/steps/base.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Union
 
 from pandas import DataFrame
 from pydantic.main import BaseModel
@@ -17,3 +18,22 @@ class BaseStep(BaseModel, ABC):
         self, df: DataFrame, domain_retriever: DomainRetriever, pipeline_executor: PipelineExecutor
     ) -> DataFrame:
         ...
+
+    def dict(
+        self,
+        *,
+        include: Union['AbstractSetIntStr', 'MappingIntStrAny'] = None,
+        exclude: Union['AbstractSetIntStr', 'MappingIntStrAny'] = None,
+        by_alias: bool = False,
+        skip_defaults: bool = None,
+        exclude_unset: bool = False,
+        exclude_defaults: bool = False,
+        exclude_none: bool = True,
+    ) -> 'DictStrAny':
+        return super().dict(include=include,
+                            exclude=exclude,
+                            by_alias=by_alias,
+                            skip_defaults=skip_defaults,
+                            exclude_unset=exclude_unset,
+                            exclude_defaults=exclude_defaults,
+                            exclude_none=exclude_none)
