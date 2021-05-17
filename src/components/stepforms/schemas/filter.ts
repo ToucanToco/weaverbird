9cb242ca78c64838dde20210a41a69655309bcdd
alice.sevin <alice.sevin@toucantoco.com>
2021-05-17 16:11:37 +0200

    fix(filter-condition): enable to have undefined value

--- src/components/stepforms/schemas/filter.ts
+++ src/components/stepforms/schemas/filter.ts
@@ -128,7 +128,7 @@ export default {
           },
         },
       },
-      required: ['column', 'value', 'operator'],
+      required: ['column', 'operator'],
       additionalProperties: false,
     },
   },
