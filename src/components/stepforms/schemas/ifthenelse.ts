09fed6d2271fb7965e209b23c55aca911dfd3aee
alice-sevin <59559689+alice-sevin@users.noreply.github.com>
2021-05-18 12:05:32 +0200

    fix(filter-condition): enable to have undefined value (#909)

--- src/components/stepforms/schemas/ifthenelse.ts
+++ src/components/stepforms/schemas/ifthenelse.ts
@@ -156,7 +156,7 @@ export default {
           },
         },
       },
-      required: ['column', 'value', 'operator'],
+      required: ['column', 'operator'],
       additionalProperties: false,
     },
     formula: {
