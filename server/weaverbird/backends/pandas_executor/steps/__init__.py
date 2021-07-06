e0bd2ee1b3eeeb7f91591f95164083c6c064d685
fspot <fred@fspot.org>
2021-07-06 17:20:26 +0200

    refactor(server): isolate formula pandas implem

--- server/weaverbird/backends/pandas_executor/steps/__init__.py
+++ server/weaverbird/backends/pandas_executor/steps/__init__.py
@@ -18,8 +18,8 @@ from .duration import execute_duration
 from .evolution import execute_evolution
 from .fillna import execute_fillna
 from .filter import execute_filter
+from .formula import execute_formula
 
-# from .formula import execute as execute_formula
 # from .fromdate import execute as execute_fromdate
 # from .ifthenelse import execute as execute_ifthenelse
 # from .join import execute as execute_join
@@ -63,7 +63,7 @@ steps_executors: Dict[str, StepExecutor] = {
     'evolution': execute_evolution,
     'fillna': execute_fillna,
     'filter': execute_filter,
-    # 'formula': execute_formula,
+    'formula': execute_formula,
     # 'fromdate': execute_fromdate,
     # 'ifthenelse': execute_ifthenelse,
     # 'join': execute_join,
