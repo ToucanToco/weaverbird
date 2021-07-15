e301d7318d8c4e7d49e1b9052a8b6c5b1dda094d
David Nowinsky <david@nowinsky.net>
2021-07-15 16:54:58 +0200

    test(server): fixtures for concatenate

--- server/tests/steps/test_concatenate.py
+++ server/tests/steps/test_concatenate.py
@@ -1,31 +1,7 @@
 from pandas import DataFrame
 
-from tests.utils import assert_dataframes_equals
 from weaverbird.backends.pandas_executor.steps.concatenate import execute_concatenate
 from weaverbird.pipeline.steps import ConcatenateStep
-
-
-def test_concatenate():
-    sample_df = DataFrame({'NAME': ['foo', 'bar'], 'AGE': [42, 43], 'SCORE': [100, 200]})
-
-    step = ConcatenateStep(
-        name='concatenate',
-        columns=['NAME', 'AGE', 'SCORE'],
-        separator=' - ',
-        new_column_name='newcol',
-    )
-
-    df_result = execute_concatenate(step, sample_df)
-
-    expected_result = DataFrame(
-        {
-            'NAME': ['foo', 'bar'],
-            'AGE': [42, 43],
-            'SCORE': [100, 200],
-            'newcol': ['foo - 42 - 100', 'bar - 43 - 200'],
-        }
-    )
-    assert_dataframes_equals(df_result, expected_result)
 
 
 def test_benchmark_concatenate(benchmark):
