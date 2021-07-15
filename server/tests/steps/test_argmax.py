f58b4b407432aeda1f189806c64cd06e0e2fad6b
David Nowinsky <david@nowinsky.net>
2021-07-15 16:48:09 +0200

    test(server): fixture for argmax

--- server/tests/steps/test_argmax.py
+++ server/tests/steps/test_argmax.py
@@ -1,51 +1,8 @@
 import numpy as np
-import pytest
 from pandas import DataFrame
 
-from tests.utils import assert_dataframes_equals
 from weaverbird.backends.pandas_executor.steps.argmax import execute_argmax
 from weaverbird.pipeline.steps.argmax import ArgmaxStep
-
-
-@pytest.fixture
-def sample_df():
-    return DataFrame(
-        {
-            'label': ['label1', 'label2', 'label3', 'label4', 'label5', 'label6'],
-            'group': ['group 1', 'group 1', 'group 1', 'group 2', 'group 2', 'group 2'],
-            'value': [13, 7, 20, 1, 10, 5],
-        }
-    )
-
-
-def test_simple_argmax(sample_df):
-    step = ArgmaxStep(name='argmax', column='value')
-    result = execute_argmax(step, sample_df, domain_retriever=None)
-    assert_dataframes_equals(
-        result,
-        DataFrame(
-            {
-                'label': ['label3'],
-                'group': ['group 1'],
-                'value': [20],
-            }
-        ),
-    )
-
-
-def test_argmax_with_group(sample_df):
-    step = ArgmaxStep(name='argmax', column='value', groups=['group'])
-    result = execute_argmax(step, sample_df, domain_retriever=None)
-    assert_dataframes_equals(
-        result,
-        DataFrame(
-            {
-                'label': ['label3', 'label5'],
-                'group': ['group 1', 'group 2'],
-                'value': [20, 10],
-            }
-        ),
-    )
 
 
 def test_benchmark_argmax(benchmark):
