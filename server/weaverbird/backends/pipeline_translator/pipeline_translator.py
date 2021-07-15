eb3ca583ce298ab7e878ab1c2f53dec9cf11a954
raphael <raphael.vignes@toucantoco.com>
2021-07-15 15:21:24 +0200

    feat(sql translator):poc sql translator

---
+++ server/weaverbird/backends/pipeline_translator/pipeline_translator.py
@@ -0,0 +1,43 @@
+from typing import Tuple
+
+from weaverbird.pipeline import PipelineWithVariables, Pipeline, PipelineStep
+
+from server.weaverbird.backends.pipeline_translator.types import QueryRetriever, PipelineTranslationReport, \
+    StepTranslationReport
+from server.weaverbird.backends.pipeline_translator.steps import steps_translators
+
+
+def translate_pipeline(
+    pipeline_to_translate: Pipeline,
+    query_retriever: QueryRetriever
+) -> Tuple[str, PipelineTranslationReport]:
+    """ For now, steps_translators are only SQL steps translators """
+    query = ''
+    translate_report = []
+    for index, step in enumerate(pipeline_to_translate.steps):
+        try:
+            query = steps_translators[step.name](
+                step,
+                query,
+                query_retriever=query_retriever,
+                translate_pipeline=translate_pipeline,
+            )
+            translate_report.append(
+                StepTranslationReport(
+                    step_index=index
+                )
+            )
+        except Exception as e:
+            raise PipelineTranslationFailure(step, index, e) from e
+    return query, PipelineTranslationReport(steps_translation_reports=translate_report)
+
+
+class PipelineTranslationFailure(Exception):
+    """ Raised when a error happens during the translation of the pipeline """
+
+    def __init__(self, step: PipelineStep, index: int, original_exception: Exception):
+        self.step = step
+        self.index = index
+        self.original_exception = original_exception
+        self.message = f'Step #{index + 1} ({step.name}) failed: {original_exception}'
+        self.details = {'index': index, 'message': self.message}
---
+++ server/weaverbird/backends/pipeline_translator/types.py
@@ -0,0 +1,28 @@
+from typing import List, Callable, Tuple, Protocol, Any, Optional
+
+from pydantic import BaseModel
+from weaverbird.pipeline import Pipeline
+
+
+class StepTranslationReport(BaseModel):
+    step_index: int
+
+
+class PipelineTranslationReport(BaseModel):
+    steps_translation_reports: List[StepTranslationReport]
+
+
+QueryRetriever = Callable[[str], str]
+PipelineTranslator = Callable[[Pipeline, QueryRetriever], Tuple[str, PipelineTranslationReport]]
+
+
+class StepTranslator(Protocol):
+    def __call__(
+        self,
+        step: Any,
+        query: str,
+        query_retriever: Optional[QueryRetriever],
+        translate_pipeline: Optional[PipelineTranslator]
+
+    ):
+        ...
