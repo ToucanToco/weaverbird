937aa0cd529e1803da949e2d2bfdcf7f4175185f
raphael.vignes@toucantoco.com <raphael.vignes@toucantoco.com>
2021-07-29 14:29:16 +0200

    test(sql table step): update test with named column

--- server/tests/steps/sql_translator/test_table.py
+++ server/tests/steps/sql_translator/test_table.py
@@ -6,25 +6,42 @@ from weaverbird.backends.sql_translator.steps import translate_table
 from weaverbird.pipeline.steps import TableStep
 
 
-def test_translate_table():
+@pytest.fixture
+def sql_query_describer():
+    return Mock(return_value={'toto': 'int', 'raichu': 'str'})
+
+
+def test_translate_table(sql_query_describer):
     sql_table_retriever_mock = Mock(
         return_value='SELECT * FROM products'
     )  # TODO update when retrieve_query will be updated
 
     step = TableStep(name='domain', domain='kalimdor')
-    query = translate_table(step, None, sql_query_retriever=sql_table_retriever_mock, index=0)
+    query = translate_table(
+        step,
+        None,
+        sql_query_retriever=sql_table_retriever_mock,
+        sql_query_describer=sql_query_describer,
+        index=0,
+    )
 
     sql_table_retriever_mock.assert_called_once_with('kalimdor')
     assert query.transformed_query == 'WITH SELECT_STEP_0 AS (SELECT * FROM products)'
-    assert query.selection_query == 'SELECT * FROM SELECT_STEP_0'
+    assert query.selection_query == 'SELECT toto, raichu FROM SELECT_STEP_0'
     assert query.query_name == 'SELECT_STEP_0'
 
 
-def test_translate_table_error_retrieve_table():
+def test_translate_table_error_retrieve_table(sql_query_describer):
     sql_table_retriever_mock = Mock(
         side_effect=Exception
     )  # TODO update when retrieve_query will be updated
 
     step = TableStep(name='domain', domain='kalimdor')
     with pytest.raises(Exception):
-        translate_table(step, None, sql_table_retriever=sql_table_retriever_mock, index=0)
+        translate_table(
+            step,
+            None,
+            sql_table_retriever=sql_table_retriever_mock,
+            sql_query_describer=sql_query_describer,
+            index=0,
+        )
