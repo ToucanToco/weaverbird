b6b768cf082180a01abfa42aca5cdc3cd806eed6
FBB <florent.bertrand@toucantoco.com>
2021-08-05 07:10:02 +0200

    feat(e2e_test): finish test

--- server/tests/backends/sql_translator/test_sql_translator_steps.py
+++ server/tests/backends/sql_translator/test_sql_translator_steps.py
@@ -1,15 +1,56 @@
 # import json
+from typing import Union, Dict
 from glob import glob
 from os import path
 
 import pymysql
 import pytest
 
+import json
+import pandas as pd
+
+from server.tests.utils import assert_dataframes_equals
+
 from weaverbird.backends.sql_translator import translate_pipeline
 from weaverbird.pipeline import Pipeline
 
-fixtures_dir_path = path.join(path.dirname(path.realpath(__file__)), '../fixtures')
+fixtures_dir_path = path.join(path.dirname(path.realpath(__file__)), '../fixtures_sql')
 step_cases_files = glob(path.join(fixtures_dir_path, '*/*.json'))
+
+type_code_mapping = {
+    0: 'int',
+    1: 'float',
+    2: 'str',
+    3: 'date',
+    4: 'timestamp',
+    5: 'variant',
+    6: 'timestamp_ltz',
+    7: 'timestamp_tz',
+    8: 'timestampe_ntz',
+    9: 'object',
+    10: 'array',
+    11: 'binary',
+    12: 'time',
+    13: 'boolean',
+}
+
+
+# Update this method to use snowflake connection
+def connect():
+    conv = pymysql.converters.conversions.copy()
+    conv[246] = float
+    con_params = {
+        'host': '127.0.0.1',
+        'user': 'ubuntu',
+        'password': 'ilovetoucan',
+        'port': 3306,  # mysql_server['port'],
+        'database': 'mysql_db',
+        'charset': 'utf8mb4',
+        'conv': conv,
+        'cursorclass': pymysql.cursors.DictCursor,
+    }
+    conn = pymysql.connect(**con_params)
+    return conn
 
 
 @pytest.fixture(scope='module')
@@ -26,39 +67,51 @@ def mysql_server(service_container):
 
 @pytest.fixture
 def mysql_connector(mysql_server):
-    conv = pymysql.converters.conversions.copy()
-    conv[246] = float
-    con_params = {
-        'host': '127.0.0.1',
-        'user': 'ubuntu',
-        'password': 'ilovetoucan',
-        'port': 3306,  # mysql_server['port'],
-        'database': 'mysql_db',
-        'charset': 'utf8mb4',
-        # 'connect_timeout': self.connect_timeout,
-        'conv': conv,
-        'cursorclass': pymysql.cursors.DictCursor,
-    }
-    conn = pymysql.connect(**con_params)
-    return conn
+    return connect()
 
 
 def execute(connection, query: str):
-    print(connection)
-    print(query)
     with connection.cursor() as cursor:
         cursor.execute(query)
         return cursor.fetchall()
 
 
 def sql_retrieve_city(t):
-    return 'SELECT * FROM City'
+    return t
+
+
+def sql_query_describer(t) -> Union[Dict[str, str], None]:
+    lst = t.split(" ")
+    lst = [item for item in lst if len(item) > 0]
+    temp = lst[lst.index("FROM") + 1]
+
+    if len(temp.split('.')) == 2:
+        table_name = temp.split('.')[1]
+    else:
+        table_name = temp.split('.')[0]
+
+    request = f'SELECT column_name as name, data_type as type_code FROM information_schema.columns WHERE table_name = "{table_name}" ORDER BY ordinal_position;'
+
+    connection = connect()
+    with connection.cursor() as cursor:
+        cursor.execute(request)
+        describe_res = cursor.fetchall()
+        res = {r['name']: r['type_code'] for r in describe_res}
+        return res
+
+
+def snowflake_query_describer(t) -> Union[Dict[str, str], None]:
+    connection = connect()
+    with connection.cursor() as cursor:
+        describe_res = cursor.describe(t)
+        res = {r.name: type_code_mapping.get(r.type_code) for r in describe_res}
+        return res
 
 
 test_cases = []
 for x in step_cases_files:
     # Generate a readable id for each test case
-    case_hierarchy = path.dirname(x)[len(fixtures_dir_path) :]
+    case_hierarchy = path.dirname(x)[len(fixtures_dir_path):]
     case_name = path.splitext(path.basename(x))[0]
     case_id = case_hierarchy + '_' + case_name
 
@@ -68,38 +121,23 @@ for x in step_cases_files:
 # Translation from Pipeline json to SQL query
 @pytest.mark.parametrize('case_id,case_spec_file_path', test_cases)
 def test_sql_translator_pipeline(case_id, case_spec_file_path, mysql_connector):
-    # spec_file = open(case_spec_file_path, 'r')
-    # spec = json.loads(spec_file.read())
-    # spec_file.close()
+    spec_file = open(case_spec_file_path, 'r')
+    spec = json.loads(spec_file.read())
+    spec_file.close()
 
-    # test = json.dumps(spec['input'])
-    # pipeline = Pipeline(steps=[{'name': 'domain', 'domain': 'in'}, spec['step']])
-    pipeline = Pipeline(
-        steps=[
-            {'name': 'domain', 'domain': 'domain_a'},
-            {
-                'name': 'filter',
-                'condition': {'column': 'CountryCode', 'operator': 'eq', 'value': 'FRA'},
-            },
-        ]
+    pipeline = Pipeline(steps=spec['step']['pipeline'])
+    pandas_result_expected = pd.DataFrame.from_dict(spec['output']['sql']['result'])
+    query_expected = spec['output']['sql']['query']
+
+    query, report = translate_pipeline(
+        pipeline,
+        sql_query_retriever=sql_retrieve_city,
+        sql_query_describer=sql_query_describer  # replace by snowflake_query_describer
     )
-    query, report = translate_pipeline(pipeline, sql_query_retriever=sql_retrieve_city)
-    print(query)
-    result = execute(mysql_connector, query)
-    print(result)
-
-    # df_in = pd.read_json(json.dumps(spec['input']), orient='table')
-    # df_out = pd.read_json(json.dumps(spec['expected']), orient='table')
-    # dfs_in_others = {
-    #     k: pd.read_json(json.dumps(v), orient='table')
-    #     for (k, v) in spec.get('other_inputs', {}).items()
-    # }
-    #
-    # pipeline = Pipeline(steps=[{'name': 'domain', 'domain': 'in'}, spec['step']])
-    # DOMAINS = {'in': df_in, **dfs_in_others}
-    # result = execute_pipeline(pipeline, domain_retriever=lambda x: DOMAINS[x])[0]
-    #
-    # assert_dataframes_equals(df_out, result)
+    sql_result = execute(mysql_connector, query)
+    assert query_expected == query
+    result = pd.DataFrame.from_dict(sql_result)
+    assert_dataframes_equals(pandas_result_expected, result)
 
 
 # Translation and execute from ...
