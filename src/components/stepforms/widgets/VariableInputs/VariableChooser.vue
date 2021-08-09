3b89768d045f66732fcfa82eee1766a47d74a6fb
alice.sevin <alice.sevin@toucantoco.com>
2021-08-09 15:42:49 +0200

    feat(variable-chooser): use new variable list component

--- src/components/stepforms/widgets/VariableInputs/VariableChooser.vue
+++ src/components/stepforms/widgets/VariableInputs/VariableChooser.vue
@@ -6,37 +6,13 @@
     bottom
     @closed="close"
   >
-    <div class="widget-variable-chooser__options-container">
-      <div
-        class="widget-variable-chooser__options-section"
-        v-for="category in variablesByCategory"
-        :key="category.label"
-      >
-        <div class="widget-variable-chooser__option-section-title" v-if="category.label">
-          {{ category.label }}
-        </div>
-        <div
-          class="widget-variable-chooser__option"
-          :class="{ 'widget-variable-chooser__option--selected': availableVariable.selected }"
-          v-for="availableVariable in category.variables"
-          :key="availableVariable.identifier"
-          v-tooltip="{
-            targetClasses: 'has-weaverbird__tooltip',
-            classes: 'weaverbird__tooltip',
-            content: makeValueReadable(availableVariable.value),
-            placement: 'bottom-center',
-          }"
-          @click="chooseVariable(availableVariable.identifier)"
-        >
-          <div class="widget-variable-chooser__option-container">
-            <span v-if="isMultiple" class="widget-variable-chooser__option-toggle" />
-            <span class="widget-variable-chooser__option-name">{{ availableVariable.label }}</span>
-          </div>
-          <span class="widget-variable-chooser__option-value">{{
-            formatIfDate(availableVariable.value)
-          }}</span>
-        </div>
-      </div>
+    <div class="widget-variable-chooser__list-container">
+      <VariableList
+        :isMultiple="isMultiple"
+        :selectedVariables="selectedVariables"
+        :availableVariables="availableVariables"
+        @input="chooseVariable"
+      />
       <div class="widget-advanced-variable" @click="addAdvancedVariable">
         Advanced variable
       </div>
@@ -45,20 +21,19 @@
 </template>
 
 <script lang="ts">
-import VTooltip from 'v-tooltip';
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
 import { POPOVER_ALIGN } from '@/components/constants';
 import Popover from '@/components/Popover.vue';
-import { VariablesBucket, VariablesCategory } from '@/lib/variables';
+import { VariablesBucket } from '@/lib/variables';
 
-Vue.use(VTooltip);
+import VariableList from './VariableList.vue';
 /**
  * This component list all the available variables to use as value in VariableInputs
  */
 @Component({
   name: 'variable-chooser',
-  components: { Popover },
+  components: { Popover, VariableList },
 })
 export default class VariableChooser extends Vue {
   @Prop({ default: false })
@@ -74,52 +49,6 @@ export default class VariableChooser extends Vue {
   isOpened!: boolean;
 
   alignLeft: string = POPOVER_ALIGN.LEFT;
-
-  /**
-   * Group variables by category to easily choose among them
-   *
-   * https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_groupby
-   */
-  get variablesByCategory(): VariablesCategory[] {
-    return this.availableVariables.reduce((categories: VariablesCategory[], variable) => {
-      const varCategoryLabel = variable.category;
-      const category = categories.find(c => c.label === varCategoryLabel);
-      const variableObj = {
-        ...variable,
-        selected: this.isVariableSelected(variable.identifier),
-      };
-      if (category !== undefined) {
-        category.variables.push(variableObj);
-      } else {
-        categories.push({
-          label: varCategoryLabel,
-          variables: [variableObj],
-        });
-      }
-      return categories;
-    }, []);
-  }
-
-  formatIfDate(value: any): any {
-    if (value instanceof Date) {
-      return value.toUTCString();
-    }
-    return value;
-  }
-
-  /**
-  Return a readable value to display as tooltip
-  **/
-  makeValueReadable(value: any): string {
-    return JSON.stringify(value);
-  }
-
-  /**
-  Verify if item is selected (multiple mode)
-  **/
-  isVariableSelected(variableIdentifier: string): boolean {
-    return this.isMultiple && this.selectedVariables.indexOf(variableIdentifier) != -1;
-  }
 
   close() {
     this.$emit('closed');
@@ -140,76 +69,16 @@ export default class VariableChooser extends Vue {
 
 <style scoped lang="scss">
 @import '../../../../styles/variables';
-
-.widget-variable-chooser__options-container {
-  display: flex;
+.widget-variable-chooser__list-container {
   border-radius: 2px;
-  width: 200px;
+  width: 300px;
   max-height: 300px;
   background-color: #fff;
   box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.25);
   color: $base-color;
   overflow: hidden;
-  flex-direction: column;
   overflow-y: auto;
 }
-
-.widget-variable-chooser__options-section {
-  border-bottom: 1px solid #eeeeee;
-  padding-bottom: 10px;
-}
-
-.widget-variable-chooser__option-section-title {
-  font-style: italic;
-  color: #888888;
-  font-size: 10px;
-  font-weight: 500;
-}
-
-.widget-variable-chooser__option-section-title,
-.widget-variable-chooser__option {
-  padding: 12px;
-}
-
-.widget-variable-chooser__option {
-  &:hover {
-    background-color: rgba(42, 102, 161, 0.05);
-    color: #2a66a1;
-    .widget-variable-chooser__option-value {
-      color: #2a66a1;
-    }
-    .widget-variable-chooser__option-toggle {
-      border-color: #2a66a1;
-    }
-  }
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  cursor: pointer;
-}
-
-.widget-variable-chooser__option-container {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-}
-
-.widget-variable-chooser__option-name {
-  font-size: 12px;
-  font-weight: 500;
-}
-
-.widget-variable-chooser__option-value {
-  font-size: 10px;
-  font-weight: 500;
-  color: #888888;
-  flex-shrink: 1;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  margin-left: 1em;
-}
-
 .widget-advanced-variable {
   padding: 12px;
   font-size: 12px;
@@ -218,67 +87,10 @@ export default class VariableChooser extends Vue {
   background: #f5f5f5;
   color: #888888;
   cursor: pointer;
+  margin-top: 8px;
 
   &:hover {
     text-decoration: underline;
-  }
-}
-
-.widget-variable-chooser__option-toggle {
-  position: relative;
-  background: white;
-  width: 12px;
-  height: 12px;
-  border: 1px solid #d4d4d4;
-  border-radius: 1px;
-  margin-right: 10px;
-
-  &:active,
-  &:focus,
-  &:hover {
-    border-color: #2a66a1;
-  }
-
-  &::before,
-  &::after {
-    content: '';
-    order: -1;
-    display: block;
-    background: black;
-    height: 2px;
-    top: 4px;
-    position: absolute;
-    opacity: 0;
-  }
-
-  &::before {
-    right: 1px;
-    width: 7px;
-    transform: rotate(-45deg);
-  }
-
-  &::after {
-    left: 1px;
-    width: 3px;
-    transform: rotate(45deg);
-  }
-}
-
-.widget-variable-chooser__option--selected {
-  background: #f5f5f5;
-  .widget-variable-chooser__option-toggle {
-    background: black;
-    border-color: black;
-    &::before,
-    &::after {
-      background: white;
-      opacity: 1;
-    }
-  }
-  &:hover {
-    .widget-variable-chooser__option-toggle {
-      background: #2a66a1;
-    }
   }
 }
 </style>
--- tests/unit/variable-chooser.spec.ts
+++ tests/unit/variable-chooser.spec.ts
@@ -1,5 +1,4 @@
 import { shallowMount, Wrapper } from '@vue/test-utils';
-import { VTooltip } from 'v-tooltip';
 
 import VariableChooser from '@/components/stepforms/widgets/VariableInputs/VariableChooser.vue';
 
@@ -9,9 +8,6 @@ describe('Variable Chooser', () => {
   beforeEach(() => {
     wrapper = shallowMount(VariableChooser, {
       sync: false,
-      directives: {
-        tooltip: VTooltip,
-      },
       propsData: {
         isOpened: false,
         availableVariables: [
@@ -64,59 +60,8 @@ describe('Variable Chooser', () => {
     expect(wrapper.find('popover-stub').exists()).toBeTruthy();
   });
 
-  it('should present all available variables organized in sections', () => {
-    const sections = wrapper.findAll('.widget-variable-chooser__options-section');
-    expect(sections).toHaveLength(2);
-    expect(
-      sections
-        .at(0)
-        .find('.widget-variable-chooser__option-section-title')
-        .text(),
-    ).toEqual('App variables');
-    expect(
-      sections
-        .at(1)
-        .find('.widget-variable-chooser__option-section-title')
-        .text(),
-    ).toEqual('Story variables');
-    const varsFromFirstSection = sections.at(0).findAll('.widget-variable-chooser__option');
-    expect(varsFromFirstSection).toHaveLength(4);
-  });
-
-  it('should display variables current values along their names', () => {
-    wrapper.findAll('.widget-variable-chooser__option').wrappers.forEach(w => {
-      expect(w.find('.widget-variable-chooser__option-name').text()).not.toBe('');
-      expect(w.find('.widget-variable-chooser__option-value').text()).not.toBe('');
-    });
-  });
-
-  it('should display dates values in UTC timezone', () => {
-    expect(
-      wrapper
-        .findAll('.widget-variable-chooser__option')
-        .at(3)
-        .find('.widget-variable-chooser__option-value')
-        .text(),
-    ).toStrictEqual('Fri, 11 Jun 2021 08:09:17 GMT');
-  });
-
-  describe('tooltip', () => {
-    it('should display a value tooltip for each variable', () => {
-      wrapper.findAll('.widget-variable-chooser__option').wrappers.forEach(w => {
-        expect(w.classes()).toContain('has-weaverbird__tooltip');
-      });
-    });
-
-    it('should display readable value in tooltip', () => {
-      expect((wrapper.vm as any).makeValueReadable([])).toStrictEqual('[]');
-      expect((wrapper.vm as any).makeValueReadable([1, 2])).toStrictEqual('[1,2]');
-      expect((wrapper.vm as any).makeValueReadable(1)).toStrictEqual('1');
-      expect((wrapper.vm as any).makeValueReadable('1')).toStrictEqual('"1"');
-      expect((wrapper.vm as any).makeValueReadable(undefined)).toStrictEqual(undefined);
-      expect((wrapper.vm as any).makeValueReadable(new Date(1623398957013))).toStrictEqual(
-        '"2021-06-11T08:09:17.013Z"',
-      );
-    });
+  it('should instantiate a variable list', () => {
+    expect(wrapper.find('VariableList-stub').exists()).toBeTruthy();
   });
 
   describe('when is advanced', () => {
@@ -155,30 +100,13 @@ describe('Variable Chooser', () => {
 
   describe('when choosing a variable', () => {
     beforeEach(async () => {
-      wrapper.find('.widget-variable-chooser__option').trigger('click');
+      wrapper.find('VariableList-stub').vm.$emit('input', 'appRequesters.view');
       await wrapper.vm.$nextTick();
     });
 
     it('should emit a new value with the chosen variable', () => {
       expect(wrapper.emitted('input')).toHaveLength(1);
       expect(wrapper.emitted('input')[0]).toEqual(['appRequesters.view']);
-    });
-  });
-
-  describe('multiple mode', () => {
-    beforeEach(async () => {
-      wrapper.setProps({
-        isMultiple: true,
-        selectedVariables: ['appRequesters.date.month', 'appRequesters.date.year'],
-      });
-    });
-
-    it('should display checkboxes before options', () => {
-      expect(wrapper.findAll('.widget-variable-chooser__option-toggle').length).toBe(6);
-    });
-
-    it('should highlight selected options', () => {
-      expect(wrapper.findAll('.widget-variable-chooser__option--selected').length).toBe(2);
     });
   });
 });
