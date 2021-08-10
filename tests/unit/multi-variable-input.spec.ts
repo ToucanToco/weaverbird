6108e4a2aa5979a674f2d39924f9540cfe261705
alice.sevin <alice.sevin@toucantoco.com>
2021-08-10 14:42:53 +0200

    feat(multi-variable-input): remove toggle to move it to variable list

--- tests/unit/multi-variable-input.spec.ts
+++ tests/unit/multi-variable-input.spec.ts
@@ -41,19 +41,11 @@ describe('Variable Input', () => {
   });
 
   describe('when choosing a variable', () => {
-    it('should add the value to the list ...', async () => {
-      wrapper.find('VariableInputBase-stub').vm.$emit('input', '{{ appRequesters.view }}');
+    it('should emit the selected variables', async () => {
+      wrapper.find('VariableInputBase-stub').vm.$emit('input', ['{{ appRequesters.view }}']);
       await wrapper.vm.$nextTick();
       expect(wrapper.emitted('input')).toHaveLength(1);
       expect(wrapper.emitted('input')[0][0]).toEqual(['{{ appRequesters.view }}']);
-    });
-
-    it('... or remove it if already in', async () => {
-      wrapper.setProps({ value: ['{{ appRequesters.view }}'] });
-      wrapper.find('VariableInputBase-stub').vm.$emit('input', '{{ appRequesters.view }}');
-      await wrapper.vm.$nextTick();
-      expect(wrapper.emitted('input')).toHaveLength(1);
-      expect(wrapper.emitted('input')[0][0]).toEqual([]);
     });
   });
 
