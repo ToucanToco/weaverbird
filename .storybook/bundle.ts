83a4f14ddbda6ad7ec9c6c9c5804656fc1f2c46d
alice.sevin <alice.sevin@toucantoco.com>
2021-08-12 11:47:10 +0200

    feat(calendar): add storybook

--- .storybook/bundle.ts
+++ .storybook/bundle.ts
@@ -36,6 +36,7 @@ import WaterfallStepForm from '../src/components/stepforms/WaterfallStepForm.vue
 import AddTotalRowsStepForm from '../src/components/stepforms/AddTotalRowsStepForm.vue';
 import TotalDimensions from '../src/components/stepforms/widgets/TotalDimensions.vue';
 import Popover from '../src/components/Popover.vue';
+import Calendar from '../src/components/Calendar.vue';
 
 export {
   FilterEditor,
@@ -71,6 +72,7 @@ export {
   AddTotalRowsStepForm,
   TotalDimensions,
   Popover,
+  Calendar,
 };
 export { setupStore, registerModule, VQBnamespace } from '../src/store';
 export { resizable } from '../src/directives/resizable/resizable';
