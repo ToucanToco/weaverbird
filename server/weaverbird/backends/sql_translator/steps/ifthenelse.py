c3b576d02391818f934aca5ac086df337412b3d6
sanix-darker <s4nixd@gmail.com>
2021-08-13 13:39:32 +0200

    fix(vqb-step): updates/fix ifthenelse step

--- server/weaverbird/backends/sql_translator/steps/ifthenelse.py
+++ server/weaverbird/backends/sql_translator/steps/ifthenelse.py
@@ -20,6 +20,21 @@ from weaverbird.pipeline.conditions import (
     SimpleCondition,
 )
 from weaverbird.pipeline.steps import IfthenelseStep
+from weaverbird.pipeline.steps.ifthenelse import IfThenElse
+
+
+def complete_fields(query: SQLQuery) -> str:
+    """
+    We're going to complete missing field from the query
+    """
+    fields: list = []
+    compiled_query: str = ""
+    for table in [*query.metadata_manager.tables_metadata]:
+        # TODO : changes the management columns on joins with duplicated columns
+        for elt in query.metadata_manager.tables_metadata[table].keys():
+            compiled_query += f'{elt}, ' if elt not in fields else ''
+
+    return compiled_query
 
 
 def format_condition(condition: Union[SimpleCondition, ConditionComboAnd, ConditionComboOr]):
@@ -54,9 +69,15 @@ def format_condition(condition: Union[SimpleCondition, ConditionComboAnd, Condit
 
     # for an normal condition
     if type(condition) in [ComparisonCondition, InclusionCondition, NullCondition, MatchCondition]:
-        composed_value: str = str(
-            tuple(condition.value) if type(condition.value) == list else condition.value
-        ).replace(",)", ")")
+        if type(condition) == MatchCondition:
+            composed_value = f"'{condition.value}'"
+        elif type(condition) == NullCondition:
+            composed_value: str = ""
+        else:
+            composed_value: str = str(
+                tuple(condition.value) if type(condition.value) == list else condition.value
+            ).replace(",)", ")")
+
         compiled_query += (
             f"""{condition.column + vqb_to_sql[condition.operator] + composed_value}"""
         )
@@ -74,7 +95,7 @@ def recurse_format_if_then_else(step: IfthenelseStep) -> str:
     """
     composed_query: str = "/-*-/"
     # while we have a nested else, pack the query
-    while type(step.else_value) == IfthenelseStep:
+    while type(step.else_value) in [IfThenElse, IfthenelseStep, SimpleCondition, ConditionComboAnd, ConditionComboOr]:
         composed_query = composed_query.replace(
             "/-*-/",
             f"""IFF({format_condition(step.condition)}, {step.then}, /-*-/)""",
@@ -83,7 +104,7 @@ def recurse_format_if_then_else(step: IfthenelseStep) -> str:
 
     return composed_query.replace(
         "/-*-/",
-        f"""IFF({format_condition(step.condition)}, {step.then}, {str(step.else_value)})""",
+        f"""IFF({format_condition(step.condition)}, {step.then}, {step.else_value})""",
     )
 
 
@@ -108,11 +129,15 @@ def translate_ifthenelse(
     new_query = SQLQuery(
         query_name=query_name,
         transformed_query=f"""{query.transformed_query}, {query_name} AS"""
-        f""" (SELECT ({recurse_format_if_then_else(step)}) AS {step.new_column}"""
+        f""" (SELECT {complete_fields(query)} {recurse_format_if_then_else(step)} AS {step.new_column}"""
         f""" FROM {query.query_name}) """,
-        selection_query=build_selection_query(query.metadata_manager.tables_metadata, query_name),
-        metadata_manager=query.metadata_manager,
     )
+
+    for table in [*query.metadata_manager.tables_metadata]:
+        query.metadata_manager.add_column(table, step.new_column, "str")
+
+    new_query.selection_query = build_selection_query(query.metadata_manager.tables_metadata, query_name)
+    new_query.metadata_manager = query.metadata_manager
 
     log.debug(
         "------------------------------------------------------------"
