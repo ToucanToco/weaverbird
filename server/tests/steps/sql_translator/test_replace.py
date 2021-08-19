cdb09b2ce7c29d92a36c2a1b94becf2aab33c019
sanix-darker <s4nixd@gmail.com>
2021-08-19 10:26:24 +0200

    feat(vqb): updates from reviews's comments

--- server/tests/steps/sql_translator/test_replace.py
+++ server/tests/steps/sql_translator/test_replace.py
@@ -29,7 +29,7 @@ def test_translate_string_integer_replace(query):
     )
     expected_transformed_query = (
         'WITH SELECT_STEP_0 AS (SELECT * FROM products), REPLACE_STEP_1 AS (SELECT TOTO, FLORIZARRE, CASE WHEN '
-        'RAICHU=2 THEN 4 END AS RAICHU FROM SELECT_STEP_0) '
+        'RAICHU=2 THEN 4 ELSE RAICHU END AS RAICHU FROM SELECT_STEP_0) '
     )
     assert query.transformed_query == expected_transformed_query
     assert query.selection_query == 'SELECT toto, raichu, florizarre FROM REPLACE_STEP_1'
