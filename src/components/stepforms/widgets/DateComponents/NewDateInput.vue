167b5018ae28c0620f3c11dbce9c101532c8d98e
alice.sevin <alice.sevin@toucantoco.com>
2021-08-19 12:06:51 +0200

    feat(new-date-input): stylize input container

--- src/components/stepforms/widgets/DateComponents/NewDateInput.vue
+++ src/components/stepforms/widgets/DateComponents/NewDateInput.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="widget-date-input" />
+  <div class="widget-date-input">
+    <div class="widget-date-input__container">
+      <span class="widget-date-input__label">Last 12 month</span>
+      <div class="widget-date-input__button">
+        <i class="far fa-calendar" aria-hidden="true" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -14,4 +21,42 @@ import { Component, Vue } from 'vue-property-decorator';
 export default class NewDateInput extends Vue {}
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import '../../../../styles/variables';
+
+$grey: #808080;
+$grey-light: #d9d9d9;
+$grey-extra-light: #f6f6f6;
+
+.widget-date-input__container {
+  border: 1px solid $grey-light;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 400px;
+}
+.widget-date-input__label {
+  padding: 10px 15px;
+  font-size: 14px;
+  font-weight: 500;
+  font-family: 'Montserrat', sans-serif;
+  max-width: 100%;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+.widget-date-input__button {
+  padding: 10px 15px;
+  background: $grey-extra-light;
+  color: $grey;
+  cursor: pointer;
+}
+
+.widget-date-input__container:hover {
+  border-color: $active-color;
+  .widget-date-input__button {
+    background-color: $active-color-faded-2;
+    color: $active-color;
+  }
+}
+</style>
