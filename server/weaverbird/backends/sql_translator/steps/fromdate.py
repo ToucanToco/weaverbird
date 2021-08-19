8d2f3dd1040b8d41ad21efa576558eae57ba40b2
sanix-darker <s4nixd@gmail.com>
2021-08-19 10:11:57 +0200

    feat(vqb): adding fromdate step

---
+++ server/weaverbird/backends/sql_translator/steps/fromdate.py
@@ -0,0 +1,54 @@
+from distutils import log
+
+from weaverbird.backends.sql_translator.steps.utils.query_transformation import (
+    build_selection_query,
+    complete_fields,
+)
+from weaverbird.backends.sql_translator.types import (
+    SQLPipelineTranslator,
+    SQLQuery,
+    SQLQueryDescriber,
+    SQLQueryRetriever,
+)
+from weaverbird.pipeline.steps import FromdateStep
+
+
+def translate_fromdate(
+    step: FromdateStep,
+    query: SQLQuery,
+    index: int,
+    sql_query_retriever: SQLQueryRetriever = None,
+    sql_query_describer: SQLQueryDescriber = None,
+    sql_translate_pipeline: SQLPipelineTranslator = None,
+) -> SQLQuery:
+    query_name = f'FROMDATE_STEP_{index}'
+
+    log.debug(
+        "############################################################"
+        f"query_name: {query_name}\n"
+        "------------------------------------------------------------"
+        f"step.name: {step.name}\n"
+        f"step.column: {step.column}\n"
+        f"step.format: {step.format}\n"
+        f"query.transformed_query: {query.transformed_query}\n"
+        f"query.metadata_manager.tables_metadata: {query.metadata_manager.tables_metadata}\n"
+        f"query.metadata_manager.query_metadata: {query.metadata_manager.query_metadata}\n"
+    )
+
+    step.format = "" if step.format is None else f", '{step.format}'"
+    new_query = SQLQuery(
+        query_name=query_name,
+        transformed_query=f"""{query.transformed_query}, {query_name} AS"""
+        f""" (SELECT {complete_fields(columns=[step.column], query=query)},"""
+        f""" TO_VARCHAR({step.column.upper()}{step.format}) AS {step.column.upper()})""",
+        selection_query=build_selection_query(query.metadata_manager.query_metadata, query_name),
+        metadata_manager=query.metadata_manager,
+    )
+
+    log.debug(
+        "------------------------------------------------------------"
+        f"SQLquery: {new_query.transformed_query}"
+        "############################################################"
+    )
+
+    return new_query
