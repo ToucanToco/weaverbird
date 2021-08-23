3a444b400fe2e4f970fd46351f19eeab37356be0
sanix-darker <s4nixd@gmail.com>
2021-08-23 14:36:22 +0200

    fix(vqb): update tests

--- server/tests/steps/sql_translator/test_top.py
+++ server/tests/steps/sql_translator/test_top.py
@@ -4,6 +4,23 @@ from weaverbird.backends.sql_translator.steps import translate_top
 from weaverbird.pipeline.steps import TopStep
 
 
+def test_translate_top_empty(query):
+    step = TopStep(name='top', groups=[], rank_on='RAICHU', sort='asc', limit=3)
+
+    query = translate_top(
+        step,
+        query,
+        index=1,
+    )
+    expected_transformed_query = (
+        'WITH SELECT_STEP_0 AS (SELECT * FROM products), TOPN_STEP_1 AS (SELECT TOP 3 '
+        'RAICHU FROM SELECT_STEP_0 GROUP BY RAICHU ORDER BY RAICHU asc) '
+    )
+    assert query.transformed_query == expected_transformed_query
+    assert query.selection_query == 'SELECT RAICHU FROM TOPN_STEP_1'
+    assert query.query_name == 'TOPN_STEP_1'
+
+
 def test_translate_top(query):
     step = TopStep(name='top', groups=['TOTO', 'FLORIZARRE'], rank_on='RAICHU', sort='asc', limit=3)
 
