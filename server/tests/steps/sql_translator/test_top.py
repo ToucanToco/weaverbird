168e46a64946fc1b9bf048a5298231921822ceca
sanix-darker <s4nixd@gmail.com>
2021-08-23 17:10:43 +0200

    fix(vqb): fix tests

--- server/tests/steps/sql_translator/test_top.py
+++ server/tests/steps/sql_translator/test_top.py
@@ -30,8 +30,8 @@ def test_translate_top(query):
         index=1,
     )
     expected_transformed_query = (
-        'WITH SELECT_STEP_0 AS (SELECT * FROM products), TOPN_STEP_1 AS (SELECT TOTO, FLORIZARRE, TOP 3 RAICHU FROM '
-        'SELECT_STEP_0 GROUP BY TOTO, FLORIZARRE ORDER BY RAICHU asc) '
+        'WITH SELECT_STEP_0 AS (SELECT * FROM products), TOPN_STEP_1 AS (SELECT TOP 3 RAICHU, TOTO, FLORIZARRE FROM '
+        'SELECT_STEP_0 GROUP BY TOTO, FLORIZARRE, RAICHU ORDER BY RAICHU asc) '
     )
     assert query.transformed_query == expected_transformed_query
     assert query.selection_query == 'SELECT TOTO, RAICHU, FLORIZARRE FROM TOPN_STEP_1'
