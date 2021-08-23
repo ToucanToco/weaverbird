07b9a39ebeaa275221981a7d3eafe3495901b88a
sanix-darker <s4nixd@gmail.com>
2021-08-23 14:15:39 +0200

    feat(vqb): fix lint

--- server/tests/steps/sql_translator/test_uniquegroups.py
+++ server/tests/steps/sql_translator/test_uniquegroups.py
@@ -12,7 +12,10 @@ def test_translate_uniquegroups_empty(query):
         query,
         index=1,
     )
-    expected_transformed_query = 'WITH SELECT_STEP_0 AS (SELECT * FROM products), UNIQUEGROUPS_STEP_1 AS (SELECT SELECT_STEP_0 ) '
+    expected_transformed_query = (
+        'WITH SELECT_STEP_0 AS (SELECT * FROM products), UNIQUEGROUPS_STEP_1 AS (SELECT '
+        'SELECT_STEP_0 ) '
+    )
     assert query.transformed_query == expected_transformed_query
     assert query.selection_query == 'SELECT UNIQUEGROUPS_STEP_1'
     assert query.query_name == 'UNIQUEGROUPS_STEP_1'
