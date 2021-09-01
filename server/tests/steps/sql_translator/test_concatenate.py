840d427f838b6374ef798be16ac01d6f98442347
sanix-darker <s4nixd@gmail.com>
2021-09-01 15:28:35 +0200

    feat(vqb): updated tests

--- server/tests/steps/sql_translator/test_concatenate.py
+++ server/tests/steps/sql_translator/test_concatenate.py
@@ -1,3 +1,4 @@
+from weaverbird.backends.sql_translator.metadata import ColumnMetadata
 from weaverbird.backends.sql_translator.steps import translate_concatenate
 from weaverbird.pipeline.steps import ConcatenateStep
 
@@ -14,10 +15,46 @@ def test_translate_simple_concatenante(query):
     )
     expected_transformed_query = (
         "WITH SELECT_STEP_0 AS (SELECT * FROM products), CONCATENATE_STEP_1 AS (SELECT TOTO, RAICHU, FLORIZARRE, "
-        "TO_AGE, CONCAT_WS(',', TOTO, AGE) AS TO_AGE FROM SELECT_STEP_0)"
+        "CONCAT_WS(',', TOTO, AGE) AS TO_AGE FROM SELECT_STEP_0)"
     )
     assert query.transformed_query == expected_transformed_query
     assert (
         query.selection_query == 'SELECT TOTO, RAICHU, FLORIZARRE, TO_AGE FROM CONCATENATE_STEP_1'
     )
     assert query.query_name == 'CONCATENATE_STEP_1'
+
+    # assert on metadatas
+    assert query.metadata_manager.retrieve_query_metadata_columns() == {
+        'FLORIZARRE': ColumnMetadata(
+            name='FLORIZARRE',
+            original_name='FLORIZARRE',
+            type='STR',
+            original_type='str',
+            alias=None,
+            delete=False,
+        ),
+        'RAICHU': ColumnMetadata(
+            name='RAICHU',
+            original_name='RAICHU',
+            type='INT',
+            original_type='int',
+            alias=None,
+            delete=False,
+        ),
+        'TOTO': ColumnMetadata(
+            name='TOTO',
+            original_name='TOTO',
+            type='STR',
+            original_type='str',
+            alias=None,
+            delete=False,
+        ),
+        'TO_AGE': ColumnMetadata(
+            name='TO_AGE',
+            original_name='TO_AGE',
+            type='STRING',
+            original_type='string',
+            alias=None,
+            delete=False,
+        ),
+    }
