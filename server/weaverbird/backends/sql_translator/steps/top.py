e46d8d0f5a14bbb8a7ac5899335ca38a026fd079
sanix-darker <s4nixd@gmail.com>
2021-09-02 17:47:48 +0200

    fix(vqb): fix tests top-n-rows

--- server/weaverbird/backends/sql_translator/steps/top.py
+++ server/weaverbird/backends/sql_translator/steps/top.py
@@ -62,7 +62,7 @@ def translate_top(
         query_name=query_name,
         transformed_query=f"""{query.transformed_query}, {query_name} AS"""
         f""" (SELECT {completed_fields}"""
-        f""" FROM {query.query_name} {group_by_query} ORDER BY {step.rank_on} {step.sort} LIMIT {step.limit}) """,
+        f""" FROM {query.query_name} {group_by_query} ORDER BY {step.rank_on} {step.sort} LIMIT {step.limit})""",
         selection_query=build_selection_query(
             query.metadata_manager.retrieve_query_metadata_columns(), query_name
         ),
