d8f3058e9d45bad7e86ced31daccdba579740a40
raphael.vignes@toucantoco.com <raphael.vignes@toucantoco.com>
2021-09-20 17:47:01 +0200

    test(sql evolution): add integration tests

--- server/tests/steps/sql_translator/test_evolution.py
+++ server/tests/steps/sql_translator/test_evolution.py
@@ -38,7 +38,7 @@ def test_translate_evolution(mocker, query):
         "WITH SELECT_STEP_0 AS (SELECT * FROM products), EVOLUTION_STEP_1 AS "
         "(SELECT A.TOTO AS TOTO, A.RAICHU AS RAICHU, A.FLORIZARRE AS FLORIZARRE, A.DATE AS DATE, "
         "B.RAICHU - A.RAICHU AS RAICHU_EVOL_ABS FROM SELECT_STEP_0 A LEFT JOIN SELECT_STEP_0 B "
-        "ON A.DATE = DATEADD('month', 1, DATE) ORDER BY A.DATE"
+        "ON A.DATE = DATEADD('month', 1, A.DATE) ORDER BY A.DATE)"
     )
     assert query.transformed_query == expected_transformed_query
     assert (
