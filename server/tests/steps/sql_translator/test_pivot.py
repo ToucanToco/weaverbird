7619bf6f99a1b818e76a519b71cbb06a24e60f74
raphael.vignes@toucantoco.com <raphael.vignes@toucantoco.com>
2021-09-28 11:51:02 +0200

    fix(sql pivot): fix pivot unit tests

--- server/tests/steps/sql_translator/test_pivot.py
+++ server/tests/steps/sql_translator/test_pivot.py
@@ -41,11 +41,11 @@ def test_translate_pivot(sql_query_executor):
         agg_function='sum',
     )
     res = translate_pivot(step, query, index=1, sql_query_executor=sql_query_executor)
-    assert (
-        res.transformed_query
-        == """WITH SELECT_STEP_0 AS (SELECT COMPANY, COUNTRY, CURRENCY, PROVIDER FROM PRODUCTS),\
- PIVOT_STEP_1 AS (SELECT COMPANY, COUNTRY, "'SPAIN'" AS SPAIN, "'FRANCE'" AS FRANCE\
- FROM SELECT_STEP_0 PIVOT(sum(PROVIDER) FOR CURRENCY IN ('SPAIN', 'FRANCE')))"""
+    assert res.transformed_query == (
+        """WITH SELECT_STEP_0 AS (SELECT COMPANY, COUNTRY, CURRENCY, PROVIDER FROM PRODUCTS), """
+        """PRE_PIVOT_STEP_1 AS (SELECT COMPANY, COUNTRY, CURRENCY, PROVIDER FROM SELECT_STEP_0), """
+        """PIVOT_STEP_1 AS (SELECT COMPANY, COUNTRY, "'SPAIN'" AS SPAIN, "'FRANCE'" AS FRANCE """
+        """FROM PRE_PIVOT_STEP_1 PIVOT(sum(PROVIDER) FOR CURRENCY IN ('SPAIN', 'FRANCE')))"""
     )
     assert res.selection_query == 'SELECT COMPANY, COUNTRY, SPAIN, FRANCE FROM PIVOT_STEP_1'
     assert res.metadata_manager.retrieve_query_metadata_columns() == {
