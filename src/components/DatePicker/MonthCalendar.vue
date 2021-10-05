a938b87a0abc036ed9b681d53adf3944e40a8066
austil <augustin@mrlacour.fr>
2021-10-05 12:54:21 +0200

    feat(month calendar): header content
    with most of the styling
    
    no interaction nor conditional display
    => not testing anything (yet)

--- src/components/DatePicker/MonthCalendar.vue
+++ src/components/DatePicker/MonthCalendar.vue
@@ -1,14 +1,62 @@
 <template>
-  <div>Month Calendar</div>
+  <div class="month-calendar" data-cy="weaverbird-month-calendar">
+    <div class="month-calendar__header">
+      <div class="month-calendar__header-btn" data-cy="weaverbird-month-calendar__previous">
+        <FAIcon icon="chevron-left" />
+      </div>
+      {{ headerLabel }}
+      <div class="month-calendar__header-btn" data-cy="weaverbird-month-calendar__next">
+        <FAIcon icon="chevron-right" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
+import { DateTime } from 'luxon';
 import { Component, Vue } from 'vue-property-decorator';
+
+import FAIcon from '@/components/FAIcon.vue';
 
 @Component({
   name: 'month-calendar',
+  components: {
+    FAIcon,
+  },
 })
-export default class MonthCalendar extends Vue {}
+export default class MonthCalendar extends Vue {
+  get headerLabel(): string {
+    return DateTime.now().year;
+  }
+}
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.month-calendar {
+  box-sizing: border-box;
+  width: 330px;
+}
+
+.month-calendar__header {
+  display: flex;
+  justify-content: space-between;
+  height: 40px;
+  align-items: center;
+}
+
+.month-calendar__header-btn {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 8px;
+  width: 40px;
+  height: 40px;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background: #f8f7fa;
+  }
+}
+</style>
