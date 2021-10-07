48610cb2e7209afe9186d1daf4d1c7c7d6bcc1d7
austil <augustin@mrlacour.fr>
2021-10-07 10:49:46 +0200

    refactor: move month picker test into dedicated describe

--- tests/unit/custom-granularity-calendar.spec.ts
+++ tests/unit/custom-granularity-calendar.spec.ts
@@ -6,7 +6,7 @@ import { DateRange } from '@/lib/dates';
 
 const currentYear = DateTime.now().year;
 
-describe('CustomGranularityCalendar - Month', () => {
+describe('CustomGranularityCalendar', () => {
   let wrapper: Wrapper<CustomGranularityCalendar>;
   const createWrapper = (props: any = {}): void => {
     wrapper = shallowMount(CustomGranularityCalendar, {
@@ -22,79 +22,83 @@ describe('CustomGranularityCalendar - Month', () => {
     if (wrapper) wrapper.destroy();
   });
 
-  describe('default', () => {
-    beforeEach(() => {
-      createWrapper();
-    });
-
-    it('should instantiate', () => {
-      expect(wrapper.exists()).toBe(true);
-    });
-
-    it('should display all 12 months', () => {
-      const months = wrapper.findAll('.custom-granularity-calendar__option');
-      expect(months).toHaveLength(12);
-      expect(months.at(0).text()).toStrictEqual('January');
-      expect(months.at(11).text()).toStrictEqual('December');
-    });
-
-    describe('when clicking on a a month', () => {
-      beforeEach(async () => {
-        const february = wrapper.findAll('.custom-granularity-calendar__option').at(1);
-        await february.trigger('click');
+  describe('Month', () => {
+    describe('default', () => {
+      beforeEach(() => {
+        createWrapper();
       });
 
-      it('should emit the selected date range', () => {
-        expect(wrapper.emitted('input')).toHaveLength(1);
-        const emittedDate: DateRange = wrapper.emitted('input')[0][0];
-        expect(emittedDate.start).toBeInstanceOf(Date);
-        expect(emittedDate.end).toBeInstanceOf(Date);
-        expect(emittedDate.start?.toISOString()).toStrictEqual(
-          `${currentYear}-02-01T00:00:00.000Z`,
+      it('should instantiate', () => {
+        expect(wrapper.exists()).toBe(true);
+      });
+
+      it('should display all 12 months', () => {
+        const months = wrapper.findAll('.custom-granularity-calendar__option');
+        expect(months).toHaveLength(12);
+        expect(months.at(0).text()).toStrictEqual('January');
+        expect(months.at(11).text()).toStrictEqual('December');
+      });
+
+      describe('when clicking on a a month', () => {
+        beforeEach(async () => {
+          const february = wrapper.findAll('.custom-granularity-calendar__option').at(1);
+          await february.trigger('click');
+        });
+
+        it('should emit the selected date range', () => {
+          expect(wrapper.emitted('input')).toHaveLength(1);
+          const emittedDate: DateRange = wrapper.emitted('input')[0][0];
+          expect(emittedDate.start).toBeInstanceOf(Date);
+          expect(emittedDate.end).toBeInstanceOf(Date);
+          expect(emittedDate.start?.toISOString()).toStrictEqual(
+            `${currentYear}-02-01T00:00:00.000Z`,
+          );
+          expect(emittedDate.end?.toISOString()).toStrictEqual(
+            `${currentYear}-03-01T00:00:00.000Z`,
+          );
+          expect(emittedDate.duration).toBe('month');
+        });
+      });
+    });
+
+    describe('with selected date range', () => {
+      beforeEach(() => {
+        createWrapper({
+          value: {
+            start: new Date('12/21/1977'),
+            // only based on start date (for now)
+          },
+        });
+      });
+
+      it('should set header on corresponding year', () => {
+        expect(wrapper.find('.custom-granularity-calendar__header').text()).toBe('1977');
+      });
+
+      it('should select the corresponding month', () => {
+        expect(wrapper.find('.custom-granularity-calendar__option--selected').text()).toBe(
+          'December',
         );
-        expect(emittedDate.end?.toISOString()).toStrictEqual(`${currentYear}-03-01T00:00:00.000Z`);
-        expect(emittedDate.duration).toBe('month');
-      });
-    });
-  });
-
-  describe('with selected date range', () => {
-    beforeEach(() => {
-      createWrapper({
-        value: {
-          start: new Date('12/21/1977'),
-          // only based on start date (for now)
-        },
-      });
-    });
-
-    it('should set header on corresponding year', () => {
-      expect(wrapper.find('.custom-granularity-calendar__header').text()).toBe('1977');
-    });
-
-    it('should select the corresponding month', () => {
-      expect(wrapper.find('.custom-granularity-calendar__option--selected').text()).toBe(
-        'December',
-      );
-    });
-
-    describe('when clicking on the previous year button', () => {
-      beforeEach(async () => {
-        await wrapper.find('.header-btn__previous').trigger('click');
       });
 
-      it('should show the previous year', () => {
-        expect(wrapper.find('.custom-granularity-calendar__header').text()).toBe('1976');
-      });
-    });
+      describe('when clicking on the previous year button', () => {
+        beforeEach(async () => {
+          await wrapper.find('.header-btn__previous').trigger('click');
+        });
 
-    describe('when clicking on the next year button', () => {
-      beforeEach(async () => {
-        await wrapper.find('.header-btn__next').trigger('click');
+        it('should show the previous year', () => {
+          expect(wrapper.find('.custom-granularity-calendar__header').text()).toBe('1976');
+        });
       });
 
-      it('should show the previous year', () => {
-        expect(wrapper.find('.custom-granularity-calendar__header').text()).toBe('1978');
+      describe('when clicking on the next year button', () => {
+        beforeEach(async () => {
+          await wrapper.find('.header-btn__next').trigger('click');
+        });
+
+        it('should show the previous year', () => {
+          expect(wrapper.find('.custom-granularity-calendar__header').text()).toBe('1978');
+        });
       });
     });
   });
