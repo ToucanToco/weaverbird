c27d5a02c885873fa1a685bd61ee56d638403685
David Nowinsky <david@nowinsky.net>
2021-10-30 12:41:00 +0200

    feat(playground): very basic support for mongo in the python server

--- server/playground.py
+++ server/playground.py
@@ -17,6 +17,7 @@ from os.path import basename, splitext
 
 import pandas as pd
 from flask import Flask, Response, jsonify, request, send_from_directory
+from pymongo import MongoClient
 
 from weaverbird.backends.pandas_executor.pipeline_executor import (
     preview_pipeline as pandas_preview_pipeline,
@@ -31,26 +32,7 @@ def handle_health_request():
     return 'OK'
 
 
-@app.route('/pandas', methods=['GET', 'POST'])
-def handle_pandas_backend_request():
-    if request.method == 'GET':
-        return jsonify(get_available_domains())
-    elif request.method == 'POST':
-        try:
-            return Response(
-                execute_pipeline(request.get_json(), **request.args), mimetype='application/json'
-            )
-        except Exception as e:
-            errmsg = f'{e.__class__.__name__}: {e}'
-            return jsonify(errmsg), 400
-
-
-@app.route('/', methods=['GET'])
-@app.route('/<path:filename>', methods=['GET'])
-def handle_static_files_request(filename=None):
-    filename = filename or 'index.html'
-    return send_from_directory('static', filename)
-
+### Pandas back-end routes
 
 # Load all csv in playground's pandas datastore
 csv_files = glob('../playground/datastore/*.csv')
@@ -76,3 +58,56 @@ def execute_pipeline(pipeline_steps, **kwargs) -> str:
         domain_retriever=lambda domain: DOMAINS[domain],
         **kwargs,
     )
+
+
+@app.route('/pandas', methods=['GET', 'POST'])
+def handle_pandas_backend_request():
+    if request.method == 'GET':
+        return jsonify(get_available_domains())
+    elif request.method == 'POST':
+        try:
+            return Response(
+                execute_pipeline(request.get_json(), **request.args), mimetype='application/json'
+            )
+        except Exception as e:
+            errmsg = f'{e.__class__.__name__}: {e}'
+            return jsonify(errmsg), 400
+
+
+### Mongo back-end routes
+mongo_client = MongoClient()
+mongo_db = mongo_client['test']
+
+
+@app.route('/mongo', methods=['GET', 'POST'])
+def handle_mongo_backend_request():
+    if request.method == 'GET':
+        return jsonify(mongo_db.list_collection_names())
+    elif request.method == 'POST':
+        try:
+            req_params = request.get_json()
+
+            query = req_params['query']
+            collection = req_params['collection']
+            limit = req_params['limit']
+            skip = req_params['skip']
+
+            # TODO: add limit/offset pipeline stages to query
+
+            results = list(mongo_db[collection].aggregate(query))
+            # ObjectID are not JSON serializable, so remove them
+            for row in results:
+                del row['_id']
+            return jsonify(list(results))
+        except Exception as e:
+            errmsg = f'{e.__class__.__name__}: {e}'
+            return jsonify(errmsg), 400
+
+
+### UI files
+
+@app.route('/', methods=['GET'])
+@app.route('/<path:filename>', methods=['GET'])
+def handle_static_files_request(filename=None):
+    filename = filename or 'index.html'
+    return send_from_directory('static', filename)
