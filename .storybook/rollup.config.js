4781b9fed27b1e882d6b8e868d39363e9a9fd8f0
Jeremy Pinhel <jeremy.pinhel@icloud.com>
2021-11-02 18:17:46 +0100

    chore(build): avoid to have extract option enable of postCss config with storybook

--- .storybook/rollup.config.js
+++ .storybook/rollup.config.js
@@ -1,10 +1,31 @@
+import autoprefixer from "autoprefixer";
+import postcss from 'rollup-plugin-postcss'
+import postcssPresetEnv from 'postcss-preset-env';
 import mainConfig from '../rollup.config'
 
+const { plugins, ...partialMainConfig } = mainConfig
+
+const storyBookPlugins = [
+  ...plugins.filter((plugin) => plugin.name !== 'postcss'), // Avoid extra config in mainConfig
+  postcss({
+    plugins: [
+      postcssPresetEnv({
+        stage: 2,
+        features: {
+          'nesting-rules': true,
+        },
+      }),
+      autoprefixer()
+    ],
+  }),
+];
+
 export default {
-  ...mainConfig,
+  ...partialMainConfig,
   input: '.storybook/bundle.ts',
   output: [{
     file: 'dist/storybook/components.js',
     format: 'esm'
   }],
+  plugins: storyBookPlugins
 }
