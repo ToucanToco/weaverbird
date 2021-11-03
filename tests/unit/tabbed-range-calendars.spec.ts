3ed7d461dc6c2491aad438443be06c987f7db386
austil <augustin@mrlacour.fr>
2021-11-03 11:40:49 +0100

    refactor: space those describe yo

--- tests/unit/tabbed-range-calendars.spec.ts
+++ tests/unit/tabbed-range-calendars.spec.ts
@@ -77,6 +77,7 @@ describe('TabbedRangeCalendars', () => {
       expect(wrapper.find('CustomGranularityCalendar-stub').props('granularity')).toBe('year');
     });
   });
+
   describe('with selected value', () => {
     describe('with known granularity', () => {
       beforeEach(async () => {
@@ -112,6 +113,7 @@ describe('TabbedRangeCalendars', () => {
       });
     });
   });
+
   describe('with updated enabled calendars', () => {
     beforeEach(async () => {
       await wrapper.setProps({
