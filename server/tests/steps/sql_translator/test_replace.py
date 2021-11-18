505466255fe0df26233fcda43d5bf10260ce250c
sanix-darker <s4nixd@gmail.com>
2021-11-18 11:25:15 +0100

    fix(youprep-replace): replace step work w & w/ quotes

--- server/tests/steps/sql_translator/test_replace.py
+++ server/tests/steps/sql_translator/test_replace.py
@@ -29,7 +29,7 @@ def test_translate_string_integer_replace(query):
     )
     expected_transformed_query = (
         'WITH SELECT_STEP_0 AS (SELECT * FROM products), REPLACE_STEP_1 AS (SELECT TOTO, FLORIZARRE, CASE WHEN '
-        'RAICHU=2 THEN 4 ELSE RAICHU END AS RAICHU FROM SELECT_STEP_0)'
+        'RAICHU=\'2\' THEN \'4\' ELSE RAICHU END AS RAICHU FROM SELECT_STEP_0)'
     )
     assert query.transformed_query == expected_transformed_query
     assert query.selection_query == 'SELECT TOTO, RAICHU, FLORIZARRE FROM REPLACE_STEP_1'
@@ -48,7 +48,29 @@ def test_translate_multiple_replace(query):
     )
     expected_transformed_query = (
         'WITH SELECT_STEP_0 AS (SELECT * FROM products), REPLACE_STEP_1 AS (SELECT TOTO, FLORIZARRE, CASE WHEN '
-        'RAICHU=2 THEN 4 WHEN RAICHU=456.765 THEN 221.3 ELSE RAICHU END AS RAICHU FROM SELECT_STEP_0)'
+        'RAICHU=\'2\' THEN \'4\' WHEN RAICHU=\'456.765\' THEN \'221.3\' ELSE RAICHU END AS RAICHU FROM SELECT_STEP_0)'
+    )
+    assert query.transformed_query == expected_transformed_query
+    assert query.selection_query == 'SELECT TOTO, RAICHU, FLORIZARRE FROM REPLACE_STEP_1'
+    assert query.query_name == 'REPLACE_STEP_1'
+
+
+def test_translate_with_quotes_and_without_replace(query):
+    step = ReplaceStep(
+        name='replace',
+        search_column='RAICHU',
+        to_replace=[["'TEST'", "'OK DOCK"], ["'L'EXEMPLE", 'GOGO"GADJET\'']],
+    )
+
+    query = translate_replace(
+        step,
+        query,
+        index=1,
+    )
+    expected_transformed_query = (
+        'WITH SELECT_STEP_0 AS (SELECT * FROM products), REPLACE_STEP_1 AS (SELECT TOTO, FLORIZARRE, CASE WHEN '
+        'RAICHU=\'TEST\' THEN \'OK DOCK\' WHEN RAICHU=\'L\\\'EXEMPLE\' THEN \'GOGO\\\'GADJET\' ELSE RAICHU END AS '
+        'RAICHU FROM SELECT_STEP_0)'
     )
     assert query.transformed_query == expected_transformed_query
     assert query.selection_query == 'SELECT TOTO, RAICHU, FLORIZARRE FROM REPLACE_STEP_1'
