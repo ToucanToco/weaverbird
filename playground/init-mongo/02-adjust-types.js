cc382fc16345e5b39625a99f54b9f019e050329c
David Nowinsky <david@nowinsky.net>
2021-11-29 15:34:59 +0100

    feat(playground): add dataset with hours

--- playground/init-mongo/02-adjust-types.js
+++ playground/init-mongo/02-adjust-types.js
@@ -13,13 +13,25 @@ db.sales.find().forEach(function(d) {
   })
 });
 
-const tableSchema = db['sin-from-2019-to-2022'].find()[0];
-db['sin-from-2019-to-2022'].insert(tableSchema.data);
-db['sin-from-2019-to-2022'].remove({_id: tableSchema._id});
+const sin20192022TableSchema = db['sin-from-2019-to-2022'].find()[0];
+db['sin-from-2019-to-2022'].insert(sin20192022TableSchema.data);
+db['sin-from-2019-to-2022'].remove({_id: sin20192022TableSchema._id});
 db['sin-from-2019-to-2022'].find().forEach(function(d) {
   db['sin-from-2019-to-2022'].update({_id: d._id}, {
     $set: {
       day: new Date(d.day),
+    }
+  })
+});
+
+
+const november2021HoursTableSchema = db['november-2021-hours'].find()[0];
+db['november-2021-hours'].insert(november2021HoursTableSchema.data);
+db['november-2021-hours'].remove({_id: november2021HoursTableSchema._id});
+db['november-2021-hours'].find().forEach(function(d) {
+  db['november-2021-hours'].update({_id: d._id}, {
+    $set: {
+      date: new Date(d.date),
     }
   })
 });
