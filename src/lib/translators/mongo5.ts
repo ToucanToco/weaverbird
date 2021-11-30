7d657cfcb9f7e4f19d924252d9328016589bfebc
alice.sevin <alice.sevin@toucantoco.com>
2021-11-30 12:51:33 +0100

    feat(mongo): use date variable rather than $$NOW for relative dates

--- src/lib/translators/mongo5.ts
+++ src/lib/translators/mongo5.ts
@@ -1,4 +1,4 @@
-import { RelativeDate } from '@/lib/dates';
+import { RELATIVE_DATE_OPERATORS, RelativeDate } from '@/lib/dates';
 
 import { Mongo42Translator } from './mongo42';
 
@@ -6,16 +6,18 @@ export class Mongo50Translator extends Mongo42Translator {
   static label = 'Mongo 5.0';
 
   protected translateRelativeDate(value: RelativeDate): object {
+    const operator = RELATIVE_DATE_OPERATORS[value.operator];
     return {
       $dateAdd: {
         startDate: {
           // Base date does not include any hour information
           $dateTrunc: {
-            date: '$$NOW', // TODO: adapt date with selected variable
+            date: value.date,
             unit: 'day',
           },
         },
-        amount: value.quantity,
+        // if relative date has a "before" operator we expect to have a negative quantity
+        amount: Math.sign(operator.sign) * Math.abs(value.quantity),
         unit: value.duration,
       },
     };
