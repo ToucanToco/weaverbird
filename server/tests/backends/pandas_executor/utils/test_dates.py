4549edd5efc652f4c6175f8211c3e6c7c7c867e6
David Nowinsky <david@nowinsky.net>
2021-12-02 11:35:54 +0100

    style(pandas): code formatting with black

--- server/tests/backends/pandas_executor/utils/test_dates.py
+++ server/tests/backends/pandas_executor/utils/test_dates.py
@@ -5,21 +5,20 @@ from weaverbird.pipeline.dates import RelativeDate
 
 
 def test_evaluate_relative_date():
-    assert evaluate_relative_date(
-        RelativeDate(
-            date=datetime(year=2020, month=8, day=1),
-            operator='until',
-            quantity=3,
-            duration='month'
+    assert (
+        evaluate_relative_date(
+            RelativeDate(
+                date=datetime(year=2020, month=8, day=1),
+                operator='until',
+                quantity=3,
+                duration='month',
+            )
         )
-    ) == datetime(year=2020, month=5, day=1)
+        == datetime(year=2020, month=5, day=1)
+    )
 
     assert evaluate_relative_date(
         RelativeDate(
-            date=datetime(year=2020, month=8, day=1),
-            operator='from',
-            quantity=3,
-            duration='day'
+            date=datetime(year=2020, month=8, day=1), operator='from', quantity=3, duration='day'
         )
     ) == datetime(year=2020, month=8, day=4)
-
