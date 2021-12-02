8af45dbc432db54d464680e6adba830ae21ff1a6
David Nowinsky <david@nowinsky.net>
2021-12-02 11:22:34 +0100

    feat(pandas): support RelativeDates for filtering

---
+++ server/src/weaverbird/backends/pandas_executor/steps/utils/dates.py
@@ -0,0 +1,25 @@
+import operator
+from datetime import datetime
+
+from dateutil.relativedelta import relativedelta
+
+from weaverbird.pipeline.dates import RelativeDate
+
+
+def evaluate_relative_date(relative_date: RelativeDate) -> datetime:
+    """
+    From a RelativeDate definition, compute the actual corresponding datetime.
+    """
+    if relative_date.operator == 'until':
+        operation = operator.sub
+    elif relative_date.operator == 'from':
+        operation = operator.add
+    else:
+        raise NotImplementedError
+
+    return operation(
+        relative_date.date,
+        relativedelta(**{
+            relative_date.duration + 's': relative_date.quantity
+        })
+    )
