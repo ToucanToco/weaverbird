88f3568dab3048f6f20c86b9310a991a6a423939
alice.sevin <alice.sevin@toucantoco.com>
2021-12-14 12:26:16 +0100

    fix: lint

--- tests/unit/step.spec.ts
+++ tests/unit/step.spec.ts
@@ -1,4 +1,4 @@
-import { createLocalVue, createWrapper, mount, shallowMount, Wrapper } from '@vue/test-utils';
+import { createLocalVue, mount, shallowMount, Wrapper } from '@vue/test-utils';
 import Vuex from 'vuex';
 
 import PipelineComponent from '@/components/Pipeline.vue';
