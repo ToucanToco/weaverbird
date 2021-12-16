090eefac6d12f84fecafbe0f9c3712865b01b003
alice-sevin <59559689+alice-sevin@users.noreply.github.com>
2021-12-16 09:21:51 +0100

    feat(configurable-subset-preview): use correct design TCTC-1339 (#1228)
    * feat(preview-source-subset): add css for specific style
    
    * feat(pipeline): remove preview source subset to move it to source step
    
    * chore(storybook): add test case for preview source subset in first step
    
    * feat(step): add preview source subset when clicking on expand button
    
    * fix: lint
    
    * feat(step): add preview subset condition to display expandable elements
    
    * refactor(step): use better naming

--- src/components/PreviewSourceSubset.vue
+++ src/components/PreviewSourceSubset.vue
@@ -1,11 +1,18 @@
 <template>
-  <div v-if="previewSourceRowsSubset" class="preview-source-rows-subset">
-    <p>Configurable "preview source" subset (beta)</p>
-    <div>
-      <input v-model.number="rowsSubsetInput" type="number" min="1" />
-      rows
-      <button @click="refreshPreview">Refresh</button>
+  <div class="preview-source-rows-subset">
+    <span class="preview-source-rows-subset__text">Preview source on</span>
+    <div class="preview-source-rows-subset__wrapper">
+      <input
+        class="preview-source-rows-subset__input"
+        v-model.number="rowsSubsetInput"
+        type="number"
+        min="1"
+      />
+      <span class="preview-source-rows-subset__button" @click="refreshPreview">
+        <FAIcon icon="sync-alt" />
+      </span>
     </div>
+    <span class="preview-source-rows-subset__text">rows subset</span>
   </div>
 </template>
 
@@ -13,11 +20,15 @@
 import Vue from 'vue';
 import { Component, Watch } from 'vue-property-decorator';
 
+import FAIcon from '@/components/FAIcon.vue';
 import { VQBModule } from '@/store';
 import { MutationCallbacks } from '@/store/mutations';
 
 @Component({
   name: 'PreviewSourceSubset',
+  components: {
+    FAIcon,
+  },
 })
 export default class PreviewSourceSubset extends Vue {
   rowsSubsetInput: number | string | undefined = this.lastSuccessfulPreviewRowsSubset;
@@ -55,13 +66,45 @@ export default class PreviewSourceSubset extends Vue {
 @import '../styles/_variables';
 
 .preview-source-rows-subset {
-  margin: 30px 0;
-  padding-left: 48px;
+  padding: 7px 12px;
   width: 100%;
-  border-top: 1px solid $grey-light;
+  background: $active-color;
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+}
+.preview-source-rows-subset__text {
+  color: white;
+  letter-spacing: 0.5px;
+  font-weight: 500;
+}
+.preview-source-rows-subset__wrapper {
+  width: 110px;
+  margin: 0 10px;
+  padding: 3px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-  input {
-    width: 90px;
+  background: white;
+  height: 100%;
+  border-radius: 2px;
+}
+.preview-source-rows-subset__input {
+  border: none;
+  width: 100%;
+  flex: 1;
+  height: 25px;
+  &:focus {
+    outline: none;
   }
+}
+.preview-source-rows-subset__button {
+  background: $active-color;
+  color: white;
+  padding: 7px;
+  font-size: 10px;
+  border-radius: 2px;
+  cursor: pointer;
 }
 </style>
