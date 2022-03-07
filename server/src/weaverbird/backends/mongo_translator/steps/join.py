78382adebbb14816e189d9625e78d71ef5e40a51
raphael.vignes@toucantoco.com <raphael.vignes@toucantoco.com>
2022-03-07 18:30:31 +0100

    test(mongo python join): add test fixtures

--- server/src/weaverbird/backends/mongo_translator/steps/join.py
+++ server/src/weaverbird/backends/mongo_translator/steps/join.py
@@ -1,51 +1,20 @@
 import logging
-from typing import Any, List, Union
+from typing import Any, List
 
 from weaverbird.backends.mongo_translator.utils import column_to_user_variable
-from weaverbird.exceptions import UnresolvedReferenceError
 from weaverbird.pipeline import Pipeline, steps
 from weaverbird.pipeline.steps import DomainStep, JoinStep
-from weaverbird.pipeline.steps.utils.combination import (
-    Reference,  # inconsistent with front translator, should be ReferenceToExternalQuery
-)
 
 logger = logging.getLogger(__name__)
 
 
-def is_reference_to_other_pipeline(pipeline_or_reference: Union[List, str, Reference]):
-    return isinstance(pipeline_or_reference, str)
-
-
-def is_reference_to_external_query(pipeline_or_reference: Union[List, str, Reference]):
-    assert isinstance(pipeline_or_reference, Reference)
-    return pipeline_or_reference.type == 'ref'
-
-
-def is_reference(pipeline_or_reference: Union[list[dict[Any, Any]], str, Reference]):
-    assert isinstance(pipeline_or_reference, Reference) or isinstance(pipeline_or_reference, str)
-    return is_reference_to_other_pipeline(pipeline_or_reference) or is_reference_to_external_query(
-        pipeline_or_reference
-    )
-
-
-def translate_join(step: JoinStep) -> list:
-    mongo_pipeline: list[Any] = []
+def translate_join(step: JoinStep) -> List:
+    mongo_pipeline: List[Any] = []
     right = step.right_pipeline
-
-    if is_reference(right):
-        logger.error(f'Unresolved reference: {right}')
-        raise UnresolvedReferenceError(
-            'References must be resolved before translating the pipeline'
-        )
     assert isinstance(right, list)
     assert isinstance(right[0], dict)
     right_domain: DomainStep = DomainStep(**right[0])
 
-    if is_reference_to_external_query(right_domain.domain):
-        logger.error(f'Unresolved reference: {right_domain.domain}')
-        raise UnresolvedReferenceError(
-            'References must be resolved before translating the pipeline'
-        )
     right_without_domain = Pipeline(
         steps=[getattr(steps, f"{s['name'].capitalize()}Step")(**s) for s in right[1:]]
     )
@@ -54,7 +23,7 @@ def translate_join(step: JoinStep) -> list:
 
     for left_on, right_on in step.on:
         mongo_let[column_to_user_variable(left_on)] = f'${left_on}'
-        mongo_expr_and.append({'$eq': [f'${right_on}', f'$(${column_to_user_variable(left_on)})']})
+        mongo_expr_and.append({'$eq': [f'${right_on}', f'$${column_to_user_variable(left_on)}']})
 
     from weaverbird.backends.mongo_translator.mongo_pipeline_translator import translate_pipeline
 
@@ -63,16 +32,15 @@ def translate_join(step: JoinStep) -> list:
             '$lookup': {
                 'from': right_domain.domain,
                 'let': mongo_let,
-                'pipeline': [
-                    translate_pipeline(right_without_domain),
-                    {'$match': {'$expr': {'$and': mongo_expr_and}}},
-                ],
+                'pipeline': translate_pipeline(right_without_domain)
+                + [{'$match': {'$expr': {'$and': mongo_expr_and}}}],
                 'as': '_vqbJoinKey',
             }
         }
     )
-
     if step.type == 'inner':
+        mongo_pipeline.append({'$unwind': '$_vqbJoinKey'})
+    elif step.type == 'left':
         mongo_pipeline.append(
             {'$unwind': {'path': '$_vqbJoinKey', 'preserveNullAndEmptyArrays': True}}
         )
--- server/tests/backends/mongo_translator/test_mongo_translator_steps.py
+++ server/tests/backends/mongo_translator/test_mongo_translator_steps.py
@@ -38,26 +38,34 @@ def mongo_server_port(mongo_version):
 
 
 @pytest.fixture()
-def mongo_collection(mongo_server_port):
-    collection_name = uuid.uuid4().hex
+def mongo_database(mongo_server_port):
     client = MongoClient('localhost', mongo_server_port)
-    return client['tests'][collection_name]
+    return client['tests']
 
 
 @pytest.mark.parametrize('case_id,case_spec_file_path', test_cases)
-def test_mongo_translator_pipeline(mongo_collection, case_id, case_spec_file_path):
+def test_mongo_translator_pipeline(mongo_database, case_id, case_spec_file_path):
     # insert in mongoDB
+    collection_uid = uuid.uuid4().hex
     spec = get_spec_from_json_fixture(case_id, case_spec_file_path)
     data = pd.read_json(json.dumps(spec['input']), orient='table').to_dict(orient='records')
-    mongo_collection.insert_many(data)
+    mongo_database[collection_uid].insert_many(data)
+    if (
+        'other_inputs' in spec and 'join' in case_id
+    ):  # needed for join step tests as we need a != collection
+        [
+            mongo_database[k].insert_many(
+                pd.read_json(json.dumps(v), orient='table').to_dict(orient='records')
+            )
+            for k, v in spec.get('other_inputs', {}).items()
+        ]
 
     # create query
     steps = spec['step']['pipeline']
     pipeline = Pipeline(steps=steps)
     query = translate_pipeline(pipeline)
-
     # execute query
-    result = list(mongo_collection.aggregate(query))
+    result = list(mongo_database[collection_uid].aggregate(query))
     df = pd.DataFrame(result)
     if '_id' in df:
         df.drop('_id', axis=1, inplace=True)
