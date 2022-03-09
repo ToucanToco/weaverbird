7c91448c5224b725cd3e31aa5829ea355d02681f
raphael.vignes@toucantoco.com <raphael.vignes@toucantoco.com>
2022-03-09 17:35:43 +0100

    wip

--- server/src/weaverbird/dereference_pipelines.py
+++ server/src/weaverbird/dereference_pipelines.py
@@ -1,23 +1,28 @@
 from typing import Any, Dict, List, Union
 
 from weaverbird.pipeline import Pipeline
+from weaverbird.pipeline.steps import BaseStep
 from weaverbird.pipeline.steps.utils.combination import Reference
 
 
 def _get_pipeline_for_domain(
-    reference: Union[Any, List[Dict[Any, Any]], str, Reference], pipelines: Dict[str, Pipeline]
+    reference: Union[str, List[Dict[str, str]]], pipelines: Dict[str, List[BaseStep]]
 ) -> Any:
     if isinstance(reference, str) and reference in pipelines:
         return dereference_pipelines(pipelines[reference], pipelines)
+    elif isinstance(reference, list) and 'domain' in reference[0]:
+        return reference
     else:
+        breakpoint()
         return [{'domain': reference, 'name': 'domain'}]
 
 
 def dereference_pipelines(
-    pipeline: Pipeline, pipelines: Dict[str, Pipeline]
+    pipeline: List[BaseStep], pipelines: Dict[str, List[BaseStep]]
 ) -> List[Dict[str, List[Reference]]]:
     dereferenced_pipeline = []  # type: ignore
-    for step in pipeline.steps:
+    breakpoint()
+    for step in pipeline:
         if step.name == 'domain':
             dereferenced_pipeline.append(*_get_pipeline_for_domain(step.domain, pipelines))  # type: ignore
         elif step.name == 'append':
