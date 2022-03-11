dff4cc7d76f68d85672084f04baf527d7a591e4a
raphael.vignes@toucantoco.com <raphael.vignes@toucantoco.com>
2022-03-11 17:29:24 +0100

    chore: format

--- server/src/weaverbird/backends/mongo_translator/steps/rename.py
+++ server/src/weaverbird/backends/mongo_translator/steps/rename.py
@@ -2,6 +2,7 @@ from typing import List
 
 from weaverbird.pipeline.steps import RenameStep
 
+
 def translate_rename(step: RenameStep) -> List:
     return [
         {'$addFields': {renaming[1]: f'${renaming[0]}'} for renaming in step.to_rename},
