db1d17129a77566eef822808a89a0ee9a727a7bd
raphael.vignes@toucantoco.com <raphael.vignes@toucantoco.com>
2022-03-14 16:52:45 +0100

    chore(mongo rename): handle edge case & add test fixtures

--- server/src/weaverbird/backends/mongo_translator/steps/rename.py
+++ server/src/weaverbird/backends/mongo_translator/steps/rename.py
@@ -5,6 +5,12 @@ from weaverbird.pipeline.steps import RenameStep
 
 def translate_rename(step: RenameStep) -> List:
     return [
-        {'$addFields': {renaming[1]: f'${renaming[0]}'} for renaming in step.to_rename},
-        {'$project': {renaming[0]: 0} for renaming in step.to_rename},
+        {'$addFields': {to_rename[1]: f'${to_rename[0]}' for to_rename in step.to_rename}},
+        {
+            '$project': {
+                to_rename[0]: 0
+                for to_rename in step.to_rename
+                if to_rename[0] not in [to_rename[1] for to_rename in step.to_rename]
+            }
+        },
     ]
