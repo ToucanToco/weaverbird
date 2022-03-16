f722bc84e40e4a850e8de905ae89016d72acf592
alice.sevin <alice.sevin@toucantoco.com>
2022-03-16 16:22:24 +0100

    feat(pandas): cast year int to str for to date step

--- server/src/weaverbird/backends/pandas_executor/steps/todate.py
+++ server/src/weaverbird/backends/pandas_executor/steps/todate.py
@@ -10,8 +10,14 @@ def execute_todate(
     domain_retriever: DomainRetriever = None,
     execute_pipeline: PipelineExecutor = None,
 ) -> DataFrame:
+    format = step.format
     timestamp_unit = 'ms' if df[step.column].dtype == 'int64' else None
+    # By default int are understood as timestamps, we prefer to convert it to %Y format if values are < 10_000
+    if format is None and df[step.column].dtype == 'int64' and df[step.column].max() < 10_000:
+        format = '%Y'
+        timestamp_unit = None
+
     datetime_serie = to_datetime(
-        df[step.column], format=step.format, errors='coerce', unit=timestamp_unit
+        df[step.column], format=format, errors='coerce', unit=timestamp_unit
     )
     return df.assign(**{step.column: datetime_serie})
