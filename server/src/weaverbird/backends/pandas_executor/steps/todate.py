f667d3c8495d5483db8e20d8eb0184d2f2f00c64
David Nowinsky <david@nowinsky.net>
2022-03-16 19:23:31 +0100

    refactor(mongo): try to clarify logic of year vs timestamps
    In particular, the fact that we can either have format or
    timestamp_unit.

--- server/src/weaverbird/backends/pandas_executor/steps/todate.py
+++ server/src/weaverbird/backends/pandas_executor/steps/todate.py
@@ -11,11 +11,15 @@ def execute_todate(
     execute_pipeline: PipelineExecutor = None,
 ) -> DataFrame:
     format = step.format
-    timestamp_unit = 'ms' if df[step.column].dtype == 'int64' else None
-    # By default int are understood as timestamps, we prefer to convert it to %Y format if values are < 10_000
-    if format is None and df[step.column].dtype == 'int64' and df[step.column].max() < 10_000:
-        format = '%Y'
-        timestamp_unit = None
+    timestamp_unit = None
+
+    if format is None and df[step.column].dtype == 'int64':
+        # By default int are understood as timestamps, we prefer to convert it to %Y format if values are < 10_000
+        if df[step.column].max() < 10_000:
+            format = '%Y'
+        else:
+            # Timestamps are expected in ms (not in ns, which is pandas' default)
+            timestamp_unit = 'ms'
 
     datetime_serie = to_datetime(
         df[step.column], format=format, errors='coerce', unit=timestamp_unit
