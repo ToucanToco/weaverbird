9d640275e6091d4b9f3c2ed982563087d72615fe
David Nowinsky <david@nowinsky.net>
2022-03-17 17:48:35 +0100

    feat(mongo): implement todate for simple cases
    - auto guess
      - handle timestamps
      - handle years
    - formats that mongo understands

--- server/src/weaverbird/backends/mongo_translator/steps/todate.py
+++ server/src/weaverbird/backends/mongo_translator/steps/todate.py
@@ -5,4 +5,69 @@ from weaverbird.pipeline.steps import ToDateStep
 
 
 def translate_todate(step: ToDateStep) -> List[MongoStep]:
-    return [{'$addFields': {step.column: {'$dateFromString': {'dateString': f'${step.column}'}}}}]
+    date_format = step.format
+
+    col_as_string = {'$toString': f'${step.column}'}
+
+    if date_format:
+        # % B and %b should be equivalent
+        date_format = date_format.replace('%B', '%b')
+
+    if not date_format:
+        # Mongo will try to guess the date format
+        return [
+            {
+                '$addFields': {
+                    step.column: {
+                        '$dateFromString': {
+                            'dateString': col_as_string,
+                            # However, we give it a second chance to find formats it can't guess natively
+                            'onError': {
+                                '$cond': [
+                                    # Integer values may be either years or timestamps
+                                    {'$eq': [{'$type': f'${step.column}'}, 'int']},
+                                    {
+                                        '$cond': [
+                                            # We decide that values lower than 10_000 will be interpreted as years...
+                                            {'$lt': [f'${step.column}', 10_000]},
+                                            {
+                                                '$dateFromString': {
+                                                    'dateString': {
+                                                        '$concat': [col_as_string, '-01-01']
+                                                    },
+                                                    'format': '%Y-%m-%d',
+                                                }
+                                            },
+                                            # ...and greater values will be interpreted as timestamps
+                                            {
+                                                '$convert': {
+                                                    'input': f'${step.column}',
+                                                    'to': 'date',
+                                                    'onError': {'$literal': None},
+                                                }
+                                            },
+                                        ]
+                                    },
+                                    # otherwise, we have no idea what date it corresponds to
+                                    {'$literal': None},
+                                ]
+                            },
+                        }
+                    }
+                }
+            }
+        ]
+    else:
+        return [
+            {
+                '$addFields': {
+                    step.column: {
+                        '$dateFromString': {
+                            'dateString': col_as_string,
+                            'format': date_format,
+                            'onError': {'$literal': None},
+                        }
+                    }
+                }
+            }
+        ]
