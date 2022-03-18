ebfb78bb11a9e22d837bc9f96d2b89443ada9c13
raphael.vignes@toucantoco.com <raphael.vignes@toucantoco.com>
2022-03-18 10:11:11 +0100

    chore: simplify build_cond_expression

--- server/src/weaverbird/backends/mongo_translator/utils.py
+++ server/src/weaverbird/backends/mongo_translator/utils.py
@@ -43,21 +43,14 @@ def build_cond_expression(
     unsupported_operators: List = []
 
     if isinstance(cond, ConditionComboAnd):
-        if len(cond.and_) == 1:
-            return build_cond_expression(cond.and_[0])
-        else:
-            return {'$and': [build_cond_expression(elem) for elem in cond.and_]}
+        return build_and_expression(cond)
     if isinstance(cond, ConditionComboOr):
         return {'$and': [build_cond_expression(elem) for elem in cond.or_]}
     if cond.operator in unsupported_operators:
         raise UnsupportedOperatorError(f'Unsupported operator ${cond.operator} in conditions')
 
     if cond.operator == 'matches' or cond.operator == 'notmatches':
-        cond_expression: MongoStep = {
-            '$regexMatch': {'input': f'${cond.column}', 'regex': cond.value}
-        }
-        if cond.operator == 'notmatches':
-            cond_expression = {'$not': cond_expression}
+        cond_expression = build_matches_expression(cond)
         return cond_expression
 
     else:
@@ -69,25 +62,43 @@ def build_cond_expression(
             if cond.operator == 'nin':
                 cond_expression = {'$not': cond_expression}
 
-            if cond.operator == 'until':
-                if isinstance(cond.value, datetime.datetime):
-                    cond_expression[operator_mapping[cond.operator]][1] = [
-                        datetime.datetime(
-                            day=cond.value.day, month=cond.value.month, year=cond.value.month
-                        ).replace(hour=23, minute=59, second=59, microsecond=999999)
-                    ]
-
-            if cond.operator == 'from' or cond.operator == 'until':
-                return {
-                    '$expr': {
-                        operator_mapping[cond.operator]: [
-                            truncate_to_day(f'${cond.column}'),
-                            truncate_to_day(
-                                translate_relative_date(cond.value)
-                                if isinstance(cond.value, RelativeDate)
-                                else cond.value
-                            ),
-                        ]
-                    }
-                }
+            cond_expression = build_dates_expressions(cond, cond_expression, operator_mapping)
             return cond_expression
+
+
+def build_dates_expressions(cond, cond_expression, operator_mapping):
+    if cond.operator == 'until':
+        if isinstance(cond.value, datetime.datetime):
+            cond_expression[operator_mapping[cond.operator]][1] = [
+                datetime.datetime(
+                    day=cond.value.day, month=cond.value.month, year=cond.value.month
+                ).replace(hour=23, minute=59, second=59, microsecond=999999)
+            ]
+    if cond.operator == 'from' or cond.operator == 'until':
+        cond_expression = {
+            '$expr': {
+                operator_mapping[cond.operator]: [
+                    truncate_to_day(f'${cond.column}'),
+                    truncate_to_day(
+                        translate_relative_date(cond.value)
+                        if isinstance(cond.value, RelativeDate)
+                        else cond.value
+                    ),
+                ]
+            }
+        }
+    return cond_expression
+
+
+def build_matches_expression(cond):
+    cond_expression: MongoStep = {'$regexMatch': {'input': f'${cond.column}', 'regex': cond.value}}
+    if cond.operator == 'notmatches':
+        cond_expression = {'$not': cond_expression}
+    return cond_expression
+
+
+def build_and_expression(cond):
+    if len(cond.and_) == 1:
+        return build_cond_expression(cond.and_[0])
+    else:
+        return {'$and': [build_cond_expression(elem) for elem in cond.and_]}
