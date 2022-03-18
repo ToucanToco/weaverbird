8e896b77c3fda8c1aac1c13fe90d3bc5fac23423
raphael.vignes@toucantoco.com <raphael.vignes@toucantoco.com>
2022-03-18 10:04:14 +0100

    chore: updates from review

--- server/src/weaverbird/backends/mongo_translator/steps/sort.py
+++ server/src/weaverbird/backends/mongo_translator/steps/sort.py
@@ -5,7 +5,11 @@ from weaverbird.pipeline.steps import SortStep
 
 
 def translate_sort(step: SortStep) -> List[MongoStep]:
-    sort_mongo = {}
-    for sort_column in step.columns:
-        sort_mongo[sort_column.column] = 1 if sort_column.order == 'asc' else -1
-    return [{'$sort': sort_mongo}]
+    return [
+        {
+            '$sort': {
+                sort_column.column: 1 if sort_column.order == 'asc' else -1
+                for sort_column in step.columns
+            }
+        }
+    ]
