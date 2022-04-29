cc894e0c89c0817faf588c5728acf844f1a5c2eb
Luka Peschke <luka.peschke@toucantoco.com>
2022-04-29 18:09:33 +0200

    feat(mongo_translator): Implemented pivot step (#1309)
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- server/src/weaverbird/backends/mongo_translator/steps/__init__.py
+++ server/src/weaverbird/backends/mongo_translator/steps/__init__.py
@@ -13,6 +13,7 @@ from weaverbird.backends.mongo_translator.steps.fromdate import translate_fromda
 from weaverbird.backends.mongo_translator.steps.ifthenelse import translate_ifthenelse
 from weaverbird.backends.mongo_translator.steps.join import translate_join
 from weaverbird.backends.mongo_translator.steps.lowercase import translate_lowercase
+from weaverbird.backends.mongo_translator.steps.pivot import translate_pivot
 from weaverbird.backends.mongo_translator.steps.rename import translate_rename
 from weaverbird.backends.mongo_translator.steps.replace import translate_replace
 from weaverbird.backends.mongo_translator.steps.select import translate_select
@@ -41,6 +42,7 @@ mongo_step_translator: Dict[str, Callable[[Any], list]] = {
     'ifthenelse': translate_ifthenelse,  # type: ignore
     'join': translate_join,
     'lowercase': translate_lowercase,
+    'pivot': translate_pivot,
     'rename': translate_rename,
     'replace': translate_replace,
     'select': translate_select,
---
+++ server/src/weaverbird/backends/mongo_translator/steps/pivot.py
@@ -0,0 +1,67 @@
+from typing import List
+
+from weaverbird.backends.mongo_translator.steps.types import MongoStep
+from weaverbird.pipeline.steps.pivot import PivotStep
+
+
+def translate_pivot(step: PivotStep) -> List[MongoStep]:
+    group_cols = {}
+    add_fields_step = {}
+
+    # Prepare groupCols to populate the `_id` field sof Mongo `$group` steps and addFields step
+    for col in step.index:
+        group_cols[col] = f'$_id.{col}'
+        add_fields_step[f'_vqbAppTmpObj.{col}'] = f'$_id.{col}'
+
+    pivot_mongo_agg_stages: List[MongoStep] = [
+        # First we perform the aggregation with the _id including the column to pivot
+        {
+            '$group': {
+                '_id': {
+                    **{col: f"${col}" for col in step.index},
+                    step.column_to_pivot: f'${step.column_to_pivot}',
+                },
+                step.value_column: {f'${step.agg_function}': f'${step.value_column}'},
+            },
+        },
+        # Then we group with with index columns as _id and we push documents as an array of sets
+        # including a column for the column to pivot and a column for the corresponding value
+        {
+            '$group': {
+                '_id': group_cols,
+                '_vqbAppArray': {
+                    '$addToSet': {
+                        step.column_to_pivot: f'$_id.{step.column_to_pivot}',
+                        step.value_column: f'${step.value_column}',
+                    },
+                },
+            },
+        },
+        # Then we project a tmp key to get an object from the array of couples [column_to_pivot, corresponding_value]
+        # including a column for the column to pivot and a column for the corresponding value
+        {
+            '$project': {
+                '_vqbAppTmpObj': {
+                    '$arrayToObject': {
+                        '$zip': {
+                            'inputs': [
+                                f'$_vqbAppArray.{step.column_to_pivot}',
+                                f'$_vqbAppArray.{step.value_column}',
+                            ],
+                        },
+                    },
+                },
+            },
+        },
+    ]
+
+    if add_fields_step:
+        # Then we include the index columns back in every document created in the previous step
+        # (still accessible in the _id object)
+        pivot_mongo_agg_stages.append({'$addFields': add_fields_step})
+
+    # Then we replace the root of the documents tree to get our columns ready for
+    # our needed table-like, unnested format
+    pivot_mongo_agg_stages.append({'$replaceRoot': {'newRoot': '$_vqbAppTmpObj'}})
+
+    return pivot_mongo_agg_stages
