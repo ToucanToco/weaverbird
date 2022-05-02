e4f8b9e7197f4a309cef743f7829302e094a2465
sanix-darker <s4nixd@gmail.com>
2022-05-02 19:51:16 +0200

    doc: added missing docstring

--- server/src/weaverbird/backends/mongo_translator/steps/rank.py
+++ server/src/weaverbird/backends/mongo_translator/steps/rank.py
@@ -5,6 +5,21 @@ from weaverbird.pipeline.steps import RankStep
 
 
 def translate_rank(step: RankStep) -> List[MongoStep]:
+
+    """
+    Here we define the order variable that will be used in the '$reduce' step
+    defined below. The order definition depends on the ranking method chosen.
+
+    Example of ranking output depending on method chosen:
+
+     - standard: [10, 15, 15, 15, 20, 20, 22] => [1, 2, 2, 2, 5, 5, 7]
+     - dense: [10, 15, 15, 15, 20, 20, 22] => [1, 2, 2, 2, 3, 3, 4]
+
+    Notes on special Mongo variables used in the '$reduce' step defined below:
+
+     - '$$this' refers to the element being processed
+     - '$$value' refers to the cumulative value of the expression
+    """
     vqb_var_order: MongoStep = {}
 
     if step.method == 'dense':
@@ -18,6 +33,17 @@ def translate_rank(step: RankStep) -> List[MongoStep]:
     else:
         vqb_var_order = {'$add': ['$$value.order', 1]}
 
+    """
+    This is the variable object used in the '$reduce' step described below (see
+    the object structure in the 'rankedArray' doc below). It's here that we
+    compute the rank, that compares two consecutive documents in sorted arrays
+    and which definition depends on the ranking method chosen (see above)
+
+    Notes on special Mongo variables used in the '$reduce' step defined below:
+
+    - '$$this' refers to the element being processed
+    - '$$value' refers to the cumulative value of the expression
+    """
     vqb_var_obj: MongoStep = {
         '$let': {
             'vars': {
@@ -57,6 +83,22 @@ def translate_rank(step: RankStep) -> List[MongoStep]:
         },
     }
 
+    """
+    This step transforms sorted arrays (1 array per group as specified by the
+    'groupby' parameter) of documents into an array of the same sorted documents,
+    with the information of ranking of each document added ionto each(key 'rank).
+
+    To do so we reduce orignal arrays in one document each with the structure:
+    {
+        a: [ < list of sorted documents with rank key added > ],
+        order: < an order counter >,
+        prevValue: < to keep track of previous document value >,
+        prevRank: < to keep track of previous document rank >
+    }
+
+    At the end we just extract the 'a' array (as other keys were only useful as
+    variables in the '$reduce' step)
+    """
     ranked_array: MongoStep = {
         '$let': {
             'vars': {
