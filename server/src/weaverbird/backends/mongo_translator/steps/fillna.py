20dde81e32eff71d5cc798a17605d539496a2100
Luka Peschke <luka.peschke@toucantoco.com>
2022-05-03 15:06:43 +0200

    feat(mongo_translator): Implement fillna step (#1315)
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

---
+++ server/src/weaverbird/backends/mongo_translator/steps/fillna.py
@@ -0,0 +1,9 @@
+from typing import List
+
+from weaverbird.backends.mongo_translator.steps.types import MongoStep
+from weaverbird.pipeline.steps.fillna import FillnaStep
+
+
+def translate_fillna(step: FillnaStep) -> List[MongoStep]:
+    add_fields = {col: {'$ifNull': [f'${col}', step.value]} for col in step.columns}
+    return [{'$addFields': add_fields}]
