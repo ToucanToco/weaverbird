5aede467bf1fc41e627061e8947a967d0bd7d783
Frédéric Matigot <fred@fspot.org>
2022-05-24 09:24:42 +0200

    chore(server): handle cumsum legacy syntax aliases (#1338)

--- server/tests/steps/test_cumsum.py
+++ server/tests/steps/test_cumsum.py
@@ -1,7 +1,17 @@
 from pandas import DataFrame
 
+from tests.utils import assert_dataframes_equals
 from weaverbird.backends.pandas_executor.steps.cumsum import execute_cumsum
 from weaverbird.pipeline.steps import CumSumStep
+
+
+def test_cumsum_legacy_syntax():
+    df = DataFrame({'x': [1, 2, 3], 'date': ['2020', '2021', '2022']})
+    step = CumSumStep(name='cumsum', referenceColumn='date', valueColumn='x', newColumn='y')  # type: ignore
+    df_result = execute_cumsum(step, df)
+
+    expected_result = DataFrame({'x': [1, 2, 3], 'y': [1, 3, 6], 'date': ['2020', '2021', '2022']})
+    assert_dataframes_equals(df_result, expected_result)
 
 
 def test_benchmark_cumsum_legacy_syntax(benchmark):
