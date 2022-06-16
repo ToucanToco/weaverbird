e23bcb2d7138591a5a5a0501b720f350b234c124
Luka Peschke <luka.peschke@toucantoco.com>
2022-06-16 18:29:16 +0200

    feat(server/backends/pandas): Implement geographic hierarchy step
    TCTC-2941
    
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

---
+++ server/src/weaverbird/pipeline/steps/hierarchy.py
@@ -0,0 +1,17 @@
+from typing import Literal
+
+from pydantic import validator
+
+from weaverbird.pipeline.steps.utils.base import BaseStep
+
+
+class HierarchyStep(BaseStep):
+    name: Literal['hierarchy'] = 'hierarchy'
+    hierarchy_level_column: str = 'hierarchy_level'
+    hierarchy: list[str]
+    include_nulls: bool = False
+
+    @validator('hierarchy')
+    def _ensure_hierarchy(cls, values: list[str]) -> list[str]:
+        assert len(values) > 0, "at least one value must be specified"
+        return values
