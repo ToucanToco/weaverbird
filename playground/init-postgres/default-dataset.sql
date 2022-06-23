8ac3e7b8d0b8712676f7bd97c7b9015ed3063131
David Nowinsky <david@nowinsky.net>
2022-06-23 18:18:28 +0200

    fix(playground): correct case for postgres columns

--- playground/init-postgres/default-dataset.sql
+++ playground/init-postgres/default-dataset.sql
@@ -1,10 +1,10 @@
 CREATE TABLE default_dataset (
-  Label VARCHAR(255),
-  Value1 INTEGER,
-  Value2 INTEGER,
-  Value3 INTEGER,
-  Value4 INTEGER,
-  Groups VARCHAR(255)
+  "Label" VARCHAR(255),
+  "Value1" INTEGER,
+  "Value2" INTEGER,
+  "Value3" INTEGER,
+  "Value4" INTEGER,
+  "Groups" VARCHAR(255)
 );
 
-COPY default_dataset(Label,Value1,Value2,Value3,Value4,Groups) FROM '/datasources/default-dataset.csv' DELIMITER ',' CSV HEADER;
+COPY default_dataset("Label","Value1","Value2","Value3","Value4","Groups") FROM '/datasources/default-dataset.csv' DELIMITER ',' CSV HEADER;
--- playground/init-postgres/sales.sql
+++ playground/init-postgres/sales.sql
@@ -1,16 +1,16 @@
 CREATE TABLE sales (
-  Transaction_date TIMESTAMP,
-  Product VARCHAR(255),
-  Price INTEGER,
-  Payment_Type VARCHAR(255),
-  Name VARCHAR(255),
-  City VARCHAR(255),
-  State VARCHAR(255),
-  Country VARCHAR(255),
-  Account_Created TIMESTAMP,
-  Last_Login TIMESTAMP,
-  Latitude DOUBLE PRECISION,
-  Longitude DOUBLE PRECISION
+  "Transaction_date" TIMESTAMP,
+  "Product" VARCHAR(255),
+  "Price" INTEGER,
+  "Payment_Type" VARCHAR(255),
+  "Name" VARCHAR(255),
+  "City" VARCHAR(255),
+  "State" VARCHAR(255),
+  "Country" VARCHAR(255),
+  "Account_Created" TIMESTAMP,
+  "Last_Login" TIMESTAMP,
+  "Latitude" DOUBLE PRECISION,
+  "Longitude" DOUBLE PRECISION
 );
 
-COPY sales(Transaction_date,Product,Price,Payment_Type,Name,City,State,Country,Account_Created,Last_Login,Latitude,Longitude) FROM '/datasources/sales.csv' DELIMITER ',' CSV HEADER;
+COPY sales("Transaction_date","Product","Price","Payment_Type","Name","City","State","Country","Account_Created","Last_Login","Latitude","Longitude") FROM '/datasources/sales.csv' DELIMITER ',' CSV HEADER;
