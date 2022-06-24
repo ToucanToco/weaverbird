4af30f24e88619da0d357de10d29bb8debb501ac
David Nowinsky <david@nowinsky.net>
2022-06-24 14:38:50 +0200

    chore(ui): duration support for postgresql

--- src/lib/translators/postgresql.ts
+++ src/lib/translators/postgresql.ts
@@ -1,6 +1,12 @@
+import * as S from '@/lib/steps';
+
 import { PypikaTranslator } from './pypika';
 
 /* istanbul ignore next */
 export class PostgresqlTranslator extends PypikaTranslator {
   static label = 'PostgreSQL';
+
+  duration(step: Readonly<S.ComputeDurationStep>) {
+    return step;
+  }
 }
