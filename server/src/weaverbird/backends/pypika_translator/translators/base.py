d2a8195b699599c96fb22ab5e95de020bf519807
sanix-darker <s4nixd@gmail.com>
2022-07-01 14:58:53 +0200

    clean: move cumsum(commented) from base to snowflake

--- server/src/weaverbird/backends/pypika_translator/translators/base.py
+++ server/src/weaverbird/backends/pypika_translator/translators/base.py
@@ -314,38 +314,6 @@ class SQLTranslator(ABC):
         # without actually executing the query
         return custom_query, StepTable(columns=["*"])
 
-    # def cumsum(
-    #     self: Self, *, step: "CumSumStep", table: StepTable
-    # ) -> tuple["QueryBuilder", StepTable]:
-    #     # !FIXME / TODO
-    #     # Since "OVER (PARTITION BY..." and "rows UNBOUNDED PRECEDING" are
-    #     # synthax used on snowflake for this step not available on redshift nor postgresql
-    #     # a tweak should be made here to adapt that on pypika, something like (https://pypika.readthedocs.io/en/latest/3_advanced.html?highlight=rank#ntile-and-rank) , this step is not
-    #     # completed, an equivalent of this step can looks like :
-
-    #     sub_query: "QueryBuilder" = self.QUERY_CLS.from_(table.name).select(*table.columns)
-    #     cumsum_col_list = [cumsum[1] or f"{cumsum[0]}_CUMSUM" for cumsum in step.to_cumsum]
-
-    #     the_table = Table(table.name)
-    #     reference_column_field: Field = the_table[step.reference_column]
-
-    #     sub_query = (
-    #         sub_query.select(
-    #             *(
-    #                 functions.Sum(the_table[cumsum[0]]).as_(cumsum[1] or f"{cumsum[0]}_CUMSUM")
-    #                 for cumsum in step.to_cumsum
-    #             ),
-    #         )
-    #         .orderby(reference_column_field, order=Order.asc)
-    #         .groupby(*(step.groupby or [*table.columns]))
-    #     )  # commented .over() .over(*groups_fields)
-
-    #     query: "QueryBuilder" = (
-    #         self.QUERY_CLS.from_(sub_query).select(*(*table.columns, *cumsum_col_list))
-    #     ).from_(table.name)
-
-    #     return query, StepTable(columns=[*table.columns, *cumsum_col_list])
-
     def delete(
         self: Self, *, step: "DeleteStep", table: StepTable
     ) -> tuple["QueryBuilder", StepTable]:
--- server/src/weaverbird/backends/pypika_translator/translators/snowflake.py
+++ server/src/weaverbird/backends/pypika_translator/translators/snowflake.py
@@ -21,5 +21,40 @@ class SnowflakeTranslator(SQLTranslator):
     REGEXP_OP = RegexOp.REGEXP
     TO_DATE_OP = ToDateOp.TO_DATE
 
+    # This step is not handling all edge cases so it's commented for now
+    # def cumsum(
+    #     self: Self, *, step: "CumSumStep", table: StepTable
+    # ) -> tuple["QueryBuilder", StepTable]:
+    #     # !FIXME / TODO
+    #     # Since "OVER (PARTITION BY..." and "rows UNBOUNDED PRECEDING" are
+    #     # synthax used on snowflake for this step not available on redshift nor postgresql
+    #     # a tweak should be made here to adapt that on pypika, something like
+    #     # (https://pypika.readthedocs.io/en/latest/3_advanced.html?highlight=rank#ntile-and-rank),
+    #     # this step is not
+    #     # completed, an equivalent of this step can looks like :
+
+    #     sub_query: "QueryBuilder" = self.QUERY_CLS.from_(table.name).select(*table.columns)
+    #     cumsum_col_list = [cumsum[1] or f"{cumsum[0]}_CUMSUM" for cumsum in step.to_cumsum]
+
+    #     the_table = Table(table.name)
+    #     reference_column_field: Field = the_table[step.reference_column]
+
+    #     sub_query = (
+    #         sub_query.select(
+    #             *(
+    #                 functions.Sum(the_table[cumsum[0]]).as_(cumsum[1] or f"{cumsum[0]}_CUMSUM")
+    #                 for cumsum in step.to_cumsum
+    #             ),
+    #         )
+    #         .orderby(reference_column_field, order=Order.asc)
+    #         .groupby(*(step.groupby or [*table.columns]))
+    #     )  # commented .over() .over(*groups_fields)
+
+    #     query: "QueryBuilder" = (
+    #         self.QUERY_CLS.from_(sub_query).select(*(*table.columns, *cumsum_col_list))
+    #     ).from_(table.name)
+
+    #     return query, StepTable(columns=[*table.columns, *cumsum_col_list])
+
 
 SQLTranslator.register(SnowflakeTranslator)
