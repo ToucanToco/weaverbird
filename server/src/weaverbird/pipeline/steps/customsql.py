f53c5e06cfd4a021108fad8893d0467166380353
Luka Peschke <luka.peschke@toucantoco.com>
2022-07-06 11:47:20 +0200

    fix(server/steps/customsql): Sanitize & validate query
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- server/src/weaverbird/pipeline/steps/customsql.py
+++ server/src/weaverbird/pipeline/steps/customsql.py
@@ -1,5 +1,7 @@
 from typing import Literal
 
+from pydantic import validator
+
 from weaverbird.pipeline.steps.utils.base import BaseStep
 from weaverbird.pipeline.steps.utils.render_variables import StepWithVariablesMixin
 
@@ -7,6 +9,17 @@ from weaverbird.pipeline.steps.utils.render_variables import StepWithVariablesMi
 class CustomSqlStep(BaseStep):
     name: Literal['customsql'] = 'customsql'
     query: str
+
+    @staticmethod
+    def _strip_query(query: str) -> str:
+        return query.strip().strip(';')
+
+    @validator('query')
+    def _validate_query(cls, query: str) -> str:
+        assert ';' not in (
+            stripped := cls._strip_query(query)
+        ), "Custom SQL queries must not contain semicolumns"
+        return stripped
 
 
 class CustomSqlStepWithVariables(CustomSqlStep, StepWithVariablesMixin):
---
+++ server/tests/steps/test_cumstomsql.py
@@ -0,0 +1,29 @@
+import pydantic
+import pytest
+
+from weaverbird.pipeline.steps import CustomSqlStep
+
+
+@pytest.mark.parametrize(
+    'query,expected',
+    [
+        ('SELECT * FROM coucou', 'SELECT * FROM coucou'),
+        ('SELECT * FROM ##PREVIOUS_STEP##', 'SELECT * FROM ##PREVIOUS_STEP##'),
+        ('  SELECT * FROM coucou;  ', 'SELECT * FROM coucou'),
+        ('  SELECT * FROM ##PREVIOUS_STEP##;  ', 'SELECT * FROM ##PREVIOUS_STEP##'),
+    ],
+)
+def test_query_sanitization(query: str, expected: str):
+    assert CustomSqlStep(query=query).query == expected
+
+
+@pytest.mark.parametrize(
+    'query',
+    [
+        'SELECT a FROM b; SELECT c FROM d;',
+        '  SELECT a FROM b  ; SELECT c FROM d;  ',
+    ],
+)
+def test_query_invalid(query: str):
+    with pytest.raises(pydantic.ValidationError):
+        CustomSqlStep(query=query)
