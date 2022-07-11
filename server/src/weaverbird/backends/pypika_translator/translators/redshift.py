e78be89e44aa5147d224e95560b093fd27662d79
Luka Peschke <luka.peschke@toucantoco.com>
2022-07-11 17:28:37 +0200

    fix(server/pypika): Fix concatenate step with redshift (#1412)
    TCTC-3333
    
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- server/src/weaverbird/backends/pypika_translator/translators/redshift.py
+++ server/src/weaverbird/backends/pypika_translator/translators/redshift.py
@@ -1,13 +1,24 @@
-from typing import TypeVar
+from typing import TYPE_CHECKING, TypeVar
 
+from pypika import functions
 from pypika.dialects import RedshiftQuery
+from pypika.queries import QueryBuilder, Table
 
 from weaverbird.backends.pypika_translator.dialects import SQLDialect
 from weaverbird.backends.pypika_translator.operators import FromDateOp, RegexOp, ToDateOp
-from weaverbird.backends.pypika_translator.translators.base import DataTypeMapping, SQLTranslator
+from weaverbird.backends.pypika_translator.translators.base import (
+    DataTypeMapping,
+    SQLTranslator,
+    StepContext,
+)
 from weaverbird.backends.pypika_translator.translators.postgresql import PostgreSQLTranslator
 
-Self = TypeVar("Self", bound="SQLTranslator")
+if TYPE_CHECKING:
+
+    from weaverbird.pipeline.steps import ConcatenateStep
+
+
+Self = TypeVar("Self", bound="RedshiftTranslator")
 
 
 class RedshiftTranslator(PostgreSQLTranslator):
@@ -25,6 +36,42 @@ class RedshiftTranslator(PostgreSQLTranslator):
     FROM_DATE_OP = FromDateOp.TO_CHAR
     REGEXP_OP = RegexOp.SIMILAR_TO
     TO_DATE_OP = ToDateOp.TO_DATE
+
+    # Redshift's CONCAT function does not support more than 2 terms, but concats can be nested. This
+    # helpers allow to nest concatenations:
+    # https://docs.aws.amazon.com/redshift/latest/dg/r_CONCAT.html
+    def _recursive_concat(
+        self, concat: functions.Concat | None, tokens: list[str]
+    ) -> functions.Concat:
+        if len(tokens) == 0:
+            assert concat is not None
+            return concat
+        if concat is None:
+            concat = functions.Concat(tokens[0], tokens[1])
+            return self._recursive_concat(concat, tokens[2:])
+        return self._recursive_concat(functions.Concat(concat, tokens[0]), tokens[1:])
+
+    def concatenate(
+        self: Self,
+        *,
+        builder: QueryBuilder,
+        prev_step_name: str,
+        columns: list[str],
+        step: "ConcatenateStep",
+    ) -> StepContext:
+        # from step.columns = ["city", "age", "username"], step.separator = " -> "
+        # create [Field("city"), " -> ", Field("age"), " -> ", Field("username")]
+        the_table = Table(prev_step_name)
+        tokens = [the_table[step.columns[0]]]
+        for col in step.columns[1:]:
+            tokens.append(step.separator)
+            tokens.append(the_table[col])
+
+        query: QueryBuilder = self.QUERY_CLS.from_(prev_step_name).select(
+            *columns,
+            self._recursive_concat(None, tokens).as_(step.new_column_name),
+        )
+        return StepContext(query, columns + [step.new_column_name])
 
 
 SQLTranslator.register(RedshiftTranslator)
