c266ea2a002cf5eb01cfe365216650fffa05abe3
Luka Peschke <luka.peschke@toucantoco.com>
2022-07-11 16:55:37 +0200

    fix(server/pypika): Fix concatenate step with redshift
    TCTC-3333
    
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

---
+++ server/tests/backends/sql_translator_unit_tests/test_redshift_translator.py
@@ -0,0 +1,17 @@
+import pytest
+from pypika.queries import Table
+
+from weaverbird.backends.pypika_translator.translators.redshift import RedshiftTranslator
+
+
+@pytest.fixture
+def redshift_translator() -> RedshiftTranslator:
+    return RedshiftTranslator(tables_columns={})
+
+
+def test_recursive_concat(redshift_translator: RedshiftTranslator):
+    the_table = Table('table')
+    concat = redshift_translator._recursive_concat(
+        None, [the_table['col1'], ' - ', the_table['col2'], ' - ', the_table['col3']]
+    )
+    assert str(concat) == """CONCAT(CONCAT(CONCAT(CONCAT("col1",' - '),"col2"),' - '),"col3")"""
