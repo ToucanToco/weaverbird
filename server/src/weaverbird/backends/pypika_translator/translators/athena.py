d4af21899e8d0b4f57c4bd23f08afe80a7610b84
sanix-darker <s4nixd@gmail.com>
2022-07-12 14:48:23 +0200

    fix: lint

--- server/src/weaverbird/backends/pypika_translator/translators/athena.py
+++ server/src/weaverbird/backends/pypika_translator/translators/athena.py
@@ -1,15 +1,7 @@
-from datetime import datetime
-
-from pypika import functions
 from pypika.dialects import Query
-from pypika.queries import QueryBuilder
 
-from weaverbird.backends.pandas_executor.steps.utils.dates import evaluate_relative_date
 from weaverbird.backends.pypika_translator.dialects import SQLDialect
-from weaverbird.backends.pypika_translator.translators.base import SQLTranslator, StepContext
-from weaverbird.pipeline.conditions import ConditionComboAnd, ConditionComboOr, DateBoundCondition
-from weaverbird.pipeline.dates import RelativeDate
-from weaverbird.pipeline.steps.filter import FilterStep
+from weaverbird.backends.pypika_translator.translators.base import SQLTranslator
 
 
 class AthenaTranslator(SQLTranslator):
