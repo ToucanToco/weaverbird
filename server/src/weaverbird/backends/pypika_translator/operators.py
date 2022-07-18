b437b1b73c6c1b47789218a12533d79711af3d86
sanix-darker <s4nixd@gmail.com>
2022-07-18 12:14:47 +0200

    feat: added to the operator custom regexpFunction keys

--- server/src/weaverbird/backends/pypika_translator/operators.py
+++ server/src/weaverbird/backends/pypika_translator/operators.py
@@ -1,12 +1,14 @@
 from enum import Enum, auto
 
 
-class RegexOp(Enum):
+class RegexOp(str, Enum):
     REGEXP = auto()
     SIMILAR_TO = auto()
     CONTAINS = auto()
-    REGEXP_LIKE = auto()
-    REGEXP_CONTAINS = auto()
+    REGEXP_LIKE = "REGEXP_LIKE"
+    REGEXP_CONTAINS = "REGEXP_CONTAINS"
+    NOT_REGEXP_LIKE = "NOT REGEXP_LIKE"
+    NOT_REGEXP_CONTAINS = "NOT REGEXP_CONTAINS"
 
 
 class FromDateOp(Enum):
