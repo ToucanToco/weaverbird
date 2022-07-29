00aed88a5ed9b227c7a850adebb5922c14de817f
Luka Peschke <luka.peschke@toucantoco.com>
2022-07-29 15:03:17 +0200

    refactor(server): make Reference model hashable
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- server/src/weaverbird/pipeline/steps/utils/combination.py
+++ server/src/weaverbird/pipeline/steps/utils/combination.py
@@ -7,6 +7,11 @@ class Reference(BaseModel):
     type: Literal['ref'] = 'ref'
     uid: str
 
+    # Adding a prefix so is does not have the same hash as a domain whose name would be the same as
+    # the uid
+    def __hash__(self):
+        return hash(f'__ref__{self.uid}')
+
 
 PipelineOrDomainName = Union[List[dict], str]  # can be either a domain name or a complete pipeline
 PipelineOrDomainNameOrReference = Union[PipelineOrDomainName, Reference]
