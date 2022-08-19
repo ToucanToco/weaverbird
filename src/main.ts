77730e4333c507c431601dfb4d931577f13c9505
Nino Filiu <nino.filiu@gmail.com>
2022-08-19 20:28:41 +0200

    chore: export types from types.ts

--- src/main.ts
+++ src/main.ts
@@ -50,11 +50,3 @@ export { dateRangeToString } from './lib/dates';
 
 // export directives
 export { resizable } from './directives/resizable/resizable';
-
-// export types
-export { PipelineStep, Pipeline } from './lib/steps';
-export { VariableDelimiters, VariablesBucket } from './lib/variables';
-export { ColumnValueStat } from './lib/dataset/helpers';
-export { PaginationContext } from './lib/dataset/pagination';
-export { DataSetColumnType, DataSetColumn, DataSet } from './lib/dataset';
-export { BackendError, BackendWarning, BackendResponse, BackendService } from './lib/backend';
