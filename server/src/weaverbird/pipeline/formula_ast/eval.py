ae39965e1d94d030b9121ad3ba0a9fff0d138778
Luka Peschke <luka.peschke@toucantoco.com>
2022-08-22 15:59:08 +0200

    docs: Add comments to formula evaluator
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- server/src/weaverbird/pipeline/formula_ast/eval.py
+++ server/src/weaverbird/pipeline/formula_ast/eval.py
@@ -45,6 +45,7 @@ class FormulaParser:
 
     @staticmethod
     def sanitize_string(s: str) -> str:
+        """Unquotes strings, escapes single quotes and wraps them in single quotes"""
         # Removing outer quotes
         unquoted = ast.literal_eval(s)
         # Escaping single quotes in the string
@@ -53,6 +54,8 @@ class FormulaParser:
         return f"'{with_escaped_single_quotes}'"
 
     def _iterate_tokens(self, tokens: Iterator[tokenize.TokenInfo]) -> Generator[str, None, None]:
+        """This function iterates over tokens, sanitizes and yields them."""
+
         def next_token():
             try:
                 return next(tokens)
@@ -61,9 +64,12 @@ class FormulaParser:
 
         def parse_col_name(prev_token: tokenize.TokenInfo) -> str:
             chunks = []
+            # Skipping the current token, which is [
             while tok := next_token():
+                # We have an identifier, append it
                 if tok.type == tokenize.NAME:
                     chunks.append(tok.string)
+                # We reached the end of the column name. Store an alias for it and yield the alias
                 elif tok.type == tokenize.OP and tok.string == ']':
                     if len(chunks) < 1:
                         raise EmptyColumnName(f'Got an empty column name at {prev_token.start[1]}')
@@ -78,16 +84,29 @@ class FormulaParser:
             )
 
         while token := next_token():
-            if token.type in (tokenize.ENCODING, tokenize.NEWLINE, tokenize.ENDMARKER):
+            if token.type in (
+                tokenize.ENCODING,
+                tokenize.INDENT,
+                tokenize.NEWLINE,
+                tokenize.ENDMARKER,
+            ):
+                # Those are whitespace tokens we don't care about, so not yielding them
                 continue
             elif token.type == tokenize.STRING:
+                # In case we have a string literal, we sanitize it
                 yield self.sanitize_string(token.string)
             elif token.type == tokenize.OP and token.string == '[':
+                # Square brackets are delimiters for column names, so here we read everyhting until
+                # the closing bracket
                 yield parse_col_name(token)
             else:
                 yield token.string
 
     def sanitize_formula(self) -> str:
+        """Removes [] around column names and aliases them.
+
+        This is needed because stuff parsed with the STL's ast module needs to be valid python code
+        """
         self._columns = {}
         # Stripping because strings starting with whitespace raise UnexpectedIndent when parsed by
         # the ast module
@@ -97,6 +116,10 @@ class FormulaParser:
 
     @staticmethod
     def _operator_from_ast_op(op: ast.operator) -> types.Operator:
+        """Returns one of our known operators from an operator of the ast module.
+
+        Raise an exception in case of an unsupported operator.
+        """
         match op:
             case ast.Add():
                 return types.Operator.ADD
@@ -112,16 +135,23 @@ class FormulaParser:
                 raise UnsupportedOperator(f'Unsupported operator: {op}')
 
     def _build_name(self, name: str) -> types.ColumnName | bool:
+        """Builds a ColumnName from a raw name.
+
+        In case the name is "true" or "false", returns the equivalent boolean
+        (users do not expect to have to capitalize booleans).
+        """
         if name == 'true':
             return True
         if name == 'false':
             return False
-        if name in self._columns:
-            return types.ColumnName(name=self._columns[name], alias=name)
-        else:
-            return types.ColumnName(name=name, alias=name)
+        return (
+            types.ColumnName(name=self._columns[name], alias=name)
+            if name in self._columns
+            else types.ColumnName(name=name, alias=name)
+        )
 
     def _parse_expr(self, expr: ast.expr) -> types.Expression:
+        """Takes an expression of the ast module and builds an AST in our own format from it."""
         match expr:
             # +LITERAL: +1, +13.37
             case ast.UnaryOp(op=ast.UAdd(), operand=ast.Constant(value=value)):
@@ -138,11 +168,13 @@ class FormulaParser:
                 return types.Operation(
                     left=-1, operator=types.Operator.MUL, right=self._build_name(name)
                 )
+            # Recursing down into both branches of the operation
             case ast.BinOp(left=left, right=right, op=op):
                 operator = self._operator_from_ast_op(op)
                 return types.Operation(
                     left=self._parse_expr(left), right=self._parse_expr(right), operator=operator
                 )
+            # Constant: number, string literal or boolean
             case ast.Constant(value=value):
                 # bool is a subtype of int
                 if isinstance(value, (int, float)):
@@ -153,6 +185,7 @@ class FormulaParser:
                     raise UnsupportedConstant(
                         f"Unsupported constant '{expr}' of type {type(value)}"
                     )
+            # Column name
             case ast.Name(id=name):
                 return self._build_name(name)
             case _:
