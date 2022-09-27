23d8eea715e239414f50299911626da0d6825f6e
Luka Peschke <luka.peschke@toucantoco.com>
2022-09-27 09:37:11 +0200

    feat(frontend): Add dateextract step to different native translators (#1472)
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- src/lib/translators/athena.ts
+++ src/lib/translators/athena.ts
@@ -1,6 +1,12 @@
+import * as S from '@/lib/steps';
+
 import { PypikaTranslator } from './pypika';
 
 /* istanbul ignore next */
 export class AthenaTranslator extends PypikaTranslator {
   static label = 'AthenaTranslator';
+
+  dateextract(step: Readonly<S.DateExtractStep>) {
+    return step;
+  }
 }
