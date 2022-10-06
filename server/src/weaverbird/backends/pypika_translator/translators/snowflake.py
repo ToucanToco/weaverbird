a243e3dd349a7fd526646a0f9bcb79bb6347f598
sanix-darker <s4nixd@gmail.com>
2022-10-06 10:28:57 +0200

    fix: unpivot by converting all targeted columns to float

--- server/src/weaverbird/backends/pypika_translator/translators/snowflake.py
+++ server/src/weaverbird/backends/pypika_translator/translators/snowflake.py
@@ -1,7 +1,9 @@
 from typing import TypeVar
 
+from pypika import functions
 from pypika.dialects import SnowflakeQuery
 from pypika.enums import Dialects
+from pypika.queries import QueryBuilder, format_quotes
 from pypika.terms import Field, Term
 
 from weaverbird.backends.pypika_translator.dialects import SQLDialect
@@ -10,7 +12,9 @@ from weaverbird.backends.pypika_translator.translators.base import (
     DataTypeMapping,
     DateAddWithoutUnderscore,
     SQLTranslator,
+    StepContext,
 )
+from weaverbird.pipeline.steps.unpivot import UnpivotStep
 
 Self = TypeVar("Self", bound="SQLTranslator")
 
@@ -41,6 +45,43 @@ class SnowflakeTranslator(SQLTranslator):
         return DateAddWithoutUnderscore(
             date_part=unit.removesuffix("s"), interval=duration, term=target_column
         )
+
+    @classmethod
+    def _build_unpivot_col(
+        cls, *, step: "UnpivotStep", quote_char: str | None, secondary_quote_char: str
+    ) -> str:
+        value_col = format_quotes(step.value_column_name, quote_char)
+        unpivot_col = format_quotes(step.unpivot_column_name, quote_char)
+        in_cols = ", ".join(col for col in step.unpivot)
+
+        return f"UNPIVOT({value_col} FOR {unpivot_col} IN ({in_cols}))"
+
+    def unpivot(
+        self: Self,
+        *,
+        builder: "QueryBuilder",
+        prev_step_name: str,
+        columns: list[str],
+        step: "UnpivotStep",
+    ) -> StepContext:
+        # FIXME: On Snowflake, We should find a better way to tell a user he/she can only do
+        # an unpivot on columns that have the same type instead of casting it like that
+        columns_to_loop = [
+            f"{functions.Cast(col, self.DATA_TYPE_MAPPING.float).get_sql()} AS {col}"
+            for col in step.unpivot
+        ] + [col for col in step.keep + [step.unpivot_column_name] + [step.value_column_name]]
+        sub_query = self.QUERY_CLS.from_(prev_step_name).select(columns_to_loop)
+
+        unpivot = self._build_unpivot_col(
+            step=step,
+            quote_char=builder.QUOTE_CHAR,
+            secondary_quote_char=builder.SECONDARY_QUOTE_CHAR,
+        )
+        cols = step.keep + [step.unpivot_column_name] + [step.value_column_name]
+        query = functions.LiteralValue(
+            f"{self.QUERY_CLS.from_(sub_query).select(*cols)!s} {unpivot}"
+        )
+        return StepContext(query, cols)
 
 
 SQLTranslator.register(SnowflakeTranslator)
