f46f9387984910f0897ba10c3005e1c8f06dbf77
Luka Peschke <luka.peschke@toucantoco.com>
2022-10-28 10:46:01 +0200

    Update server/src/weaverbird/pipeline/steps/evolution.py
    Co-authored-by: Eric Jolibois <em.jolibois@gmail.com>

--- server/src/weaverbird/pipeline/steps/evolution.py
+++ server/src/weaverbird/pipeline/steps/evolution.py
@@ -21,4 +21,4 @@ class EvolutionStep(BaseStep):
 
 
 class EvolutionStepWithVariable(EvolutionStep, StepWithVariablesMixin):
-    index_columns: TemplatedVariable | list[TemplatedVariable]
+    index_columns: TemplatedVariable | list[TemplatedVariable] = Field(default_factory=list)
