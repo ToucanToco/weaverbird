6dd6e1fc0fdd639ec636a1dab3ada8b2be22c918
David Nowinsky <david@nowinsky.net>
2022-10-31 15:21:19 +0100

    chore(front): upgrade to FontAwesome 6

--- src/assets/FA-ICONS.ts
+++ src/assets/FA-ICONS.ts
@@ -1,4 +1,4 @@
-/* Every icons from font-awesome should be declared here to be used as svg (check the FAIcon component to add it to template) */
+/* Every icon from font-awesome should be declared here to be used as svg (check the FAIcon component to add it to template) */
 import {
   faCalendar as faCalendarFar,
   faClock as faClockFar,
