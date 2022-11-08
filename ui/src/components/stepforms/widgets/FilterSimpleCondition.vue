a555e3fa818014083d05720497089592a1f42342
David Nowinsky <david@nowinsky.net>
2022-11-08 16:41:03 +0100

    ci(ui): run prettier

--- ui/src/components/stepforms/widgets/FilterSimpleCondition.vue
+++ ui/src/components/stepforms/widgets/FilterSimpleCondition.vue
@@ -184,7 +184,7 @@ export default class FilterSimpleConditionWidget extends Vue {
   ];
 
   created() {
-    console.log('FSC created')
+    console.log('FSC created');
     console.log('FSC', this.value);
     // In absence of condition, emit directly to the parent the default value
     if (isEqual(this.value, DEFAULT_FILTER)) {
