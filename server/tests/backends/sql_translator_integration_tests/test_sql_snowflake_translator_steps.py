f50feaaa898bd8b4988d890066eaca07d1f97200
sanix-darker <s4nixd@gmail.com>
2022-12-06 15:15:00 +0100

    test: now we have money hehe, let's unskip snowflake tests

--- server/tests/backends/sql_translator_integration_tests/test_sql_snowflake_translator_steps.py
+++ server/tests/backends/sql_translator_integration_tests/test_sql_snowflake_translator_steps.py
@@ -50,9 +50,6 @@ def engine():
     connection.close()
 
 
-@pytest.skip(
-    "Should be skipped, waiting the payment of creds on november...", allow_module_level=True
-)
 @pytest.mark.parametrize(
     "case_id, case_spec_file", retrieve_case("sql_translator", "snowflake_pypika")
 )
