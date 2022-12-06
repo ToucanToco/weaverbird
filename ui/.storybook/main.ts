2b59021deaf8eddb6fd0dd01a254dd4203599f64
David Nowinsky <david@nowinsky.net>
2022-12-06 12:33:49 +0100

    docs(ui): storybook downgrade to 6
    7 was broken :/

--- ui/.storybook/main.ts
+++ ui/.storybook/main.ts
@@ -1,21 +1,25 @@
-import { mergeConfig } from "vite";
+const { mergeConfig } = require('vite');
 
 module.exports = {
   stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-essentials'],
-  framework: {
-    name: '@storybook/vue-vite',
-    options: {},
+  features: {
+    storyStoreV7: true,
   },
+  // framework: {
+  //   name: '@storybook/vue-vite',
+  //   options: {},
+  // },
   core: {
     builder: '@storybook/builder-vite',
+    disableTelemetry: true,
   },
   async viteFinal(config) {
     // Merge custom configuration into the default config
     return mergeConfig(config, {
       // Use the same "resolve" configuration as your app
       // @ts-ignore
-      resolve: (await import('../vite.config.ts')).default.default.resolve,
+      resolve: (await require('../vite.config.ts')).resolve,
     });
   },
 };
