0921eac05b2a50e34ee06783f0ab9e1fc347a97e
Tiffany Vachez <tiffany.vachez@outlook.com>
2022-12-15 11:13:01 +0100

    fix(new-date-input): popover position [TCTC-4324] (#1655)
    * fix(popover): avoid popover overflowing the window
    
    * fix(new-date-input): adapt design
    
    * chore(ui): v0.98.2
    
    * chore: remove unecessary comment

---
+++ ui/tests/unit/domutil.spec.ts
@@ -0,0 +1,27 @@
+import { describe, expect, it } from 'vitest';
+
+import { computeHeight } from '@/components/domutil';
+
+describe('domutil', () => {
+  describe('computeHeight', () => {
+    it('should return undefined when there is enough space to display', () => {
+      const ctx = {
+        body: { height: 500, top: 0 },
+        parent: { height: 40, top: 200 },
+        element: { offsetHeight: 400 },
+        window: { innerHeight: 500 },
+      };
+      expect(computeHeight(true, ctx)).toBe(undefined);
+    });
+
+    it('should return 200 when there is not enough space to display', () => {
+      const ctx = {
+        body: { height: 500, top: 0 },
+        parent: { height: 40, top: 300 },
+        element: { offsetHeight: 400 },
+        window: { innerHeight: 500 },
+      };
+      expect(computeHeight(true, ctx)).toBe(160);
+    });
+  });
+});
