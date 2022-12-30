50628e28d2ddc60bd8ff69c3d9d042c946d98362
Luka Peschke <luka.peschke@toucantoco.com>
2022-12-30 15:46:13 +0100

    fix(backend/mongo/aggregate): Rename `_id` to `__id` in case it's part of an aggregation's new columns
    _id is reserved by mongo. Thus, we cannot overwrite it
    
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- server/src/weaverbird/backends/mongo_translator/steps/aggregate.py
+++ server/src/weaverbird/backends/mongo_translator/steps/aggregate.py
@@ -3,17 +3,22 @@ from typing import Any
 from weaverbird.backends.mongo_translator.steps.types import MongoStep
 from weaverbird.pipeline.steps import AggregateStep
 
+_ID_COLUMN = "_id"
+_NEW_ID_COLUMN = "__id"
+
 
 def translate_aggregate(step: AggregateStep) -> list[MongoStep]:
     idblock = {col: f"${col}" for col in step.on}
     group: dict[str, dict] = {}
     project: dict[str, Any] = {}
     add_fields = {}
-    group["_id"] = idblock
+    group[_ID_COLUMN] = idblock
 
     for agg_f_step in step.aggregations:
         cols = agg_f_step.columns
-        new_cols = agg_f_step.new_columns
+        # _id is a name reserved by mongo. If for some reason, a user wants to aggregate the _id
+        # column, the aggregation result will be stored in a new __id column
+        new_cols = [col if col != _ID_COLUMN else _NEW_ID_COLUMN for col in agg_f_step.new_columns]
 
         # There is no `$count` operator in Mongo, we have to `$sum` 1s to get
         # an equivalent result
@@ -37,18 +42,18 @@ def translate_aggregate(step: AggregateStep) -> list[MongoStep]:
             {"$unwind": "$_vqbDocsArray"},
             {"$replaceRoot": {"newRoot": {"$mergeObjects": ["$_vqbDocsArray", "$$ROOT"]}}},
             {"$project": {"_vqbDocsArray": 0}},
-            {"$sort": {"_id": 1}},
+            {"$sort": {_ID_COLUMN: 1}},
         ]
     else:
         for group_key in group.keys():
-            if group_key == "_id":
+            if group_key == _ID_COLUMN:
                 for id_key in group[group_key]:
-                    project[id_key] = f"$_id.{id_key}"
+                    project[id_key] = f"${_ID_COLUMN}.{id_key}"
             else:
                 project[group_key] = 1
         return [
             {"$group": group},
             *add_fields_to_add_to_pipeline,
             {"$project": project},
-            {"$sort": {"_id": 1}},
+            {"$sort": {_ID_COLUMN: 1}},
         ]
