cac8542f93eb16fbaeffa5c26258e9365e4987a9
Eric Jolibois <em.jolibois@gmail.com>
2023-01-05 18:36:53 +0100

    fix(mongo): preserve order in uniquegroups

--- server/src/weaverbird/backends/mongo_translator/steps/uniquegroups.py
+++ server/src/weaverbird/backends/mongo_translator/steps/uniquegroups.py
@@ -5,4 +5,26 @@ from weaverbird.pipeline.steps import UniqueGroupsStep
 def translate_uniquegroups(step: UniqueGroupsStep) -> list[MongoStep]:
     id_block = {col: f"${col}" for col in step.on}
     project_block = {col: f"$_id.{col}" for col in step.on}
-    return [{"$group": {"_id": id_block}}, {"$project": project_block}]
+    # Since $group doesn't preserve order, we need to add an extra column to know in which
+    # order arrived the rows, and then sort by it.
+    # But we can't use `$documentNumber` with `$setWindowFields`
+    # or `$unwind` with `includeArrayIndex`
+    # Why? because we would need to group or sort them and
+    # we have no way to know in which order they arrived.
+    # We hence use a JS function to increment a counter.
+    return [
+        {
+            "$set": {
+                "__row_number__": {
+                    "$function": {
+                        "body": "function() {try {row_number+= 1;} catch (e) {row_number= 0;}return row_number;}",
+                        "args": [],
+                        "lang": "js",
+                    }
+                }
+            }
+        },
+        {"$group": {"_id": id_block, "__row_number__": {"$first": "$__row_number__"}}},
+        {"$sort": {"__row_number__": 1}},
+        {"$project": {"_id": 0, **project_block}},
+    ]
