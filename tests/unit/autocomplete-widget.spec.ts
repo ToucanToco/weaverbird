ab95743cfe8d9d3a01f22d81f6da32983f98cfd9
Tiffany Vachez <tiffany.vachez@outlook.com>
2023-01-06 11:32:20 +0100

    fix(new-date-input): show all options for relative date dropdown [TCTC-3428] [TCTC-2484] (#1680)
    * fix(new-date-input): scrolling zone for relative date
    
    * fix(new-date-input): input sizes for relative date
    
    * feat(widget-autocomplete): add a title attribute
    
    * fix(new-date-input): fix dropwdown width and ellipsis
    
    * feat(widget-autocomplete): title attribute only when label provided
    
    * fix(widget-autocomplete): manage undefined case

--- tests/unit/autocomplete-widget.spec.ts
+++ tests/unit/autocomplete-widget.spec.ts
@@ -18,17 +18,17 @@ describe('Widget Autocomplete', () => {
     expect(wrapper.find('VariableInput-stub').exists()).toBeTruthy();
   });
 
-  it('should not have specific templates if the prop withExample is false', () => {
+  it('should not have specific templates by default', () => {
     const wrapper = mount(AutocompleteWidget, {
-      propsData: { withExample: false, options: [{ label: 'foo', example: 'bar' }] },
+      propsData: { options: ['foo', 'bar'] },
     });
     expect(wrapper.find('.option__title').exists()).toBeFalsy();
     expect(wrapper.find('.option__container').exists()).toBeFalsy();
   });
 
-  it('should have specific templates if the prop withExample is true', () => {
+  it('should have specific templates a label is provided in options', () => {
     const wrapper = mount(AutocompleteWidget, {
-      propsData: { withExample: true, options: [{ label: 'foo', example: 'bar' }] },
+      propsData: { options: [{ label: 'foo', example: 'bar' }] },
     });
     expect(wrapper.find('.option__title').exists()).toBeTruthy();
     expect(wrapper.find('.option__container').exists()).toBeTruthy();
@@ -76,11 +76,20 @@ describe('Widget Autocomplete', () => {
   it('should add a tooltip to the option', () => {
     const wrapper = mount(AutocompleteWidget, {
       propsData: {
-        withExample: true,
         options: [{ label: 'foo', example: 'bar', tooltip: 'ukulélé' }],
       },
     });
     expect(wrapper.find('.option__container').exists()).toBeTruthy();
     expect(wrapper.find('.option__container').attributes('title')).toEqual('ukulélé');
+  });
+
+  it('should add a title attribute to the title', () => {
+    const wrapper = mount(AutocompleteWidget, {
+      propsData: {
+        options: [{ label: 'foo', example: 'bar' }],
+      },
+    });
+    expect(wrapper.find('.option__title').exists()).toBeTruthy();
+    expect(wrapper.find('.option__title').attributes('title')).toEqual('foo');
   });
 });
