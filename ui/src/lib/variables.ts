5754f89fe68c4e24d247ebd97fa7b50a7cfa92f9
Alice <alice.sevin@toucantoco.com>
2023-04-19 14:27:25 +0200

    feat(variables): upadte utils and type to enable to use untrusted type delimiters

--- ui/src/lib/variables.ts
+++ ui/src/lib/variables.ts
@@ -1,9 +1,27 @@
+/* notify which delimiters should be used (default or trusted ones)
+  this way we can use different delimiters for each variable
+  [...
+  { 
+    identifier: 'data.client',
+    label: 'Data from client',
+    // => if variable is not trusted it will use default delimiters
+  },
+  {
+    identifier: 'data.server',
+    label: 'Data from server',
+    ...
+    trusted: true -> it will use trusted delimiters
+  }
+  ...]
+*/
+
 export interface AvailableVariable {
   identifier: string; // how the variable will be written in the code
   value?: any; // current value of the variable
   category?: string;
   label: string;
   selected?: boolean;
+  trusted?: boolean;
 }
 
 export type VariablesBucket = AvailableVariable[];
@@ -18,13 +36,11 @@ export interface VariableDelimiters {
   end: string;
 }
 
-/**
- * Determine if value is variable surrounded by delimiters or not, and if yes, extract its identifier.
- */
-export function extractVariableIdentifier(
+/* Retrieve the identifier in a string with specific delimiters (trusted/default) */
+function extractVariableIdentifierWithDelimiters(
   value: any,
-  variableDelimiters: VariableDelimiters,
-): string | undefined {
+  variableDelimiters?: VariableDelimiters,
+) {
   if (typeof value !== 'string' || !variableDelimiters) {
     return undefined;
   }
@@ -55,6 +71,21 @@ export function extractVariableIdentifier(
   }
 }
 
+/**
+ * Determine if value is variable surrounded by delimiters or not, and if yes, extract its identifier.
+ */
+export function extractVariableIdentifier(
+  value: any,
+  variableDelimiters: VariableDelimiters,
+  trustedVariableDelimiters?: VariableDelimiters,
+): string | undefined {
+  // extract variable from trusted or default delimiters
+  return (
+    extractVariableIdentifierWithDelimiters(value, trustedVariableDelimiters) ??
+    extractVariableIdentifierWithDelimiters(value, variableDelimiters)
+  );
+}
+
 /*
 Retrieve a variable in a list of available variables based on the value with variable delimiters and identifier: ex {{toto}}
 */
@@ -62,9 +93,25 @@ export const retrieveVariable = (
   value: string,
   availableVariables: VariablesBucket = [],
   variableDelimiters: VariableDelimiters = { start: '', end: '' },
+  trustedVariableDelimiters?: VariableDelimiters,
 ): AvailableVariable | undefined => {
+  // Retrieve the type of variable (trusted or default)
+  const trimmedValue = value.trim();
+  const isVariable =
+    trustedVariableDelimiters &&
+    trimmedValue.startsWith(trustedVariableDelimiters.start) &&
+    trimmedValue.endsWith(trustedVariableDelimiters.end);
   // retrieve the variable identifier
-  const identifier = extractVariableIdentifier(value, variableDelimiters);
+  const identifier = extractVariableIdentifierWithDelimiters(
+    value,
+    isVariable ? trustedVariableDelimiters : variableDelimiters,
+  );
   // search on relative available variables
-  return availableVariables.find((v: AvailableVariable) => v.identifier === identifier);
+  return availableVariables.find(
+    (v: AvailableVariable) => v.identifier === identifier,
+  );
 };
+
+export const isTrustedVariable = (variable?: AvailableVariable): Boolean => {
+  return !!variable?.trusted;
+}
