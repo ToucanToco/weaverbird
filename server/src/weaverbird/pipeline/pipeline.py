39130bb7076f0b91ea8ae06538dfbdbe767c40af
sanix-darker <s4nixd@gmail.com>
2023-05-05 04:24:07 +0200

    feat: a poc on avoiding the void step when rendering a pipeline having a filter step as __VOID__ in it

--- server/src/weaverbird/pipeline/pipeline.py
+++ server/src/weaverbird/pipeline/pipeline.py
@@ -2,6 +2,17 @@ from typing import Annotated, Any
 
 from pydantic import BaseModel, Field
 
+from weaverbird.pipeline.conditions import (
+    ComparisonCondition,
+    Condition,
+    ConditionComboAnd,
+    ConditionComboOr,
+    DateBoundCondition,
+    InclusionCondition,
+    MatchCondition,
+    NullCondition,
+    SimpleCondition,
+)
 from weaverbird.pipeline.steps.hierarchy import HierarchyStep
 
 from .steps import (
@@ -184,13 +195,58 @@ PipelineStepWithVariables = Annotated[
 ]
 
 
+def _remove_void_condition_steps(
+    variables: dict[str, Any], steps: list[PipelineStepWithVariables | PipelineStep]
+) -> tuple[dict[str, Any], list[PipelineStepWithVariables | PipelineStep]]:
+    void_var_keys = {k for k, v in variables.items() if v == "__VOID__"}
+    variables = {k: v for k, v in variables.items() if v != "__VOID__"}
+
+    def is_not_null_and_value_is_void(condition: Condition):
+        if not isinstance(condition, (NullCondition, ConditionComboAnd, ConditionComboOr)):
+            for vv in void_var_keys:
+                if isinstance(condition.value, str) and condition.value == "{{ " + vv + " }}":
+                    return True
+        return False
+
+    # We only choose to loop on filter step like
+    filter_steps = [s for s in steps if isinstance(s, (FilterStep, FilterStepWithVariables))]
+    for step in filter_steps:
+        if isinstance(
+            step.condition,
+            (
+                ComparisonCondition,
+                InclusionCondition,
+                NullCondition,
+                MatchCondition,
+                DateBoundCondition,
+            ),
+        ):
+            if is_not_null_and_value_is_void(step.condition):
+                steps.remove(step)
+        elif isinstance(step.condition, (ConditionComboAnd, ConditionComboOr)):
+            if isinstance(step.condition, ConditionComboOr):
+                for cond in step.condition.or_:
+                    if is_not_null_and_value_is_void(cond):
+                        step.condition.or_.remove(cond)
+            else:
+                for cond in step.condition.and_:
+                    if is_not_null_and_value_is_void(cond):
+                        step.condition.and_.remove(cond)
+
+    return variables, steps
+
+
 class PipelineWithVariables(BaseModel):
     steps: list[PipelineStepWithVariables | PipelineStep]
 
     def render(self, variables: dict[str, Any], renderer) -> Pipeline:
         # TODO it must be more efficient to render the full pipeline once
+
+        # We clean __VOID__ variable like.
+        # This will also clean the pipeline steps containing those variables
+        cleaned_variables, cleaned_steps = _remove_void_condition_steps(variables, self.steps)
         steps_rendered = [
-            step.render(variables, renderer) if hasattr(step, "render") else step  # type: ignore
-            for step in self.steps
+            step.render(cleaned_variables, renderer) if hasattr(step, "render") else step  # type: ignore
+            for step in cleaned_steps
         ]
         return Pipeline(steps=steps_rendered)
--- server/tests/test_pipeline.py
+++ server/tests/test_pipeline.py
@@ -1,13 +1,21 @@
 import glob
 import json
+from datetime import datetime
 
 import pytest
 from pydantic import BaseModel
 from toucan_connectors.common import nosql_apply_parameters_to_query
 
+from weaverbird.pipeline.conditions import (
+    ComparisonCondition,
+    ConditionComboAnd,
+    ConditionComboOr,
+    DateBoundCondition,
+)
 from weaverbird.pipeline.pipeline import Pipeline, PipelineWithVariables
 from weaverbird.pipeline.steps import DomainStep, RollupStep
 from weaverbird.pipeline.steps.aggregate import Aggregation
+from weaverbird.pipeline.steps.filter import FilterStep
 
 
 class Case(BaseModel):
@@ -82,3 +90,106 @@ def test_to_dict():
     }
     assert actual_dict == expected_dict
     assert pipeline == Pipeline(**pipeline.dict())
+
+
+def test_skip_void_parameter_from_variables():
+    void_step = FilterStep(
+        name="filter",
+        condition=ComparisonCondition(
+            column="colA",
+            operator="ge",
+            value="{{ __front_var_0__ }}",
+        ),
+    )
+
+    simple_filter_step = FilterStep(
+        name="filter",
+        condition=ComparisonCondition(
+            column="colB",
+            operator="eq",
+            value="{{ __front_var_1__ }}",
+        ),
+    )
+
+    composed_filter_step_and_ = FilterStep(
+        condition=ConditionComboAnd(
+            and_=[
+                ComparisonCondition(
+                    column="colC",
+                    operator="gt",
+                    value="{{ __front_var_2__ }}",
+                ),
+                DateBoundCondition(
+                    column="Transaction_date",
+                    operator="until",
+                    value=datetime(2009, 1, 3),  # naive
+                ),
+            ]
+        )
+    )
+
+    composed_filter_step_or_ = FilterStep(
+        condition=ConditionComboOr(
+            or_=[
+                ComparisonCondition(
+                    column="colA",
+                    operator="eq",
+                    value="{{ __front_var_3__ }}",
+                ),
+                ComparisonCondition(
+                    column="colX",
+                    operator="lt",
+                    value="{{ __front_var_4__ }}",
+                ),
+                ComparisonCondition(
+                    column="colA",
+                    operator="eq",
+                    value="toto",
+                ),
+            ],
+        ),
+    )
+    steps = [
+        {"domain": "foobar", "name": "domain"},
+        void_step,
+        composed_filter_step_or_,
+        simple_filter_step,
+        composed_filter_step_and_,
+    ]
+    variables = {
+        "__front_var_0__": "__VOID__",
+        "__front_var_1__": 32,
+        "__front_var_2__": "__VOID__",
+        "__front_var_3__": "TEST TEST",
+        "__front_var_4__": "__VOID__",
+    }
+
+    pipeline_with_variables = PipelineWithVariables(steps=steps)
+    pipeline = pipeline_with_variables.render(variables, renderer=nosql_apply_parameters_to_query)
+
+    # It should not have been rendered:
+    assert pipeline.steps == [
+        {"name": "domain", "domain": "foobar"},
+        {
+            "name": "filter",
+            "condition": {
+                "or_": [
+                    {"column": "colA", "operator": "eq", "value": "TEST TEST"},
+                    {"column": "colA", "operator": "eq", "value": "toto"},
+                ]
+            },
+        },
+        {"name": "filter", "condition": {"column": "colB", "operator": "eq", "value": 32}},
+        {
+            "name": "filter",
+            "condition": {
+                "and_": [
+                    {
+                        "column": "Transaction_date",
+                        "operator": "until",
+                        "value": datetime(2009, 1, 3, 0, 0),
+                    },
+                ]
+            },
+        },
+    ]
