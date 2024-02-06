30b5c286d4ee4037ea8f45ad3c060ed1914216e5
Luka Peschke <luka.peschke@toucantoco.com>
2024-02-06 14:27:32 +0100

    fix(server/mongo): evolution step filters on all index columns
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- server/src/weaverbird/backends/mongo_translator/steps/evolution.py
+++ server/src/weaverbird/backends/mongo_translator/steps/evolution.py
@@ -82,7 +82,10 @@ def translate_evolution(step: EvolutionStep) -> list[MongoStep]:
                         "cond": {
                             "$and": [
                                 {"$eq": ["$_VQB_DATE_PREV", f"$$item.{step.date_col}"]},
-                                {"$eq": [f"${col}", f"$$item.{col}"] for col in step.index_columns},
+                                *(
+                                    {"$eq": [f"${col}", f"$$item.{col}"]}
+                                    for col in step.index_columns
+                                ),
                             ],
                         },
                     },
