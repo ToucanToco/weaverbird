f0c95cd1fd255913667d097c04a489653320f6db
Luka Peschke <luka.peschke@toucantoco.com>
2024-08-27 16:40:37 +0200

    fix(server/mongo): support accents in uppercase and lowercase [TCTC-9132]
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- server/src/weaverbird/backends/mongo_translator/steps/lowercase.py
+++ server/src/weaverbird/backends/mongo_translator/steps/lowercase.py
@@ -2,5 +2,19 @@ from weaverbird.backends.mongo_translator.steps.types import MongoStep
 from weaverbird.pipeline.steps import LowercaseStep
 
 
+# NOTE: Executing javascript is not very performant, however the native $toLower function does
+# not support accents
 def translate_lowercase(step: LowercaseStep) -> list[MongoStep]:
-    return [{"$addFields": {step.column: {"$toLower": f"${step.column}"}}}]
+    return [
+        {
+            "$addFields": {
+                step.column: {
+                    "$function": {
+                        "body": "function(elem) { return typeof elem === 'string' ? elem.toLowerCase() : elem; }",
+                        "args": [f"${step.column}"],
+                        "lang": "js",
+                    }
+                }
+            }
+        }
+    ]
