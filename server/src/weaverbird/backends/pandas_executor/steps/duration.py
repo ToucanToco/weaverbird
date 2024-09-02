4c85b8bf3034d1931754a2f8610d873fe45b8045
Luka Peschke <luka.peschke@toucantoco.com>
2024-09-02 17:22:25 +0200

    duration: convert start and end series to datetime
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- server/src/weaverbird/backends/pandas_executor/steps/duration.py
+++ server/src/weaverbird/backends/pandas_executor/steps/duration.py
@@ -18,8 +18,8 @@ def execute_duration(
     domain_retriever: DomainRetriever = None,
     execute_pipeline: PipelineExecutor = None,
 ) -> DataFrame:
-    start_date_series = to_datetime(df[step.start_date_column])
-    end_date_series = to_datetime(df[step.end_date_column])
+    start_date_series = df[step.start_date_column] = to_datetime(df[step.start_date_column])
+    end_date_series = df[step.end_date_column] = to_datetime(df[step.end_date_column])
     duration_serie_in_seconds = (end_date_series - start_date_series).astype("timedelta64[s]")
     # in pandas 2, there is a timedelta type, and we want seconds
     if pd_version.startswith("2"):
