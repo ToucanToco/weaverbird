7ff68b69d626306c9868c90cb5c97356390ef4e0
Alice <alice.sevin@toucantoco.com>
2024-09-19 11:23:42 +0200

    feat(components): add banner component as in DS

---
+++ ui/src/components/Banner.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="banner">
+		<div class="banner-icon">
+			<FAIcon icon="info" />
+		</div>
+		<div class="banner-content">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	name: 'banner',
+});
+</script>
+
+<style lang="scss" scoped>
+.banner {
+	display: flex;
+	align-items: center;
+	padding: 8px 16px;
+	gap: 8px;
+	color: #126EFF;
+	background-color: #E0ECFF;
+	border-radius: 4px;
+}
+
+.banner-icon {
+	display: flex;
+	width: 16px;
+	height: 16px;
+}
+
+.banner-content {
+	display: flex;
+	align-items: center;
+	gap: 8px;
+	font-weight: '500';
+	font-size: '.875rem';
+	line-height: '150%';
+}
+</style>
