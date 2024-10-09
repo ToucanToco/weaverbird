e812fc6730e2ab55494a61edc7e5e1e36a79dc5e
Alice <alice.sevin@toucantoco.com>
2024-10-09 16:58:21 +0200

    fix: format

--- ui/src/components/stepforms/StepFormComponent.vue
+++ ui/src/components/stepforms/StepFormComponent.vue
@@ -120,7 +120,7 @@ export default class StepFormComponent extends Vue {
 
   @Watch('selectedColumn')
   onSelectedColumnChanged() {
-    this.setSelectedColumns({ column: this.selectedColumn })
+    this.setSelectedColumns({ column: this.selectedColumn });
   }
 }
 </script>
