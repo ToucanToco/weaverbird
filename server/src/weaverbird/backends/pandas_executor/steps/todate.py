ace8f4bb2274f12fd9cec98ec82dc5afa3602c1b
Luka Peschke <luka.peschke@toucantoco.com>
2025-06-26 14:32:32 +0200

    feat(server/pandas): support french months in todate step
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- server/src/weaverbird/backends/pandas_executor/steps/todate.py
+++ server/src/weaverbird/backends/pandas_executor/steps/todate.py
@@ -3,6 +3,39 @@ from pandas import DataFrame, to_datetime
 from weaverbird.backends.pandas_executor.types import DomainRetriever, PipelineExecutor
 from weaverbird.pipeline.steps import ToDateStep
 
+_FR_TO_EN_MONTHS_LONG = [
+    ("janvier", "january"),
+    ("février", "february"),
+    ("mars", "march"),
+    ("avril", "april"),
+    ("mai", "may"),
+    ("juin", "june"),
+    ("juillet", "july"),
+    ("août", "august"),
+    ("aout", "august"),
+    ("septembre", "september"),
+    ("octobre", "october"),
+    ("novembre", "november"),
+    ("décembre", "december"),
+    ("decembre", "december"),
+]
+
+_FR_TO_EN_MONTHS_SHORT = [
+    ("janv", "jan"),
+    ("févr", "feb"),
+    ("mars", "mar"),
+    ("avr", "apr"),
+    ("mai", "may"),
+    ("juin", "jun"),
+    ("juil", "jul"),
+    ("août", "aug"),
+    ("aout", "aug"),
+    ("sept", "sep"),
+    ("oct", "oct"),
+    ("nov", "nov"),
+    ("déc", "dec"),
+]
+
 
 def execute_todate(
     step: ToDateStep,
@@ -20,6 +53,13 @@ def execute_todate(
         else:
             # Timestamps are expected in ms (not in ns, which is pandas' default)
             timestamp_unit = "ms"
+
+    if format is not None and ("%b" in format or "%B" in format):
+        replacements = _FR_TO_EN_MONTHS_SHORT if "%b" in format else _FR_TO_EN_MONTHS_LONG
+        col = df[step.column].str.lower()
+        for french_month, english_month in replacements:
+            col = col.str.replace(french_month, english_month)
+        df[step.column] = col
 
     datetime_serie = to_datetime(df[step.column], format=format, errors="coerce", unit=timestamp_unit)
     return df.assign(**{step.column: datetime_serie})
